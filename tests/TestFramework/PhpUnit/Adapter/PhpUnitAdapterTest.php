923602b7074bafb4eed5d2b014fd36f1e93a0693
borNfreee <b0rn@list.ru>
2017-05-21 22:19:52 +0300

    Add test for PhpUnitAdapter->testsPass method

---
+++ tests/TestFramework/PhpUnit/Adapter/PhpUnitAdapterTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TestFramework\PhpUnit\Adapter;
+
+
+use Infection\Finder\AbstractExecutableFinder;
+use Infection\TestFramework\CommandLineArgumentsAndOptionsBuilder;
+use Infection\TestFramework\PhpUnit\Adapter\PhpUnitAdapter;
+use Infection\TestFramework\PhpUnit\Config\Builder\InitialConfigBuilder;
+use Infection\TestFramework\PhpUnit\Config\Builder\MutationConfigBuilder;
+use PHPUnit\Framework\TestCase;
+use Mockery;
+
+class PhpUnitAdapterTest extends TestCase
+{
+    /**
+     * @dataProvider passProvider
+     */
+    public function test_it_determines_whether_tests_pass_or_not($output, $expectedResult)
+    {
+        $executableFined = Mockery::mock(AbstractExecutableFinder::class);
+        $initialConfigBuilder = Mockery::mock(InitialConfigBuilder::class);
+        $mutationConfigBuilder = Mockery::mock(MutationConfigBuilder::class);
+        $cliArgumentsBuilder = Mockery::mock(CommandLineArgumentsAndOptionsBuilder::class);
+
+        $adapter = new PhpUnitAdapter($executableFined, $initialConfigBuilder, $mutationConfigBuilder, $cliArgumentsBuilder);
+
+        $result = $adapter->testsPass($output);
+
+        $this->assertSame($expectedResult, $result);
+    }
+
+    public function passProvider()
+    {
+        return [
+            ['OK, but incomplete, skipped, or risky tests!', true],
+            ['OK (5 tests, 3 assertions)', true],
+            ['FAILURES!', false],
+            ['ERRORS!', false],
+        ];
+    }
+}
