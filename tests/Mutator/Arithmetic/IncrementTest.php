8aa944ec0312d8fb1268a6ef6b0760081b32d913
Maks Rafalko <b0rn@list.ru>
2017-07-23 21:06:54 +0300

    Add arithmetic tests (#12)

---
+++ tests/Mutator/Arithmetic/IncrementTest.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Copyright © 2017 Maks Rafalko
+ *
+ * License: https://opensource.org/licenses/BSD-3-Clause New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Infection\Tests\Mutator\Arithmetic;
+
+use Infection\Mutator\Arithmetic\Decrement;
+use Infection\Mutator\Arithmetic\Increment;
+use Infection\Mutator\Mutator;
+use Infection\Tests\Mutator\AbstractMutator;
+
+class IncrementTest extends AbstractMutator
+{
+    protected function getMutator(): Mutator
+    {
+        return new Increment();
+    }
+
+    public function test_replaces_post_decrement()
+    {
+        $code = '<?php $a = 1; $a++;';
+        $mutatedCode = $this->mutate($code);
+
+        $expectedMutatedCode = <<<'CODE'
+<?php
+
+$a = 1;
+$a--;
+CODE;
+
+        $this->assertSame($expectedMutatedCode, $mutatedCode);
+    }
+
+    public function test_replaces_pre_decrement()
+    {
+        $code = '<?php $a = 1; ++$a;';
+        $mutatedCode = $this->mutate($code);
+
+        $expectedMutatedCode = <<<'CODE'
+<?php
+
+$a = 1;
+--$a;
+CODE;
+
+        $this->assertSame($expectedMutatedCode, $mutatedCode);
+    }
+
+    public function test_parses_decrement_correctly()
+    {
+        $code = '<?php 1 + +1;';
+        $mutatedCode = $this->mutate($code);
+
+        $expectedMutatedCode = <<<'CODE'
+<?php
+
+1 + +1;
+CODE;
+
+        $this->assertSame($expectedMutatedCode, $mutatedCode);
+    }
+}
