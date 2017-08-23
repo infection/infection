9529c2253bf5c9ad54d3997151ead2312d756110
Dmitry Khomutov <poisoncorpsee@gmail.com>
2017-08-23 14:23:57 +0700

    Fixed bootstrap.php for case with install by Composer as dependency and run from vendor/bin (#22)
    * Fixed bootstrap.php for case with composer install as dependency and
    run as `./vendor/bin/infection`.
    
    * Fixes.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,6 +4,31 @@
  *
  * License: https://opensource.org/licenses/BSD-3-Clause New BSD License
  */
-require __DIR__ . '/../vendor/autoload.php';
+
+$files = [
+    __DIR__ . '/../../../autoload.php',
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/vendor/autoload.php'
+];
+
+foreach ($files as $file) {
+    if (file_exists($file)) {
+        define('INFECTION_COMPOSER_INSTALL', $file);
+
+        break;
+    }
+}
+
+if (!defined('INFECTION_COMPOSER_INSTALL')) {
+    fwrite(STDERR,
+        'You need to set up the project dependencies using Composer:' . PHP_EOL . PHP_EOL .
+        '    composer install' . PHP_EOL . PHP_EOL .
+        'You can learn all about Composer on https://getcomposer.org/.' . PHP_EOL
+    );
+
+    die(1);
+}
+
+require INFECTION_COMPOSER_INSTALL;
 
 $container = require __DIR__ . '/container.php';
