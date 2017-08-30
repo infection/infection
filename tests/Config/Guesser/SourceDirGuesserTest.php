27215852cc57016ddd47ff1e9a571099f9213ff7
SiD <plbsid@gmail.com>
2017-08-30 22:53:16 +0300

    Fix autoload section from composer.json and allow to use multiple paths

--- tests/Config/Guesser/SourceDirGuesserTest.php
+++ tests/Config/Guesser/SourceDirGuesserTest.php
@@ -7,8 +7,7 @@
 
 declare(strict_types=1);
 
-
-namespace Infection\Tests\Guesser;
+namespace Infection\Tests\Config\Guesser;
 
 use Infection\Config\Guesser\SourceDirGuesser;
 use PHPUnit\Framework\TestCase;
@@ -63,5 +62,43 @@ CODE;
         $guesser = new SourceDirGuesser(json_decode($composerJson));
 
         $this->assertSame(['src'], $guesser->guess());
+    }
+
+    public function test_it_returns_null_when_does_not_have_autoload()
+    {
+        $guesser = new SourceDirGuesser(json_decode('{}'));
+
+        $this->assertNull($guesser->guess());
+    }
+
+    public function test_it_returns_only_src_if_contains_array_of_paths()
+    {
+        $guesser = new SourceDirGuesser(
+            json_decode('{"autoload":{"psr-0": {"": ["src/", "libs"]}}}')
+        );
+
+        $this->assertSame(['src'], $guesser->guess());
+    }
+
+    public function test_it_returns_list_if_contains_array_of_paths_without_src()
+    {
+        $guesser = new SourceDirGuesser(
+            json_decode('{"autoload":{"psr-4": {"NameSpace\\//": ["sources/", "libs"]}}}')
+        );
+
+        $this->assertSame(['sources', 'libs'], $guesser->guess());
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage autoload section does not match the expected JSON schema
+     */
+    public function test_it_throw_invalid_autoload_exception()
+    {
+        $guesser = new SourceDirGuesser(
+            json_decode('{"autoload":{"psr-4": [{"NameSpace\\//": ["sources/", "libs"]}]}}')
+        );
+
+        $guesser->guess();
     }
 }
