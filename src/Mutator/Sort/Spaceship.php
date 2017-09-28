3a2192eed02b66391711e349bfbe5354656035e3
Marco Perone <m.perone@mvlabs.it>
2017-09-28 10:18:52 +0200

    spaceship operator mutator

---
+++ src/Mutator/Sort/Spaceship.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright © 2017 Maks Rafalko
+ *
+ * License: https://opensource.org/licenses/BSD-3-Clause New BSD License
+ */
+declare(strict_types=1);
+
+namespace Infection\Mutator\Sort;
+
+use Infection\Mutator\FunctionBodyMutator;
+use PhpParser\Node;
+
+class Spaceship extends FunctionBodyMutator
+{
+    /**
+     * Swaps the arguments in the Spaceship operator <=>
+     *
+     * @param Node $node
+     *
+     * @return Node\Expr\BinaryOp\Spaceship
+     */
+    public function mutate(Node $node)
+    {
+        return new Node\Expr\BinaryOp\Spaceship($node->right, $node->left);
+    }
+
+    public function shouldMutate(Node $node): bool
+    {
+        return $node instanceof Node\Expr\BinaryOp\Spaceship;
+    }
+}
