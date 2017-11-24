7d738bf15995d44fe2f930d61cc4b33cfdddd1fe
Lander Vanderstraeten <Landerstraeten@users.noreply.github.com>
2017-11-24 21:23:52 +0100

    Add comma separated files filter (#84)

---
+++ src/Finder/SourceFilesFinder.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright © 2017 Maks Rafalko
+ *
+ * License: https://opensource.org/licenses/BSD-3-Clause New BSD License
+ */
+declare(strict_types=1);
+
+namespace Infection\Finder;
+
+use Symfony\Component\Finder\Finder;
+
+class SourceFilesFinder
+{
+    private $sourceDirectories;
+    private $excludeDirectories;
+
+    /**
+     * @param string[] $sourceDirectories
+     * @param string[] $excludeDirectories
+     */
+    public function __construct(array $sourceDirectories, array $excludeDirectories)
+    {
+        $this->sourceDirectories = $sourceDirectories;
+        $this->excludeDirectories = $excludeDirectories;
+    }
+
+    public function getSourceFiles(string $filter = ''): Finder
+    {
+        $finder = new Finder();
+
+        array_walk($this->excludeDirectories, function ($excludePath) use ($finder) {
+            $finder->notPath($excludePath);
+        });
+
+        if ('' === $filter) {
+            $finder->in($this->sourceDirectories)->files();
+
+            return $finder;
+        }
+
+        $finder->in('.')->files();
+
+        $filters = explode(',', $filter);
+        array_walk($filters, function ($fileFilter) use ($finder) {
+            $finder->path($fileFilter);
+        });
+
+        return $finder;
+    }
+}
---
+++ tests/Files/Finder/SecondFile.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © 2017 Maks Rafalko
+ *
+ * License: https://opensource.org/licenses/BSD-3-Clause New BSD License
+ */
+declare(strict_types=1);
+
+namespace Infection\Tests\Files\Finder;
+
+class SecondFile
+{
+}
