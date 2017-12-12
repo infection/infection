fc80ff8ddcef3e8fb3a4a48a0c415e2643bcc0c8
borNfreee <b0rn@list.ru>
2017-12-12 23:41:06 +0300

    Cache original statements (AST) in order to improve performance by cloning AST before mutatiing
    Signed-off-by: borNfreee <b0rn@list.ru>

---
+++ src/Visitor/CloneVisitor.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright © 2017 Maks Rafalko
+ *
+ * License: https://opensource.org/licenses/BSD-3-Clause New BSD License
+ */
+declare(strict_types=1);
+
+namespace Infection\Visitor;
+
+use PhpParser\Node;
+use PhpParser\NodeVisitorAbstract;
+
+class CloneVisitor extends NodeVisitorAbstract
+{
+    public function enterNode(Node $origNode)
+    {
+        return clone $origNode;
+    }
+}
