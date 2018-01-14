0f06fb474d30cc978419b66ba34ec22d70684934
Gert de Pagter <BackEndTea@users.noreply.github.com>
2018-01-14 19:56:10 +0100

    Add header comment to php-cs fixer (#119)
    * Keep php-cs sorted
    
    * Add header to php-cs-fixer

--- src/Visitor/MutatorVisitor.php
+++ src/Visitor/MutatorVisitor.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * Copyright © 2017 Maks Rafalko
+ * Copyright © 2017-2018 Maks Rafalko
  *
  * License: https://opensource.org/licenses/BSD-3-Clause New BSD License
  */
+
 declare(strict_types=1);
 
 namespace Infection\Visitor;
