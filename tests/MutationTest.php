5d5855f3ec0535dcf4e46b8f8500e7e073470fa6
Gert de Pagter <gertp93@gmail.com>
2018-01-16 16:58:06 +0100

    Add a test for isOnFunctionSignature

--- tests/MutationTest.php
+++ tests/MutationTest.php
@@ -38,4 +38,28 @@ class MutationTest extends TestCase
 
         $this->assertSame('5f52c44bcebde86a7ee79d0080c0e12a', $mutation->getHash());
     }
+
+    public function test_it_correctly_sets_is_on_function_signature()
+    {
+        $mutator = new Plus();
+        $attributes = [
+            'startLine' => 3,
+            'endLine' => 5,
+            'startTokenPos' => 21,
+            'endTokenPos' => 31,
+            'startFilePos' => 43,
+            'endFilePos' => 53,
+        ];
+
+        $mutation = new Mutation(
+            '/abc.php',
+            [],
+            $mutator,
+            $attributes,
+            'Interface_',
+            false
+        );
+
+        $this->assertFalse($mutation->isOnFunctionSignature());
+    }
 }
