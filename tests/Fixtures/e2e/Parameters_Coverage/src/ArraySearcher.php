e2321425728e322ed6098c031f57ba322aec5ba4
Gert de Pagter <gertp93@gmail.com>
2018-01-22 17:42:26 +0100

    Introduce e2e tests

---
+++ tests/Fixtures/e2e/Parameters_Coverage/src/ArraySearcher.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ParamCoverage;
+
+class ArraySearcher
+{
+    private $items;
+
+    public function __construct($items)
+    {
+        $this->items = $items;
+
+    }
+
+    public function search($value, bool $strict = false)
+    {
+        if (($return =  \array_search($value, $this->items, $strict)) === false) {
+            throw new \Exception($value);
+        }
+        return $return;
+    }
+}
