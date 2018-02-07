a49d8820d2a6bfc8e73da237108dfcf469d54c0b
Gert de Pagter <gertp93@gmail.com>
2018-02-07 21:24:15 +0100

    Add the Throw Mutator
    This mutator simply removes the 'throw' keyword.

---
+++ src/Mutator/Operator/Throw_.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright © 2017-2018 Maks Rafalko
+ *
+ * License: https://opensource.org/licenses/BSD-3-Clause New BSD License
+ */
+declare(strict_types=1);
+
+namespace Infection\Mutator\Operator;
+
+use Infection\Mutator\Mutator;
+use PhpParser\Node;
+
+class Throw_ extends Mutator
+{
+    public function mutate(Node $node)
+    {
+        return $node->expr;
+    }
+
+    public function shouldMutate(Node $node): bool
+    {
+        return $node instanceof Node\Stmt\Throw_;
+    }
+}
