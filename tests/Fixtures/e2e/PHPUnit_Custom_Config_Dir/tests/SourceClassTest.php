bc20144f40fc9c55765a73069159bbf716b7e89f
borNfreee <b0rn@list.ru>
2018-02-28 23:52:20 +0300

    Add e2e tests for custom PHPUnit config dir

---
+++ tests/Fixtures/e2e/PHPUnit_Custom_Config_Dir/tests/SourceClassTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Namespace_\Test;
+
+use Namespace_\SourceClass;
+use PHPUnit\Framework\TestCase;
+
+class SourceClassTest extends TestCase
+{
+    public function test_hello()
+    {
+        $sourceClass = new SourceClass();
+        $this->assertSame('hello', $sourceClass->hello());
+    }
+}
