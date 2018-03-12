ec233eb600cb5f7b54a53a8127b42e90b7254b01
Tom Newby <me@tomnewby.net>
2018-03-12 20:54:01 +1000

    Use a safer filesystem strategy across platforms (Win) and clean up

--- tests/Finder/TestFrameworkFinderTest.php
+++ tests/Finder/TestFrameworkFinderTest.php
@@ -12,23 +12,26 @@ namespace Infection\Tests\Finder;
 use Infection\Finder\Exception\FinderException;
 use Infection\Finder\TestFrameworkFinder;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Filesystem\Filesystem;
 
 class TestFrameworkFinderTest extends TestCase
 {
     public function test_it_can_load_a_custom_path()
     {
-        $filename = '/tmp/infection-test-framework-finder-test' . uniqid('', false);
-
-        touch($filename);
+        $fileSystem = new Filesystem();
+        $filename = $fileSystem->tempnam(sys_get_temp_dir(), 'infection-test');
+        $fileSystem->touch([$filename]);
 
         $frameworkFinder = new TestFrameworkFinder('not-used', $filename);
 
         static::assertEquals($filename, $frameworkFinder->find(), 'Should return the custom path');
+
+        $fileSystem->remove([$filename]);
     }
 
     public function test_invalid_custom_path_throws_exception()
     {
-        $filename = '/tmp/infection-test-framework-finder-test' . uniqid('', false);
+        $filename = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'infection-test' . \microtime(true) . \random_int(100, 999);
 
         $frameworkFinder = new TestFrameworkFinder('not-used', $filename);
 
