ee9496288e7a6127974f27c351cba591d0e50d7a
Tom Newby <me@tomnewby.net>
2018-03-12 18:16:19 +1000

    Add breaking test

---
+++ tests/Finder/TestFrameworkFinderTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright © 2017-2018 Maks Rafalko
+ *
+ * License: https://opensource.org/licenses/BSD-3-Clause New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Infection\Tests\Finder;
+
+use Infection\Finder\SourceFilesFinder;
+use Infection\Finder\TestFrameworkFinder;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Finder\Finder;
+
+class TestFrameworkFinderTest extends TestCase
+{
+    public function test_it_can_load_a_custom_path(): void
+    {
+        $filename = '/tmp/infection-test-framework-finder-test' . uniqid('', false);
+
+        touch($filename);
+
+        $frameworkFinder = new TestFrameworkFinder('not-used', $filename);
+
+        static::assertEquals($filename, $frameworkFinder->find(), 'Should return the custom path');
+    }
+}
