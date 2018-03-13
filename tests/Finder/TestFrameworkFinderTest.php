cbbb11aa364fcc49ca0baf40788a585ef4f39250
Tom Newby <me@tomnewby.net>
2018-03-13 21:40:40 +1000

    Use setup/teardown

--- tests/Finder/TestFrameworkFinderTest.php
+++ tests/Finder/TestFrameworkFinderTest.php
@@ -11,27 +11,49 @@ namespace Infection\Tests\Finder;
 
 use Infection\Finder\Exception\FinderException;
 use Infection\Finder\TestFrameworkFinder;
+use Infection\Utils\TmpDirectoryCreator;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Filesystem\Filesystem;
 
 class TestFrameworkFinderTest extends TestCase
 {
+    /**
+     * @var Filesystem
+     */
+    private $fileSystem;
+
+    /**
+     * @var string
+     */
+    private $workspace;
+
+    /**
+     * @var string
+     */
+    private $tmpDir;
+
+    protected function setUp()
+    {
+        $this->workspace = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'infection-test' . \microtime(true) . \random_int(100, 999);
+
+        $this->fileSystem = new Filesystem();
+        $this->tmpDir = (new TmpDirectoryCreator($this->fileSystem))->createAndGet($this->workspace);
+    }
+
     public function test_it_can_load_a_custom_path()
     {
-        $fileSystem = new Filesystem();
-        $filename = $fileSystem->tempnam(sys_get_temp_dir(), 'infection-test');
-        $fileSystem->touch([$filename]);
+        $filename = $this->fileSystem->tempnam($this->tmpDir, 'test');
 
         $frameworkFinder = new TestFrameworkFinder('not-used', $filename);
 
-        static::assertEquals($filename, $frameworkFinder->find(), 'Should return the custom path');
-
-        $fileSystem->remove([$filename]);
+        $this->assertEquals($filename, $frameworkFinder->find(), 'Should return the custom path');
     }
 
     public function test_invalid_custom_path_throws_exception()
     {
-        $filename = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'infection-test' . \microtime(true) . \random_int(100, 999);
+        $filename = $this->fileSystem->tempnam($this->tmpDir, 'test');
+        // Remove it so that the file doesn't exist
+        $this->fileSystem->remove($filename);
 
         $frameworkFinder = new TestFrameworkFinder('not-used', $filename);
 
@@ -39,5 +61,10 @@ class TestFrameworkFinderTest extends TestCase
         $this->expectExceptionMessageRegExp('/custom path/');
 
         $frameworkFinder->find();
+    }
+
+    protected function tearDown()
+    {
+        $this->fileSystem->remove($this->workspace);
     }
 }
