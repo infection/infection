9cebae0969c6968828b30d5fe838858240296a78
Jesper Klippel <klip0004@hz.nl>
2018-03-19 13:34:37 +0100

    Add a For Loop mutator & test

---
+++ src/Mutator/ZeroIteration/For_.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright © 2017-2018 Maks Rafalko
+ *
+ * License: https://opensource.org/licenses/BSD-3-Clause New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Infection\Mutator\ZeroIteration;
+
+use Infection\Mutator\Mutator;
+use PhpParser\Node;
+
+class For_ extends Mutator
+{
+    public function mutate(Node $node)
+    {
+        return new Node\Stmt\For_(
+            [
+                'init' => $node->init,
+                'cond' => [new Node\Expr\ConstFetch(new Node\Name('false'))],
+                'loop' => $node->loop,
+                'stmts' => $node->stmts,
+            ],
+            $node->getAttributes()
+        );
+    }
+
+    public function shouldMutate(Node $node): bool
+    {
+        return $node instanceof Node\Stmt\For_;
+    }
+}
