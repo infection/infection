76bf3d5c369c52ea03428d84b5ab406bec8e55b8
Alexey Kopytko <alexey@kopytko.com>
2018-03-24 02:24:25 +0900

    ArrayItem Mutator (#240)
    Replaces [$a->foo => $b->bar] with [$a->foo > $b->bar]

---
+++ tests/Mutator/Boolean/ArrayItemTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright © 2017-2018 Maks Rafalko
+ *
+ * License: https://opensource.org/licenses/BSD-3-Clause New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Infection\Tests\Mutator\Boolean;
+
+use Infection\Tests\Mutator\AbstractMutatorTestCase;
+
+class ArrayItemTest extends AbstractMutatorTestCase
+{
+    /**
+     * @dataProvider provideMutationCases
+     */
+    public function test_mutator($input, $expected = null)
+    {
+        $this->doTest($input, $expected);
+    }
+
+    public function provideMutationCases(): \Generator
+    {
+        yield 'It mutates double arrow operator to a greater than comparison when operands can have side-effects' => [
+            <<<'PHP'
+<?php
+
+[$a->foo => $b->bar];
+[$a->foo() => bar()];
+[foo() => $b->bar];
+PHP
+            ,
+            <<<'PHP'
+<?php
+
+[$a->foo > $b->bar];
+[$a->foo() > bar()];
+[foo() > $b->bar];
+PHP
+            ,
+        ];
+
+        yield 'It does not mutate arrays without double arrow operator' => [
+            <<<'PHP'
+<?php
+
+[$b];
+PHP
+            ,
+        ];
+
+        yield 'It does not mutate arrays when side-effects are not expected' => [
+            <<<'PHP'
+<?php
+
+['string' => 1];
+[true => false];
+[$a => $b];
+PHP
+            ,
+        ];
+    }
+}
