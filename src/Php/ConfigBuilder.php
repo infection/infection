2052cfc75e7fc72f5a8666d4551f3d4e0641a352
Théo FIDRY <theo.fidry@gmail.com>
2018-03-24 07:10:48 +0000

    Use loaded ini settings
    The original issue was reported by @senmai in #252:
    
    >right now `php -dmemory_limit=512M` infection has no effect on child processes
    
    The issue is actually not specific to this peculiar parameter but any parameter overriden with the
    `-d` option.
    
    Indeed when building the temporary ini file, we are only looking at the settings coming from the
    scanned files. Those settings however can be changed with the `-d` option. So provided you have:
    
    ```
    // php.ini
    param=X
    
    // console
    $ php -dparam=Y infection
    ```
    
    We _should_ have in the temporary ini file `param=Y`.

--- src/Php/ConfigBuilder.php
+++ src/Php/ConfigBuilder.php
@@ -80,6 +80,13 @@ final class ConfigBuilder
             $content .= preg_replace($regex, ';$1', file_get_contents($iniPath)) . PHP_EOL;
         }
 
+        $loaded = ini_get_all(null, false);
+        $config = parse_ini_string($content);
+
+        $content .= $this->mergeLoadedConfig($loaded, $config);
+        // Work-around for https://bugs.php.net/bug.php?id=75932
+        $content .= 'opcache.enable_cli=0'.PHP_EOL;
+
         return (bool) @file_put_contents($this->tmpIniPath, $content);
     }
 
@@ -90,5 +97,39 @@ final class ConfigBuilder
         }
 
         return putenv(self::ENV_TEMP_PHP_CONFIG_PATH . '=' . $this->tmpIniPath);
+    }
+
+    /**
+     * Returns default or changed settings for the tmp ini
+     *
+     * Ini settings can be passed on the command line using the -d option. To
+     * preserve these, all loaded settings that are either not present or
+     * different from those in the ini files are added at the end of the tmp ini.
+     *
+     * @param array $loadedConfig All current ini settings
+     * @param array $iniConfig Settings from user ini files
+     *
+     * @return string
+     *
+     * @see https://github.com/composer/xdebug-handler/blob/master/src/XdebugHandler.php
+     * @license MIT composer/xdebug-handler
+     */
+    private function mergeLoadedConfig(array $loadedConfig, array $iniConfig)
+    {
+        $content = '';
+        foreach ($loadedConfig as $name => $value) {
+            // Values will either be null, string or array (HHVM only)
+            if (!is_string($value) || strpos($name, 'xdebug') === 0) {
+                continue;
+            }
+            if (!isset($iniConfig[$name]) || $iniConfig[$name] !== $value) {
+                // Based on main -d option handling in php-src/sapi/cli/php_cli.c
+                if ($value && !ctype_alnum($value)) {
+                    $value = '"'.str_replace('"', '\\"', $value).'"';
+                }
+                $content .= $name.'='.$value.PHP_EOL;
+            }
+        }
+        return $content;
     }
 }
