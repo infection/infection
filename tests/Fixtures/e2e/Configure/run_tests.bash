a0321142adf5538bcfdc2e02dc25432d521f5927
Alexey Kopytko <alexey@kopytko.com>
2018-04-05 18:44:07 +0900

    TestFrameworkConfigPathProvider needs strings for all inputs, not nulls
    E2E test added for initial configuration

---
+++ tests/Fixtures/e2e/Configure/run_tests.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if test ! -f "$(which expect)"; then
+    test -x $(which tput) && tput setaf 1 # red
+    echo "Please install expect; it is readily available from apt and brew"
+    exit 1;
+fi
+
+cd $(dirname $0)
+rm -f infection.json.dist
+
+set -e
+
+./do_configure.expect
+
+test -f infection.json.dist
+diff -u infection.json.test infection.json.dist
