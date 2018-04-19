225b4c0125cb5ffb078dec520f6cccfadfd0caf0
Alexey Kopytko <alexey@kopytko.com>
2018-04-20 08:57:14 +0900

    Got back PhpExecutableFinderTest

---
+++ tests/Process/ExecutableFinder/PhpExecutableFinderTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright © 2017-2018 Maks Rafalko
+ *
+ * License: https://opensource.org/licenses/BSD-3-Clause New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Infection\Tests\Process\ExecutableFinder;
+
+use Infection\Process\ExecutableFinder\PhpExecutableFinder;
+use PHPUnit\Framework\TestCase;
+
+class PhpExecutableFinderTest extends TestCase
+{
+    public function test_it_finds_needed_args()
+    {
+        $finder = new PhpExecutableFinder();
+
+        if ('phpdbg' == PHP_SAPI) {
+            $this->assertSame(['-qrr'], $finder->findArguments());
+
+            return;
+        }
+
+        $this->assertSame([], $finder->findArguments());
+    }
+}
