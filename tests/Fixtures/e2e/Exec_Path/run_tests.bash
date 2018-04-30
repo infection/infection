5becc87ba459e0d040aa78e924bfe19a84423596
Alexey Kopytko <alexey@kopytko.com>
2018-04-30 14:48:10 +0900

    Extended test suite for Exec_Path e2e test

--- tests/Fixtures/e2e/Exec_Path/run_tests.bash
+++ tests/Fixtures/e2e/Exec_Path/run_tests.bash
@@ -2,6 +2,10 @@
 
 set -e
 
+tputx () {
+	test -x $(which tput) && tput "$@"
+}
+
 run () {
     local INFECTION=${1}
     local PHPARGS=${2}
@@ -16,8 +20,42 @@ run () {
 
 cd $(dirname "$0")
 
-PATH=$PATH:bin php vendor/bin/phpunit
+if [ "$PHPDBG" = "1" ]
+then
+    tputx bold
+    echo "Will be using phpdbg"
+    tputx sgr0
+fi
 
-PATH=$PATH:bin run "../../../../bin/infection"
+tputx bold
+echo "Initial test run outside Infection must be successful"
+tputx sgr0
+
+if [ "$PHPDBG" = "1" ]
+then
+    PATH=$PATH:bin phpdbg -qrr vendor/bin/phpunit --coverage-xml=coverage/coverage-xml --log-junit=coverage/phpunit.junit.xml
+else
+    PATH=$PATH:bin php vendor/bin/phpunit --coverage-xml=coverage/coverage-xml --log-junit=coverage/phpunit.junit.xml
+fi
+
+test -f coverage/phpunit.junit.xml
+test -f coverage/coverage-xml/RunShellScript.php.xml
+test -f coverage/coverage-xml/index.xml
+
+tputx bold
+echo "PHPUnit finished all right"
+echo "Pre-generated coverage..."
+tputx sgr0
+
+PATH=$PATH:bin run "../../../../bin/infection --coverage=coverage --quiet"
+diff -uw expected-output.txt infection-log.txt
+
+tputx bold
+echo "Internal coverage..."
+tputx sgr0
+
+PATH=$PATH:bin run "../../../../bin/infection --quiet"
 
 diff -w expected-output.txt infection-log.txt
+
+rm -vfr coverage
