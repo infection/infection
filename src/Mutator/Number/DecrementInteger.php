f22303de0ffc848a9ddf4a3ebd3549c20c9e49d9
borNfreee <b0rn@list.ru>
2018-06-01 20:21:04 +0300

    Do not decrement integer 0 when it is being compared with the result of count()

--- src/Mutator/Number/DecrementInteger.php
+++ src/Mutator/Number/DecrementInteger.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 namespace Infection\Mutator\Number;
 
 use Infection\Mutator\Util\Mutator;
+use Infection\Visitor\ParentConnectorVisitor;
 use PhpParser\Node;
 
 /**
@@ -31,6 +32,30 @@ final class DecrementInteger extends Mutator
 
     protected function mutatesNode(Node $node): bool
     {
-        return $node instanceof Node\Scalar\LNumber && $node->value != 1;
+        if (!$node instanceof Node\Scalar\LNumber || $node->value === 1) {
+            return false;
+        }
+
+        return !$this->isZeroComparedWithCountResult($node);
+    }
+
+    private function isZeroComparedWithCountResult(Node $node): bool
+    {
+        if ($node->value !== 0) {
+            return false;
+        }
+
+        $parentNode = $node->getAttribute(ParentConnectorVisitor::PARENT_KEY);
+
+        if (!$parentNode instanceof Node\Expr\BinaryOp\Identical
+            && !$parentNode instanceof Node\Expr\BinaryOp\NotIdentical
+            && !$parentNode instanceof Node\Expr\BinaryOp\Equal
+            && !$parentNode instanceof Node\Expr\BinaryOp\NotEqual
+            && !$parentNode instanceof Node\Expr\BinaryOp\Greater
+            && !$parentNode instanceof Node\Expr\BinaryOp\GreaterOrEqual) {
+            return false;
+        }
+
+        return $parentNode->left instanceof Node\Expr\FuncCall && $parentNode->left->name->toString() === 'count';
     }
 }
