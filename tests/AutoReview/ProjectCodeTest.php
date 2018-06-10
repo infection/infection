f8d23f384380aa6d65cdbbec80854c95ec9f13c6
Gert de Pagter <gertp93@gmail.com>
2018-06-10 22:27:11 +0200

    Add tests for Subscriber Builder

--- tests/AutoReview/ProjectCodeTest.php
+++ tests/AutoReview/ProjectCodeTest.php
@@ -31,7 +31,6 @@ use Infection\Logger\TextFileLogger;
 use Infection\Mutant\MetricsCalculator;
 use Infection\Mutator\Util\Mutator;
 use Infection\Process\Builder\ProcessBuilder;
-use Infection\Process\Builder\SubscriberBuilder;
 use Infection\Process\Listener\MutantCreatingConsoleLoggerSubscriber;
 use Infection\Process\Listener\MutationGeneratingConsoleLoggerSubscriber;
 use Infection\Process\Runner\MutationTestingRunner;
@@ -120,7 +119,6 @@ final class ProjectCodeTest extends TestCase
         ResultsLoggerTypes::class,
         SummaryFileLogger::class,
         TextFileLogger::class,
-        SubscriberBuilder::class,
         MutantCreatingConsoleLoggerSubscriber::class,
         MutationGeneratingConsoleLoggerSubscriber::class,
         MutationTestingRunner::class,
