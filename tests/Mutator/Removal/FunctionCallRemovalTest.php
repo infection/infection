56c29bd31e50b29acf6692e9ef558e4b21e7ae77
Gert de Pagter <BackEndTea@users.noreply.github.com>
2018-08-04 14:19:17 +0200

    Add function and method removal mutators (#418)
    * Add function and method removal mutators
    
    * Rename new mutators to *CallRemoval
    
    * Fix min msi
    
    * Fix e2e and remove parenthesis

---
+++ tests/Mutator/Removal/FunctionCallRemovalTest.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Copyright © 2017-2018 Maks Rafalko
+ *
+ * License: https://opensource.org/licenses/BSD-3-Clause New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Infection\Tests\Mutator\Removal;
+
+use Infection\Tests\Mutator\AbstractMutatorTestCase;
+
+/**
+ * @internal
+ */
+final class FunctionCallRemovalTest extends AbstractMutatorTestCase
+{
+    /**
+     * @dataProvider provideMutationCases
+     */
+    public function test_mutator($input, $expected = null): void
+    {
+        $this->doTest($input, $expected);
+    }
+
+    public function provideMutationCases(): \Generator
+    {
+        yield 'It removes a function call' => [
+            <<<'PHP'
+<?php
+
+foo();
+bar(3, 4);
+$a = 3;
+PHP
+            ,
+            <<<'PHP'
+<?php
+
+$a = 3;
+PHP
+            ,
+        ];
+
+        yield 'It does not remove a function call that is assigned to something' => [
+            <<<'PHP'
+<?php
+
+$b = foo();
+$a = 3;
+PHP
+            ,
+        ];
+
+        yield 'It does not remove a function call within a statement' => [
+            <<<'PHP'
+<?php
+
+if (foo()) {
+    $a = 3;
+}
+while (foo()) {
+    $a = 3;
+}
+
+PHP
+            ,
+        ];
+
+        yield 'It does not remove a function call that is the parameter of another function or method' => [
+            <<<'PHP'
+<?php
+
+$a = foo(3, bar());
+PHP
+        ];
+
+        yield 'It does not remove a method call' => [
+            <<<'PHP'
+<?php
+
+$this->foo();
+$a = 3;
+PHP
+        ];
+    }
+}
