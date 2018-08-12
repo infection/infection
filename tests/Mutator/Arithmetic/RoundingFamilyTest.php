e2d74da3debd332ad096d1ce7b58f98179db0351
Marco Aurélio Deleu <deleugyn@gmail.com>
2018-08-12 17:43:22 +0200

    [Feature] Round Family (#449)
    * Include more test-cases
    
    * Add arithmetic operation test for rounding family

--- tests/Mutator/Arithmetic/RoundingFamilyTest.php
+++ tests/Mutator/Arithmetic/RoundingFamilyTest.php
@@ -30,20 +30,20 @@ final class RoundingFamilyTest extends AbstractMutatorTestCase
             <<<'PHP'
 <?php
 
-$a = round(1.23);
+$var = round(1.23);
 PHP
             ,
             [
                 <<<'PHP'
 <?php
 
-$a = floor(1.23);
+$var = floor(1.23);
 PHP
             ,
             <<<'PHP'
 <?php
 
-$a = ceil(1.23);
+$var = ceil(1.23);
 PHP
             ],
         ];
@@ -52,20 +52,20 @@ PHP
             <<<'PHP'
 <?php
 
-$a = floor(1.23);
+$var = floor(1.23);
 PHP
             ,
             [
                 <<<'PHP'
 <?php
 
-$a = ceil(1.23);
+$var = ceil(1.23);
 PHP
                 ,
                 <<<'PHP'
 <?php
 
-$a = round(1.23);
+$var = round(1.23);
 PHP
             ],
         ];
@@ -74,20 +74,20 @@ PHP
             <<<'PHP'
 <?php
 
-$a = ceil(1.23);
+$var = ceil(1.23);
 PHP
             ,
             [
                 <<<'PHP'
 <?php
 
-$a = floor(1.23);
+$var = floor(1.23);
 PHP
                 ,
                 <<<'PHP'
 <?php
 
-$a = round(1.23);
+$var = round(1.23);
 PHP
             ],
         ];
@@ -96,20 +96,20 @@ PHP
             <<<'PHP'
 <?php
 
-$a = CeIl(1.23);
+$var = CeIl(1.23);
 PHP
             ,
             [
                 <<<'PHP'
 <?php
 
-$a = floor(1.23);
+$var = floor(1.23);
 PHP
                 ,
                 <<<'PHP'
 <?php
 
-$a = round(1.23);
+$var = round(1.23);
 PHP
             ],
         ];
@@ -127,20 +127,121 @@ PHP
             <<<'PHP'
 <?php
 
-$a = round(1.23, 2, PHP_ROUND_HALF_UP);
+$var = round(1.23, 2, PHP_ROUND_HALF_UP);
 PHP
             ,
             [
                 <<<'PHP'
 <?php
 
-$a = floor(1.23);
+$var = floor(1.23);
 PHP
                 ,
                 <<<'PHP'
 <?php
 
-$a = ceil(1.23);
+$var = ceil(1.23);
+PHP
+            ],
+        ];
+
+        yield 'It does not mutate other functions' => [
+            <<<'PHP'
+<?php
+ strtolower('lower');
+PHP
+        ];
+
+        yield 'It mutates \ceil() to round() and floor()' => [
+            <<<'PHP'
+<?php
+
+$float = 1.23;
+return \ceil($float);
+PHP
+            ,
+            [
+                <<<'PHP'
+<?php
+
+$float = 1.23;
+return floor($float);
+PHP
+                ,
+                <<<'PHP'
+<?php
+
+$float = 1.23;
+return round($float);
+PHP
+            ],
+        ];
+
+        yield 'It mutates \floor() to round() and ceil() in a control flow statement' => [
+            <<<'PHP'
+<?php
+
+while (\floor(1.23)) {
+}
+PHP
+            ,
+            [
+                <<<'PHP'
+<?php
+
+while (ceil(1.23)) {
+}
+PHP
+                ,
+                <<<'PHP'
+<?php
+
+while (round(1.23)) {
+}
+PHP
+            ],
+        ];
+
+        yield 'It mutates ceil() to round() and floor() while assigning inside the function call' => [
+            <<<'PHP'
+<?php
+
+echo ceil($result = $this->average());
+PHP
+            ,
+            [
+                <<<'PHP'
+<?php
+
+echo floor($result = $this->average());
+PHP
+                ,
+                <<<'PHP'
+<?php
+
+echo round($result = $this->average());
+PHP
+            ],
+        ];
+
+        yield 'It mutates round() to ceil() and floor() during arithmetic operations' => [
+            <<<'PHP'
+<?php
+
+return round($this->positive / $this->total);
+PHP
+            ,
+            [
+                <<<'PHP'
+<?php
+
+return floor($this->positive / $this->total);
+PHP
+                ,
+                <<<'PHP'
+<?php
+
+return ceil($this->positive / $this->total);
 PHP
             ],
         ];
