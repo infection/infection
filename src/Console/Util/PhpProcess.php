7fe54aaa88c5c3ffd45610b8f85506f2df50807e
johnstevenson <john-stevenson@blueyonder.co.uk>
2018-09-04 11:54:29 +0100

    Improve doc comment

--- src/Console/Util/PhpProcess.php
+++ src/Console/Util/PhpProcess.php
@@ -26,6 +26,10 @@ final class PhpProcess extends Process
      * This method allows a sub-process to run with xdebug enabled (if it was
      * originally loaded), then restores the xdebug-free environment.
      *
+     * This means that we can use xdebug when it is required and not have to
+     * worry about it for the bulk of other processes, which do not need it and
+     * work better without it.
+     *
      * {@inheritdoc}
      */
     public function start(callable $callback = null, array $env = null): void
