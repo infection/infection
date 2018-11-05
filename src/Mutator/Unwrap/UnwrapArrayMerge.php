e85f05f683202fe9b86bfe3145a6ff4ac91e0965
Andreas Möller <am@localheinz.com>
2018-11-05 05:44:44 +0100

    Fix: Use plural for method name as it returns a generator (#546)

--- src/Mutator/Unwrap/UnwrapArrayMerge.php
+++ src/Mutator/Unwrap/UnwrapArrayMerge.php
@@ -47,7 +47,7 @@ final class UnwrapArrayMerge extends AbstractUnwrapMutator
         return 'array_merge';
     }
 
-    protected function getParameterIndex(Node $node): \Generator
+    protected function getParameterIndexes(Node $node): \Generator
     {
         yield from array_keys($node->args);
     }
