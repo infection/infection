a354c5d5865d9dfe2f58a5ef2ea9a9f2358c83a6
Maks Rafalko <b0rn@list.ru>
2018-11-13 07:22:01 +0300

    Build a correct absolute path to XSD schema when `phpunit.xml` is located in a subfolder (#567)
    * Respect @sidz
    
    * Build a correct absolute path to XSD schema when `phpunit.xml` is located in a subfolder. Fixes https://github.com/infection/infection/issues/566

---
+++ tests/Fixtures/e2e/PHPUnit_XSD_Validation/tests/SourceClassTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PHPUnit_XSD_Validation\Test;
+
+use PHPUnit_XSD_Validation\SourceClass;
+use PHPUnit\Framework\TestCase;
+
+class SourceClassTest extends TestCase
+{
+    public function test_hello()
+    {
+        $sourceClass = new SourceClass();
+        $this->assertSame('hello', $sourceClass->hello());
+    }
+}
