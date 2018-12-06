c2a9a03c816e26ed8daf8ba7817397962f2c8567
Oleg Zhulnev <plbsid@gmail.com>
2018-12-06 21:50:45 +0300

    Revert e2e test with fix

---
+++ tests/Fixtures/e2e/Initial_Configuration/run_tests.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if test ! -f "$(which expect)"; then
+    test -x $(which tput) && tput setaf 1 # red
+    echo "Please install expect; it is readily available from apt and brew"
+    exit 1;
+fi
+
+cd $(dirname $0)
+rm -f infection.json.dist
+
+set -e
+
+if [ "$PHPDBG" = "1" ]
+then
+    INFECTION="phpdbg -qrr ../../../../bin/infection"
+else
+    INFECTION="php ../../../../bin/infection"
+fi
+export INFECTION
+
+./do_configure.expect
+
+test -f infection.json.dist
+diff -u expected.log infection.log
---
+++ tests/Fixtures/e2e/Initial_Configuration/skipped_src/IgnoredSourceClass.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infection\Tests\Fixtures\e2e\Initial_Configuration\skipped_src;
+
+class IgnoredSourceClass
+{
+    public function doSmth()
+    {
+        return 10;
+    }
+}
