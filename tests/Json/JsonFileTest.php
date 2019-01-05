7894cf5f6030c3c499004d57f7a29db52aeb2af8
borNfreee <b0rn@list.ru>
2019-01-05 11:52:21 +0300

    Update License

--- tests/Json/JsonFileTest.php
+++ tests/Json/JsonFileTest.php
@@ -2,7 +2,7 @@
 /**
  * This code is licensed under the BSD 3-Clause License.
  *
- * Copyright (c) 2017-2018, Maks Rafalko
+ * Copyright (c) 2017-2019, Maks Rafalko
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
