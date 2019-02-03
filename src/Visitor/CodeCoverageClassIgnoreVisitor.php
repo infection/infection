e6d79194e6854a05889edf54e04722c4dafd6d9c
Gert de Pagter <BackEndTea@gmail.com>
2019-02-03 11:32:55 +0100

    Use node doc comment on cod coverage class ignore as well
    Overall this looks cleaner as well

--- src/Visitor/CodeCoverageClassIgnoreVisitor.php
+++ src/Visitor/CodeCoverageClassIgnoreVisitor.php
@@ -46,31 +46,21 @@ use PhpParser\NodeVisitorAbstract;
  */
 final class CodeCoverageClassIgnoreVisitor extends NodeVisitorAbstract
 {
-    private $namespace;
-
     public function enterNode(Node $node)
     {
-        if ($node instanceof Stmt\Namespace_) {
-            $this->namespace = $node->name;
-        } elseif ($node instanceof Stmt\ClassLike) {
-            if (!$node->name) {
-                return null;
-            }
-
-            /** @var Name $fullyQualifiedClassName */
-            $fullyQualifiedClassName = Name::concat($this->namespace, $node->name->name);
-
-            $reflectionClass = new \ReflectionClass($fullyQualifiedClassName->toString());
-
-            $docComment = $reflectionClass->getDocComment();
-
-            if ($docComment === false) {
-                return null;
-            }
-
-            if (strpos($docComment, '@codeCoverageIgnore') !== false) {
-                return NodeTraverser::DONT_TRAVERSE_CURRENT_AND_CHILDREN;
-            }
+        if (!$node instanceof Stmt\ClassLike) {
+            return null;
         }
+
+        $docComment = $node->getDocComment();
+
+        if ($docComment === null) {
+            return null;
+        }
+
+        if (strpos($docComment->getText(), '@codeCoverageIgnore') !== false) {
+            return NodeTraverser::DONT_TRAVERSE_CURRENT_AND_CHILDREN;
+        }
+
     }
 }
