c9bdd883aa11e19179f1928df3c308d38c7282f9
Théo FIDRY <theo.fidry@gmail.com>
2019-06-28 01:45:36 +0200

    Reset Empty_Path e2e tests

--- tests/Fixtures/e2e/Empty_Path/run_tests.bash
+++ tests/Fixtures/e2e/Empty_Path/run_tests.bash
@@ -2,18 +2,13 @@
 
 set -e pipefail
 
-cd $(dirname "$0")
-
-readonly INFECTION="./../../../../bin/infection"
-readonly PHP=$(command -v php)
-
-export PATH=
+readonly INFECTION="./bootstrap"
 
 if [ "$PHPDBG" = "1" ]
 then
     $(which phpdbg) -qrr $INFECTION
 else
-    $PHP $INFECTION
+    $(which php) $INFECTION
 fi
 
 diff expected-output.txt infection.log
