infection/infection

commit 8b5fe199e222eff7a0fd5960815c91f825b0489f

Update the license (#720)
- Update the license your to the start year `2017` to avoid to have to update the end year all the time
- Update manually the license header in `bin/infection` and `bin/infection-debug` as PHP-CS-Fixer seemed unable to handle those two files for some reasons
- Apply the CS fixer

Makefile
@@ -67,7 +67,7 @@ validate:
 .PHONY: test
 test:		## Runs all the tests
 # TODO: add a test to ensure we are not missing targets here
-test: test-unit test-e2e test-infection
+test: test-unit test- test-infection
 
 .PHONY: test-unit
 test-unit:	## Runs the unit tests

commit 420240b09bd2282ed83b54429a51ac37fe992acf

Fix the gitignore and Makefile (#728)
- Add missing entries to `.gitignore`
- Fix the mistake introduced by https://github.com/infection/infection/commit/8b5fe199e222eff7a0fd5960815c91f825b0489f#diff-b67911656ef5d18c4ae36cb6741b7965L70

.gitignore
@@ -8,11 +8,13 @@
 /.idea
 /.php_cs.cache
 /.tools/
-/build/*
-/build/bin/infection.phar
-/build/bin/infection.phar.pubkey
+/build/
 /devTools/Docker*.json
 /infection-cache
 /infection.json
 /infection.log
-/vendor
+/tests/Fixtures/e2e/*/coverage/
+/tests/Fixtures/e2e/*/infection/
+/tests/Fixtures/e2e/Configure/infection.json.dist
+/tests/Fixtures/e2e/SymfonyFlex/bin/.phpunit/
+/vendor/

Makefile
@@ -67,7 +67,7 @@ validate:
 .PHONY: test
 test:		## Runs all the tests
 # TODO: add a test to ensure we are not missing targets here
-test: test-unit test- test-infection
+test: test-unit test-e2e test-infection
 
 .PHONY: test-unit
 test-unit:	## Runs the unit tests