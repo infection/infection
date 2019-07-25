4caac9376a7f7bbb2070fa4316aaf9ed297ddc84
Théo FIDRY <theo.fidry@gmail.com>
2019-07-25 19:16:30 +0200

    Rework ProjectCodeTest (#753)
    As per an internal discussion, since the test code is not public from a library consumer PoV, it is unnecessary to mark it as `@internal`.
    
    Before actually removing the `@internal` tag (which will be done in a separate PR for clarity), I took the opportunity to refactor a bit the `ProjectCodeTest`:
    
    - I extracted the providers into a dedicated class in order to ease its testing, which makes `ProjectCodeTest` slimmer and less confusing (as the provider tests no longer need to be there)
    - I introduced a convention `provideX(): Generator` for a regular generator and `xProvider()` for a PHPUnit provider
    - a new utility function `generator_to_phpunit_data_provider` has been introduced to facilitate the transformation of a regular provider to a PHPUnit data provider
    - Remove the dependency on statically declared variables for the providers
    - All the code has been reviewed to:
      - Import the functions when necessary including switching to `Safe\*` functions when appropriate
      - Review all the error messages

--- tests/AutoReview/ProjectCode/DocBlockParser.php
+++ tests/AutoReview/ProjectCode/DocBlockParser.php
@@ -33,23 +33,56 @@
 
 declare(strict_types=1);
 
-namespace Infection\Tests;
+namespace Infection\Tests\AutoReview\ProjectCode;
 
-use Generator;
+use function array_filter;
+use function array_map;
+use function current;
+use function end;
+use function explode;
+use function implode;
+use function substr;
+use function trim;
 
-/**
- * @internal
- *
- * Normalizes path. Replaces backslashes with forward ones
- */
-function normalizePath(string $value): string
+final class DocBlockParser
 {
-    return str_replace(\DIRECTORY_SEPARATOR, '/', $value);
-}
+    public static function parse(string $docblock): string
+    {
+        $docblock = trim($docblock);
 
-function generator_to_phpunit_data_provider(iterable $source): Generator
-{
-    foreach ($source as $key => $value) {
-        yield $key => [$value];
+        if ('' === $docblock) {
+            return '';
+        }
+
+        /** @var string[] $lines */
+        $lines = array_map(
+            'trim',
+            explode("\n", $docblock)
+        );
+
+        /** @var string $firstLine */
+        $firstLine = current($lines);
+
+        $lines[0] = substr($firstLine, 3);
+
+        $nbrOfLines = \count($lines);
+
+        for ($i = 1; $i < $nbrOfLines; ++$i) {
+            $lines[$i] = substr($lines[$i], 1);
+        }
+
+        end($lines);
+
+        /** @var string $lastLine */
+        $lastLine = current($lines);
+
+        $lines[$nbrOfLines - 1] = substr($lastLine, 0, -2);
+
+        return implode(
+            "\n",
+            array_filter(
+                array_map('trim', $lines)
+            )
+        );
     }
 }
--- tests/AutoReview/ProjectCode/DocBlockParserTest.php
+++ tests/AutoReview/ProjectCode/DocBlockParserTest.php
@@ -33,23 +33,85 @@
 
 declare(strict_types=1);
 
-namespace Infection\Tests;
+namespace Infection\Tests\AutoReview\ProjectCode;
 
 use Generator;
+use PHPUnit\Framework\TestCase;
 
+final class DocBlockParserTest extends TestCase
+{
+    /**
+     * @dataProvider docBlocksProvider
+     */
+    public function test_it_can_parse_PHP_doc_blocks(string $docBlock, string $expected): void
+    {
+        $actual = DocBlockParser::parse($docBlock);
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function docBlocksProvider(): Generator
+    {
+        yield ['', ''];
+
+        yield [
+            <<<'PHP'
 /**
- * @internal
- *
- * Normalizes path. Replaces backslashes with forward ones
+ * This is a
+ * multi-line
+ * doc-block
  */
-function normalizePath(string $value): string
-{
-    return str_replace(\DIRECTORY_SEPARATOR, '/', $value);
-}
+PHP
+            ,
+            <<<'TEXT'
+This is a
+multi-line
+doc-block
+TEXT
+        ];
 
-function generator_to_phpunit_data_provider(iterable $source): Generator
-{
-    foreach ($source as $key => $value) {
-        yield $key => [$value];
+        yield [
+            <<<'PHP'
+/**
+ * Single line doc-block
+ */
+PHP
+            ,
+            'Single line doc-block',
+        ];
+
+        yield [
+            <<<'PHP'
+   /**
+ * This is a
+ * multi-line  
+   * doc-block
+ * with weird indentation
+    */
+PHP
+            ,
+            <<<'TEXT'
+This is a
+multi-line
+doc-block
+with weird indentation
+TEXT
+        ];
+
+        yield [
+            <<<'PHP'
+/** Inlined doc-block */
+PHP
+            ,
+            'Inlined doc-block',
+        ];
+
+        yield [
+            <<<'PHP'
+// Comment
+PHP
+            ,
+            'Comme', // Weird result: regular comments are not properly supported
+        ];
     }
 }
