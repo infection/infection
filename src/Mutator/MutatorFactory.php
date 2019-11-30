3652559a9de20954ea0e730c1de285d4c0ac485a
Théo FIDRY <theo.fidry@gmail.com>
2019-11-30 15:23:11 +0100

    Introduce the MutatorFactory, meant as a replacement for MutatorsGenerator
    - Unlike MutatorsGenerator is stateless so can be used as an injected service
    - Does not handle a default value -> should be handled at the factory level
    - Does not have a test yet
    - Does not throw RuntimeErrors
    - Completely remove the "unset" mutators/profiles
    - Process the profiles _first_
    
    TODO:
    
    - test
    - make use of it
    - should all false profiles be handled as last instead? To prevent the order to matter

--- src/Mutator/MutatorFactory.php
+++ src/Mutator/MutatorFactory.php
@@ -1,51 +1,18 @@
 <?php
-/**
- * This code is licensed under the BSD 3-Clause License.
- *
- * Copyright (c) 2017, Maks Rafalko
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * * Neither the name of the copyright holder nor the names of its
- *   contributors may be used to endorse or promote products derived from
- *   this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
 declare(strict_types=1);
 
 namespace Infection\Mutator;
 
-use function array_key_exists;
-use function class_exists;
 use Infection\Mutator\Util\Mutator;
 use Infection\Mutator\Util\MutatorConfig;
+use Infection\Mutator\Util\MutatorProfile;
 use InvalidArgumentException;
-use function sprintf;
 use stdClass;
+use function array_key_exists;
+use function class_exists;
+use function sprintf;
 
-/**
- * @internal
- */
 final class MutatorFactory
 {
     /**
@@ -55,63 +22,51 @@ final class MutatorFactory
      */
     public function create(array $mutatorSettings): array
     {
-        return self::createFromNames(
-            self::retrieveMutatorNames($mutatorSettings)
-        );
-    }
-
-    /**
-     * @param array<string, bool|array<string, string>> $mutatorSettings
-     *
-     * @return array<string, array<string, string>>
-     */
-    private static function retrieveMutatorNames(array $mutatorSettings): array
-    {
         $mutators = [];
 
+        // First parse the profiles
         foreach ($mutatorSettings as $mutatorOrProfile => $setting) {
-            if (array_key_exists($mutatorOrProfile, ProfileList::ALL_PROFILES)) {
-                self::registerFromProfile($mutatorOrProfile, $setting, $mutators);
-
+            if (!array_key_exists($mutatorOrProfile, MutatorProfile::MUTATOR_PROFILE_LIST)) {
                 continue;
             }
 
-            if (array_key_exists($mutatorOrProfile, ProfileList::ALL_MUTATORS)) {
-                self::registerFromName($mutatorOrProfile, $setting, $mutators);
-
-                continue;
-            }
-
-            throw new InvalidArgumentException(sprintf(
-                'The profile or mutator "%s" was not recognized.',
-                $mutatorOrProfile
-            ));
+            $this->registerFromProfile($mutatorOrProfile, $setting, $mutators);
         }
 
-        return $mutators;
+        // First parse the profiles
+        foreach ($mutatorSettings as $mutatorOrProfile => $setting) {
+            if (!array_key_exists($mutatorOrProfile, MutatorProfile::FULL_MUTATOR_LIST)) {
+                continue;
+            }
+
+            $this->registerFromName($mutatorOrProfile, $setting, $mutators);
+        }
+
+        return $this->createFromNames($mutators);
     }
 
     /**
      * @param array<string, string>|bool           $settings
      * @param array<string, array<string, string>> $mutators
      */
-    private static function registerFromProfile(
+    private function registerFromProfile(
         string $profile,
         $settings,
         array &$mutators
-    ): void {
-        foreach (ProfileList::ALL_PROFILES[$profile] as $mutatorOrProfile) {
+    ): void
+    {
+        foreach (MutatorProfile::MUTATOR_PROFILE_LIST[$profile] as $mutatorOrProfile) {
             /** @var string $mutatorOrProfile */
 
             // A profile may refer to another collection of profiles
-            if (array_key_exists($mutatorOrProfile, ProfileList::ALL_PROFILES)) {
-                self::registerFromProfile($mutatorOrProfile, $settings, $mutators);
+            if (array_key_exists($mutatorOrProfile, MutatorProfile::MUTATOR_PROFILE_LIST)) {
+                $this->registerFromProfile($mutatorOrProfile, $settings, $mutators);
 
                 continue;
             }
 
-            if (class_exists($mutatorOrProfile, true)) {
-                self::registerFromClass($mutatorOrProfile, $settings, $mutators);
+            if (class_exists($mutatorOrProfile)) {
+                $this->registerFromClass($mutatorOrProfile, $settings, $mutators);
 
                 continue;
             }
@@ -128,20 +83,21 @@ final class MutatorFactory
      * @param array<string, string>|bool           $settings
      * @param array<string, array<string, string>> $mutators
      */
-    private static function registerFromName(
+    private function registerFromName(
         string $mutator,
         $settings,
         array &$mutators
-    ): void {
-        if (!array_key_exists($mutator, ProfileList::ALL_MUTATORS)) {
+    ): void
+    {
+        if (!array_key_exists($mutator, MutatorProfile::FULL_MUTATOR_LIST)) {
             throw new InvalidArgumentException(sprintf(
                 'The "%s" mutator/profile was not recognized.',
                 $mutator
             ));
         }
 
-        self::registerFromClass(
-            ProfileList::ALL_MUTATORS[$mutator],
+        $this->registerFromClass(
+            MutatorProfile::FULL_MUTATOR_LIST[$mutator],
             $settings,
             $mutators
         );
@@ -151,11 +107,12 @@ final class MutatorFactory
      * @param array<string, string>|bool|stdClass  $settings
      * @param array<string, array<string, string>> $mutators
      */
-    private static function registerFromClass(
+    private function registerFromClass(
         string $mutator,
         $settings,
         array &$mutators
-    ): void {
+    ): void
+    {
         if ($settings === false) {
             unset($mutators[$mutator]);
         } else {
@@ -168,14 +125,14 @@ final class MutatorFactory
      *
      * @return array<string, Mutator>
      */
-    private static function createFromNames(array $mutatorNames): array
+    private function createFromNames(array $mutatorNames): array
     {
         $mutators = [];
 
-        foreach ($mutatorNames as $mutatorClass => $settings) {
-            /** @var Mutator $mutator */
-            $mutator = new $mutatorClass(new MutatorConfig($settings));
-            $mutators[$mutator::getName()] = $mutator;
+        foreach ($mutatorNames as $mutator => $settings) {
+            $mutators[$mutator::getName()] = new $mutator(
+                new MutatorConfig($settings)
+            );
         }
 
         return $mutators;
