07ed0255482b2fb25cfbe3d636719fa71daa6b1d
Théo FIDRY <theo.fidry@gmail.com>
2019-12-08 21:05:47 +0100

    Revert directory: it was used

---
+++ tests/phpunit/Fixtures/Files/Finder/SecondFile.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © 2017-2018 Maks Rafalko
+ *
+ * License: https://opensource.org/licenses/BSD-3-Clause New BSD License
+ */
+
+declare(strict_types=1);
+
+namespace Infection\Tests\Fixtures\Files\Finder;
+
+class SecondFile
+{
+}
