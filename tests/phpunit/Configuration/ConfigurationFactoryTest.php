1a2714275dd8c9694ba513cab9ff89b4e3169a8a
Théo FIDRY <theo.fidry@gmail.com>
2019-12-23 14:18:18 +0100

    Rework AssignmentEqual
    - Rename `AssignmentEqual` to `EqualOrIdenticalToAssignment`
    - Change identical besides equal to an assignment

--- tests/phpunit/Configuration/ConfigurationFactoryTest.php
+++ tests/phpunit/Configuration/ConfigurationFactoryTest.php
@@ -44,7 +44,7 @@ use Infection\Configuration\Entry\Source;
 use Infection\Configuration\Schema\SchemaConfiguration;
 use Infection\FileSystem\SourceFileCollector;
 use Infection\FileSystem\TmpDirProvider;
-use Infection\Mutator\Arithmetic\AssignmentEqual;
+use Infection\Mutator\Arithmetic\EqualOrIdentical;
 use Infection\Mutator\Boolean\EqualIdentical;
 use Infection\Mutator\Boolean\TrueValue;
 use Infection\Mutator\MutatorFactory;
@@ -474,7 +474,7 @@ final class ConfigurationFactoryTest extends TestCase
             ],
             'AssignmentEqual,EqualIdentical',
             [
-                'AssignmentEqual' => new AssignmentEqual(new MutatorConfig([])),
+                'AssignmentEqual' => new EqualOrIdentical(new MutatorConfig([])),
                 'EqualIdentical' => new EqualIdentical(new MutatorConfig([])),
             ]
         );
