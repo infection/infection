1eb101608fb7809bde7064c1f7a49ed06e170dfe
Théo FIDRY <theo.fidry@gmail.com>
2019-12-30 09:53:33 +0100

    Cleanup

--- src/Mutator/Arithmetic/EqualOrIdenticalAssignment.php
+++ src/Mutator/Arithmetic/EqualOrIdenticalAssignment.php
@@ -44,8 +44,9 @@ use PhpParser\Node;
 final class EqualOrIdenticalAssignment extends Mutator
 {
     /**
-     * Replaces a equal or identical (respectively "==" and "===") comparison into an assignment
-     * ("=).
+     * Replaces examples of augmented or compound (shorter way to apply an arithmetic or bitwise
+     * operation) assignment operators, i.e. "+=", "*=", ".=", etc. By a plain assignment operator
+     * "=".
      *
      * @param Node&Node\Expr\BinaryOp\Equal $node
      *
