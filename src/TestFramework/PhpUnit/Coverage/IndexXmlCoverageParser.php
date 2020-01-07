08d0409175f5271353edddbb12aec2248cc7ce50
Théo FIDRY <theo.fidry@gmail.com>
2020-01-07 21:58:46 +0100

    Fix typo

--- src/TestFramework/PhpUnit/Coverage/IndexXmlCoverageParser.php
+++ src/TestFramework/PhpUnit/Coverage/IndexXmlCoverageParser.php
@@ -156,7 +156,7 @@ class IndexXmlCoverageParser
         $percentage = $linesNode->getAttribute('percent');
 
         if (substr($percentage, -1) === '%') {
-            // In PHPUnit <6 the percentage value would tak ethe form "0.00%" in _some_ cases.
+            // In PHPUnit <6 the percentage value would take the form "0.00%" in _some_ cases.
             // For example could find both with percentage and without in
             // https://github.com/maks-rafalko/tactician-domain-events/tree/1eb23434d3a833dedb6180ead75ff983ef09a2e9
             $percentage = substr($percentage, 0, -1);
