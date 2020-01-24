46b10b365e2ab02f30b5b62bd2888a836b69d301
Théo FIDRY <theo.fidry@gmail.com>
2020-01-24 10:28:47 +0100

    Fix locator's data providers

--- tests/phpunit/FileSystem/Locator/RootsFileLocatorTest.php
+++ tests/phpunit/FileSystem/Locator/RootsFileLocatorTest.php
@@ -51,7 +51,7 @@ use Symfony\Component\Filesystem\Filesystem;
  */
 final class RootsFileLocatorTest extends TestCase
 {
-    private const FIXTURES_DIR = __DIR__ . '/../Fixtures/Locator';
+    private const FIXTURES_DIR = __DIR__ . '/../../Fixtures/Locator';
 
     /**
      * @var Filesystem
