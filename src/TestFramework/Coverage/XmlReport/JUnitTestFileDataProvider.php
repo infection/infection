a0cf06ac6a3358ff2e52183eadd5d474f47dfbb9
Gregory Heitz <edno@edno.io>
2020-02-05 23:10:40 +0100

    Coding style fixes from pretty

--- src/TestFramework/Coverage/XmlReport/JUnitTestFileDataProvider.php
+++ src/TestFramework/Coverage/XmlReport/JUnitTestFileDataProvider.php
@@ -81,10 +81,11 @@ final class JUnitTestFileDataProvider implements TestFileDataProvider
             );
         }
 
-        $feature = preg_replace("/^(.*):+.*$/", "$1.feature", $fullyQualifiedClassName);
+        $feature = preg_replace('/^(.*):+.*$/', '$1.feature', $fullyQualifiedClassName);
+
         if ($nodes->length === 0) {
-          // try another format where the class name is inside `file` attribute of `testcase` tag
-          $nodes = $xPath->query(sprintf('//testcase[contains(@file, "%s")]', $feature));
+            // try another format where the class name is inside `file` attribute of `testcase` tag
+            $nodes = $xPath->query(sprintf('//testcase[contains(@file, "%s")]', $feature));
         }
 
         if ($nodes->length === 0) {
