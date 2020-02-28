5200c539f46233a05e48e0e951f76531053e2cdf
Alexey Kopytko <alexey@kopytko.com>
2020-02-28 21:22:59 +0900

    Update ParallelProcessRunner.php

--- src/Process/Runner/Parallel/ParallelProcessRunner.php
+++ src/Process/Runner/Parallel/ParallelProcessRunner.php
@@ -160,7 +160,7 @@ class ParallelProcessRunner
      */
     private static function fillBucket(array &$bucket, Generator $input, int $level = self::MUTATOR_TO_PROCESS_RATIO): void
     {
-        if (count($bucket) === $level) {
+        if (count($bucket) >= $level) {
             return;
         }
 
