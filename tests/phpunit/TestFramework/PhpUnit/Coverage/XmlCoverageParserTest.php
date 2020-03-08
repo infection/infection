08cce88dcc5eb695c9ca0c0df09823efea588459
Alexey Kopytko <alexey@kopytko.com>
2020-03-09 04:56:56 +0900

    Update XmlCoverageParserTest

--- tests/phpunit/TestFramework/PhpUnit/Coverage/XmlCoverageParserTest.php
+++ tests/phpunit/TestFramework/PhpUnit/Coverage/XmlCoverageParserTest.php
@@ -35,10 +35,13 @@ declare(strict_types=1);
 
 namespace Infection\Tests\TestFramework\PhpUnit\Coverage;
 
+use Infection\TestFramework\Coverage\CoverageFileData;
 use Infection\TestFramework\PhpUnit\Coverage\SourceFileInfoProvider;
 use Infection\TestFramework\PhpUnit\Coverage\XmlCoverageParser;
+use Infection\TestFramework\PhpUnit\Coverage\XPathFactory;
 use Infection\Tests\TestFramework\Coverage\CoverageHelper;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Finder\SplFileInfo;
 
 /**
  * @group integration
@@ -66,7 +69,7 @@ final class XmlCoverageParserTest extends TestCase
      *
      * @param array<string, mixed> $expectedCoverage
      */
-    public function test_it_reads_every_type_of_data(
+    public function test_it_reads_every_type_of_fixture(
         SourceFileInfoProvider $provider,
         array $expectedCoverage,
         string $sourceFilePath
@@ -82,5 +85,50 @@ final class XmlCoverageParserTest extends TestCase
             $expectedCoverage,
             CoverageHelper::convertToArray([$coverageData])[0]
         );
+    }
+
+    public function test_it_reads_report_with_no_covered_lines(): void
+    {
+        $xml = <<<'XML'
+<?xml version="1.0"?>
+<phpunit xmlns="http://schema.phpunit.de/coverage/1.0">
+    <file name="secondLevel.php" path="/FirstLevel/SecondLevel">
+        <totals>
+            <lines total="1" comments="0" code="1" executable="1" executed="1" percent="100"/>
+        </totals>
+        <coverage>
+        </coverage>
+    </file>
+</phpunit>
+XML;
+
+        $coverageData = $this->parseXml($xml);
+
+        $this->assertSame([], $coverageData->byLine);
+        $this->assertSame([], $coverageData->byMethod);
+    }
+
+    private function parseXml(string $xml): CoverageFileData
+    {
+        $xPath = XPathFactory::createXPath($xml);
+
+        $this->assertSame(0, $xPath->query('/phpunit/file/coverage/line')->length);
+
+        $providerMock = $this->createMock(SourceFileInfoProvider::class);
+
+        $providerMock
+            ->expects($this->once())
+            ->method('provideFileInfo')
+            ->willReturn($this->createMock(SplFileInfo::class));
+
+        $providerMock
+            ->expects($this->once())
+            ->method('provideXPath')
+            ->willReturn($xPath);
+
+        $parser = new XmlCoverageParser($providerMock);
+        $fileData = $parser->parse();
+
+        return $fileData->retrieveCoverageFileData();
     }
 }
