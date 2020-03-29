f014090d3e89fe00dc59a16a08fb7a831ea58c03
Théo FIDRY <theo.fidry@gmail.com>
2020-03-29 13:00:03 +0200

    Move node manipulation in a dedicated class

---
+++ tests/phpunit/PhpParser/Visitor/ParentConnectorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infection\Tests\PhpParser\Visitor;
+
+use Infection\PhpParser\Visitor\ParentConnector;
+use PHPUnit\Framework\TestCase;
+
+final class ParentConnectorTest extends TestCase
+{
+
+}
