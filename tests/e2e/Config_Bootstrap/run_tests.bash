9a1622473bb76930f7d535a4296c63d14fa0d5c5
Alexey Kopytko <alexey@kopytko.com>
2020-07-07 02:05:24 +0900

    Fix remaining E2E test

--- tests/e2e/Config_Bootstrap/run_tests.bash
+++ tests/e2e/Config_Bootstrap/run_tests.bash
@@ -13,5 +13,10 @@ else
     php $INFECTION
 fi
 
+if [[ -v GOLDEN ]]; then
+   cp -v infection.log expected-output.txt
+   cp -v infection-file.txt expected-file.txt
+fi
+
 diff -u expected-output.txt infection.log
 diff -u expected-file.txt infection-file.txt
