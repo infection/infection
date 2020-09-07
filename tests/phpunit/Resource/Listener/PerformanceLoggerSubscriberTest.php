2c4b105f74bd2c60c44f58c42ac9920ecad06a3d
Alexey Kopytko <alexey@kopytko.com>
2020-09-07 19:09:26 +0900

    Revert "DocBlock.patch"
    This reverts commit 1182aaed8e7ef76fb533d8f9280047e27dbf7d0c.
    
    Revert "Allow patches to fail"
    
    This reverts commit 34296f1e9da059c9ee0e7cc65fb1f499660b6493.
    
    Revert "Util_Test.patch"
    
    This reverts commit 2ed2423b8cf32986665245f10daf25805e89f469.
    
    Revert "Hot-patch CallCenter"
    
    This reverts commit 89651db3c1b9bc0d415f5213629fa787e48b8c51.
    
    Revert "Allow failures on PHP 8"
    
    This reverts commit 1c7bf8af14147310b4806862e8f068c397db46f9.
    
    Revert "And another"
    
    This reverts commit 4ee444edd31c07da80b100f0b1d85b1e56252e5f.
    
    Revert "Disable another test on PHP 8"
    
    This reverts commit 9fa5ffe22c87df3cee3b40186c680b77f2138b16.

--- tests/phpunit/Resource/Listener/PerformanceLoggerSubscriberTest.php
+++ tests/phpunit/Resource/Listener/PerformanceLoggerSubscriberTest.php
@@ -59,9 +59,6 @@ final class PerformanceLoggerSubscriberTest extends TestCase
         $this->output = $this->createMock(OutputInterface::class);
     }
 
-    /**
-     * @requires PHP < 8.0
-     */
     public function test_it_reacts_on_application_execution_events(): void
     {
         $this->output->expects($this->once())
