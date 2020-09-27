1c07150fbf33c59bec2fe02ad16ba57f00bb641b
Alexey Kopytko <alexey@kopytko.com>
2020-09-28 00:43:23 +0900

    Fix serveral E2E tests
    - Provide_Existing_Coverage should generate coverage on the fly
    - So should Skip_Initial_Tests
    - Unconfigured should start in a non-interactive mode

--- tests/e2e/Skip_Initial_Tests/tests/SourceClassTest.php
+++ tests/e2e/Skip_Initial_Tests/tests/SourceClassTest.php
@@ -9,7 +9,6 @@ class SourceClassTest extends TestCase
 {
     public function test_it_adds(): void
     {
-        file_put_contents(__DIR__ . '/../has_run', 'phpunit');
         $source = new SourceClass();
         $this->assertSame(3, $source->add(1, 2));
     }
