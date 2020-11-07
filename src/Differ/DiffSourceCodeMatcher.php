a8467ee63feb324a69849a3bd16a4d4dfa20b352
Alexey Kopytko <alexey@kopytko.com>
2020-11-07 19:56:17 +0900

    Use not included delimiter

--- src/Differ/DiffSourceCodeMatcher.php
+++ src/Differ/DiffSourceCodeMatcher.php
@@ -42,10 +42,29 @@ use function Safe\preg_match;
  */
 final class DiffSourceCodeMatcher
 {
+    private const POSSIBLE_DELIMITERS = [
+            '#', '%', ':', ';', '=', '?', '@', '^', '~',
+    ];
+
     public function matches(string $diff, string $sourceCodeRegex): bool
     {
-        $regexWithEscapedDelimiters = str_replace('/', '\/', $sourceCodeRegex);
+        // https://www.php.net/manual/en/regexp.reference.delimiters.php
 
-        return preg_match("/^-\s*{$regexWithEscapedDelimiters}$/mu", $diff) === 1;
+        $delimiter = $this->findDelimiter($sourceCodeRegex);
+        // There's no need to escape delimiters since we're assuming there's none.
+
+        return preg_match("{$delimiter}^-\s*{$sourceCodeRegex}\${$delimiter}mu", $diff) === 1;
+    }
+
+    private function findDelimiter(string $sourceCodeRegex): string
+    {
+        foreach (self::POSSIBLE_DELIMITERS as $possibleDelimiter) {
+            if (strpos($sourceCodeRegex, $possibleDelimiter) === false) {
+                return $possibleDelimiter;
+            }
+        }
+
+        // Let it fail naturally, for now
+        return $possibleDelimiter;
     }
 }
