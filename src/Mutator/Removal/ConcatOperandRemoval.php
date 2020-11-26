350c6164d2b3ab571b25240d0084620a94aa26cb
Rudolph Gottesheim <r.gottesheim@midnight-design.at>
2020-11-26 09:21:07 +0100

    Consistent nowdoc terminator

--- src/Mutator/Removal/ConcatOperandRemoval.php
+++ src/Mutator/Removal/ConcatOperandRemoval.php
@@ -53,7 +53,7 @@ final class ConcatOperandRemoval implements Mutator
     public static function getDefinition(): ?Definition
     {
         return new Definition(
-            <<<'TxT'
+            <<<'TXT'
 Removes an operand from a string concatenation.
 
 ```php
@@ -71,7 +71,7 @@ And:
 ```php
 $x = 'bar';
 
-TxT
+TXT
             ,
             MutatorCategory::SEMANTIC_REDUCTION,
             null
