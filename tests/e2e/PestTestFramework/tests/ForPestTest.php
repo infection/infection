acf45a929d970a93ed32938453ec5593c3948de8
maks-rafalko <b0rn@list.ru>
2021-05-03 08:22:57 +0300

    Add Pest, PhpUnit and mixed tested classes

--- tests/e2e/PestTestFramework/tests/ForPestTest.php
+++ tests/e2e/PestTestFramework/tests/ForPestTest.php
@@ -1,21 +1,21 @@
 <?php
 
-use PestTestFramework\SourceClass;
+use PestTestFramework\ForPest;
 
 test('Source Class from SourceTest.php', function () {
-    $sourceClass = new SourceClass();
+    $sourceClass = new ForPest();
 
     expect($sourceClass->hello())->toBe('hello');
 });
 
 test('Another test from SourceTest.php', function () {
-    $sourceClass = new SourceClass();
+    $sourceClass = new ForPest();
 
     expect($sourceClass->hello())->toBe('hello');
 });
 
 test('Third test from SourceTest.php', function () {
-    $sourceClass = new SourceClass();
+    $sourceClass = new ForPest();
 
     expect($sourceClass->add(5, 3))->toBeGreaterThan(0);
 });
