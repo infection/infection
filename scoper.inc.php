infection/infection

commit 15015ab708de60227cddacbc2c46654b3cac64a0

Do not use ocramius/package-versions (and do not break building PHAR this time) (#1583)
* Do not use ocramius/package-versions (and do not break building PHAR)

* Fix building PHAR

scoper.inc.php
@@ -42,7 +42,7 @@ $polyfillsBootstrap = Finder::create()
 
 return [
     'whitelist' => [
-        \Composer\Autoload\ClassLoader::class,
+        'Composer\*',
         'Safe\*',
         // PHP 8.0
         'T_NAME_QUALIFIED',

src/Command/ConfigureCommand.php
@@ -35,6 +35,7 @@ declare(strict_types=1);
 
 namespace Infection\Command;
 
+use Composer\InstalledVersions;
 use function count;
 use function file_exists;
 use const GLOB_ONLYDIR;
@@ -55,7 +56,6 @@ use Infection\TestFramework\TestFrameworkTypes;
 use const JSON_PRETTY_PRINT;
 use const JSON_UNESCAPED_SLASHES;
 use OutOfBoundsException;
-use PackageVersions\Versions;
 use RuntimeException;
 use function Safe\file_get_contents;
 use function Safe\file_put_contents;
@@ -65,7 +65,6 @@ use function Safe\json_encode;
 use function Safe\sprintf;
 use stdClass;
 use function strpos;
-use function strstr;
 use Symfony\Component\Console\Input\InputOption;
 
 /**
@@ -243,9 +242,9 @@ final class ConfigureCommand extends BaseCommand
         }
 
         try {
-            $version = strstr(Versions::getVersion(Application::PACKAGE_NAME), '@', true);
+            $version = InstalledVersions::getPrettyVersion(Application::PACKAGE_NAME);
 
-            if ($version === false || strpos($version, 'dev-') === 0) {
+            if ($version === null || strpos($version, 'dev-') === 0) {
                 $version = 'master';
             }
         } catch (OutOfBoundsException $e) {

src/Console/Application.php
@@ -36,12 +36,12 @@ declare(strict_types=1);
 namespace Infection\Console;
 
 use function array_merge;
+use Composer\InstalledVersions;
 use Infection\Command\ConfigureCommand;
 use Infection\Command\DescribeCommand;
 use Infection\Command\RunCommand;
 use Infection\Container;
 use OutOfBoundsException;
-use PackageVersions\Versions;
 use function preg_quote;
 use function Safe\preg_match;
 use function Safe\sprintf;
@@ -73,7 +73,7 @@ final class Application extends BaseApplication
     public function __construct(Container $container)
     {
         try {
-            $version = Versions::getVersion(self::PACKAGE_NAME);
+            $version = (string) InstalledVersions::getVersion(self::PACKAGE_NAME);
             // @codeCoverageIgnoreStart
         } catch (OutOfBoundsException $e) {
             if (preg_match('#package .*' . preg_quote(self::PACKAGE_NAME, '#') . '.* not installed#i', $e->getMessage()) === 0) {

src/Process/Factory/InitialTestsRunProcessFactory.php
@@ -35,10 +35,10 @@ declare(strict_types=1);
 
 namespace Infection\Process\Factory;
 
+use Composer\InstalledVersions;
 use Infection\AbstractTestFramework\TestFrameworkAdapter;
 use Infection\Process\OriginalPhpProcess;
 use function method_exists;
-use PackageVersions\Versions;
 use Symfony\Component\Process\Process;
 use function version_compare;
 
@@ -79,7 +79,7 @@ class InitialTestsRunProcessFactory
 
         $process->setTimeout(null); // Ignore the default timeout of 60 seconds
 
-        if (method_exists($process, 'inheritEnvironmentVariables') && version_compare(Versions::getVersion('symfony/console'), 'v4.4', '<')) {
+        if (method_exists($process, 'inheritEnvironmentVariables') && version_compare((string) InstalledVersions::getVersion('symfony/console'), 'v4.4', '<')) {
             // In version 4.4.0 this method is deprecated and removed in 5.0.0
             $process->inheritEnvironmentVariables();
         }

src/Process/Factory/MutantProcessFactory.php
@@ -35,6 +35,7 @@ declare(strict_types=1);
 
 namespace Infection\Process\Factory;
 
+use Composer\InstalledVersions;
 use Infection\AbstractTestFramework\TestFrameworkAdapter;
 use Infection\Event\EventDispatcher\EventDispatcher;
 use Infection\Event\MutantProcessWasFinished;
@@ -42,7 +43,6 @@ use Infection\Mutant\Mutant;
 use Infection\Mutant\MutantExecutionResultFactory;
 use Infection\Process\MutantProcess;
 use function method_exists;
-use PackageVersions\Versions;
 use Symfony\Component\Process\Process;
 use function version_compare;
 
@@ -84,7 +84,7 @@ class MutantProcessFactory
 
         $process->setTimeout($this->timeout);
 
-        if (method_exists($process, 'inheritEnvironmentVariables') && version_compare(Versions::getVersion('symfony/console'), 'v4.4', '<')) {
+        if (method_exists($process, 'inheritEnvironmentVariables') && version_compare((string) InstalledVersions::getVersion('symfony/console'), 'v4.4', '<')) {
             // in version 4.4.0 this method is deprecated and removed in 5.0.0
             $process->inheritEnvironmentVariables();
         }

commit 04f5ab61d309ef1e4825f4a53eb23e3869399b33

Test infection.phar works from subfolder where vendor folder is not available; Revert #1583 (#1586)
* Test `infection.phar` works from subfolder where `vendor` folder is not available

This catches a bug introduced in https://github.com/infection/infection/pull/1583, where `./infection.phar -V` fails with

```
Warning: require(phar:///app/infection-builds/infection.phar/vendor/composer/installed.php): Failed to open stream: phar error: "vendor/composer/installed.php" is not a file in phar "/app/infection-builds/infection.phar" in phar:///app/infection-builds/infection.phar/vendor/composer/InstalledVersions.php on line 189
```

while `build/infection.phar` works as expected

* Revert "Do not use ocramius/package-versions (and do not break building PHAR this time) (#1583)"

This reverts commit 15015ab708de60227cddacbc2c46654b3cac64a0.

* Get back `build/` prefix

.github/workflows/ci.yaml
@@ -112,4 +112,6 @@ jobs:
           TERM: xterm-256color
         run: |
           make compile
+          # test PHAR works from subfolder
+          cd build && ./infection.phar -V && cd ..
           make test-e2e E2E_PHPUNIT_GROUP=e2e INFECTION=build/infection.phar BENCHMARK_SOURCES=

scoper.inc.php
@@ -42,7 +42,7 @@ $polyfillsBootstrap = Finder::create()
 
 return [
     'whitelist' => [
-        'Composer\*',
+        \Composer\Autoload\ClassLoader::class,
         'Safe\*',
         // PHP 8.0
         'T_NAME_QUALIFIED',

src/Command/ConfigureCommand.php
@@ -35,7 +35,6 @@ declare(strict_types=1);
 
 namespace Infection\Command;
 
-use Composer\InstalledVersions;
 use function count;
 use function file_exists;
 use const GLOB_ONLYDIR;
@@ -56,6 +55,7 @@ use Infection\TestFramework\TestFrameworkTypes;
 use const JSON_PRETTY_PRINT;
 use const JSON_UNESCAPED_SLASHES;
 use OutOfBoundsException;
+use PackageVersions\Versions;
 use RuntimeException;
 use function Safe\file_get_contents;
 use function Safe\file_put_contents;
@@ -65,6 +65,7 @@ use function Safe\json_encode;
 use function Safe\sprintf;
 use stdClass;
 use function strpos;
+use function strstr;
 use Symfony\Component\Console\Input\InputOption;
 
 /**
@@ -242,9 +243,9 @@ final class ConfigureCommand extends BaseCommand
         }
 
         try {
-            $version = InstalledVersions::getPrettyVersion(Application::PACKAGE_NAME);
+            $version = strstr(Versions::getVersion(Application::PACKAGE_NAME), '@', true);
 
-            if ($version === null || strpos($version, 'dev-') === 0) {
+            if ($version === false || strpos($version, 'dev-') === 0) {
                 $version = 'master';
             }
         } catch (OutOfBoundsException $e) {

src/Console/Application.php
@@ -36,12 +36,12 @@ declare(strict_types=1);
 namespace Infection\Console;
 
 use function array_merge;
-use Composer\InstalledVersions;
 use Infection\Command\ConfigureCommand;
 use Infection\Command\DescribeCommand;
 use Infection\Command\RunCommand;
 use Infection\Container;
 use OutOfBoundsException;
+use PackageVersions\Versions;
 use function preg_quote;
 use function Safe\preg_match;
 use function Safe\sprintf;
@@ -73,7 +73,7 @@ final class Application extends BaseApplication
     public function __construct(Container $container)
     {
         try {
-            $version = (string) InstalledVersions::getVersion(self::PACKAGE_NAME);
+            $version = Versions::getVersion(self::PACKAGE_NAME);
             // @codeCoverageIgnoreStart
         } catch (OutOfBoundsException $e) {
             if (preg_match('#package .*' . preg_quote(self::PACKAGE_NAME, '#') . '.* not installed#i', $e->getMessage()) === 0) {

src/Process/Factory/InitialTestsRunProcessFactory.php
@@ -35,10 +35,10 @@ declare(strict_types=1);
 
 namespace Infection\Process\Factory;
 
-use Composer\InstalledVersions;
 use Infection\AbstractTestFramework\TestFrameworkAdapter;
 use Infection\Process\OriginalPhpProcess;
 use function method_exists;
+use PackageVersions\Versions;
 use Symfony\Component\Process\Process;
 use function version_compare;
 
@@ -79,7 +79,7 @@ class InitialTestsRunProcessFactory
 
         $process->setTimeout(null); // Ignore the default timeout of 60 seconds
 
-        if (method_exists($process, 'inheritEnvironmentVariables') && version_compare((string) InstalledVersions::getVersion('symfony/console'), 'v4.4', '<')) {
+        if (method_exists($process, 'inheritEnvironmentVariables') && version_compare(Versions::getVersion('symfony/console'), 'v4.4', '<')) {
             // In version 4.4.0 this method is deprecated and removed in 5.0.0
             $process->inheritEnvironmentVariables();
         }

src/Process/Factory/MutantProcessFactory.php
@@ -35,7 +35,6 @@ declare(strict_types=1);
 
 namespace Infection\Process\Factory;
 
-use Composer\InstalledVersions;
 use Infection\AbstractTestFramework\TestFrameworkAdapter;
 use Infection\Event\EventDispatcher\EventDispatcher;
 use Infection\Event\MutantProcessWasFinished;
@@ -43,6 +42,7 @@ use Infection\Mutant\Mutant;
 use Infection\Mutant\MutantExecutionResultFactory;
 use Infection\Process\MutantProcess;
 use function method_exists;
+use PackageVersions\Versions;
 use Symfony\Component\Process\Process;
 use function version_compare;
 
@@ -84,7 +84,7 @@ class MutantProcessFactory
 
         $process->setTimeout($this->timeout);
 
-        if (method_exists($process, 'inheritEnvironmentVariables') && version_compare((string) InstalledVersions::getVersion('symfony/console'), 'v4.4', '<')) {
+        if (method_exists($process, 'inheritEnvironmentVariables') && version_compare(Versions::getVersion('symfony/console'), 'v4.4', '<')) {
             // in version 4.4.0 this method is deprecated and removed in 5.0.0
             $process->inheritEnvironmentVariables();
         }