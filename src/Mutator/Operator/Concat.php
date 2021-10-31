46d74fe1e8dd9577bbdd348362dc19d9eb2dee21
Michał Bundyra <contact@webimpress.com>
2021-10-31 10:04:52 +0000

    suppress psalm issue - ImpureMethodCall
    - per @maks-rafalko

--- src/Mutator/Operator/Concat.php
+++ src/Mutator/Operator/Concat.php
@@ -93,6 +93,7 @@ DIFF
 
         $newNode = new Node\Expr\BinaryOp\Concat($left, $right);
 
+        /** @psalm-suppress ImpureMethodCall */
         if ($printer->prettyPrint([clone $node]) !== $printer->prettyPrint([$newNode])) {
             yield $newNode;
         }
