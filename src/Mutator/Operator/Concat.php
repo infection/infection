730f3ff28f564cf160359713a6ec3a157a4829cb
Michał Bundyra <contact@webimpress.com>
2021-10-31 10:09:08 +0000

    Update Concat.php

--- src/Mutator/Operator/Concat.php
+++ src/Mutator/Operator/Concat.php
@@ -93,7 +93,7 @@ DIFF
 
         $newNode = new Node\Expr\BinaryOp\Concat($left, $right);
 
-        /** @psalm-suppress ImpureMethodCall */
+        /* @psalm-suppress ImpureMethodCall */
         if ($printer->prettyPrint([clone $node]) !== $printer->prettyPrint([$newNode])) {
             yield $newNode;
         }
