467bd29d8363a29f5ab7ccd498033dee1b4da58a
maks-rafalko <b0rn@list.ru>
2022-09-03 15:24:18 +0300

    Fix CS

--- tests/phpunit/AutoReview/ProjectCode/ProjectCodeProvider.php
+++ tests/phpunit/AutoReview/ProjectCode/ProjectCodeProvider.php
@@ -41,7 +41,6 @@ use const DIRECTORY_SEPARATOR;
 use function in_array;
 use Infection\CannotBeInstantiated;
 use Infection\Command\ConfigureCommand;
-use Infection\Command\RunCommand;
 use Infection\Config\ConsoleHelper;
 use Infection\Config\Guesser\SourceDirGuesser;
 use Infection\Configuration\Schema\SchemaConfigurationFactory;
