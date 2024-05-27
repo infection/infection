infection/infection

commit 0d5fd7e266048f1a5f46485f679e5f625e8f010d

Support custom mutators (#1686)
* Support custom mutators

* Add e2e test to make sure Custom Mutator works as expected

- exclude Mutator, Definition, MutatorCategory from being prefixes by Box/Scoper

* Update expectations of e2e test

* Play with exposing/excluding extension points to work with bot `bin/infection` and `infection.phar`

* Allow custom mutators only as `RooNamespace\SubNamespace\Class`

Invalid FQCN are forbidden

* Replace `exclude-namespaces` with `expose-namespaces` for scoper config as per comments

* Revert "Replace `exclude-namespaces` with `expose-namespaces` for scoper config as per comments"

This reverts commit 809d8f243e6baf72321e4e6f055be65a76c9b764.

---------

Co-authored-by: Stanislav Vozhov <svozhov@plesk.com>
Co-authored-by: maks-rafalko <b0rn@list.ru>

src/Logger/Html/StrykerHtmlReportBuilder.php
@@ -54,18 +54,17 @@ use Infection\Mutant\DetectionStatus;
 use Infection\Mutant\MutantExecutionResult;
 use Infection\Mutator\FunctionSignature\ProtectedVisibility;
 use Infection\Mutator\FunctionSignature\PublicVisibility;
-use Infection\Mutator\Mutator;
 use Infection\Mutator\MutatorFactory;
-use Infection\Mutator\ProfileList;
+use Infection\Mutator\MutatorResolver;
 use Infection\Mutator\Removal\MethodCallRemoval;
 use Infection\Str;
 use function ltrim;
 use function md5;
 use const PHP_EOL;
-use PhpParser\NodeAbstract;
 use function Safe\file_get_contents;
 use function Safe\preg_match;
 use function Safe\preg_split;
+use function sprintf;
 use function str_starts_with;
 use function strlen;
 use function substr;
@@ -247,7 +246,7 @@ final class StrykerHtmlReportBuilder
                     'id' => $result->getMutantHash(),
                     'mutatorName' => $result->getMutatorName(),
                     'replacement' => Str::convertToUtf8(Str::trimLineReturns(ltrim($replacement))),
-                    'description' => $this->getMutatorDescription($result->getMutatorName()),
+                    'description' => $this->getMutatorDescription($result->getMutatorClass()),
                     'location' => [
                         'start' => ['line' => $result->getOriginalStartingLine(), 'column' => $startingColumn],
                         'end' => ['line' => $endingLine, 'column' => $endingColumn],
@@ -313,12 +312,9 @@ final class StrykerHtmlReportBuilder
         return 0;
     }
 
-    private function getMutatorDescription(string $mutatorName): string
+    private function getMutatorDescription(string $mutatorClass): string
     {
-        Assert::keyExists(ProfileList::ALL_MUTATORS, $mutatorName);
-
-        /** @var Mutator<NodeAbstract> $mutatorClass */
-        $mutatorClass = ProfileList::ALL_MUTATORS[$mutatorName];
+        Assert::true(MutatorResolver::isValidMutator($mutatorClass), sprintf('Unknown mutator "%s"', $mutatorClass));
 
         $definition = $mutatorClass::getDefinition();
 

tests/phpunit/Logger/Html/StrykerHtmlReportBuilderTest.php
@@ -479,6 +479,7 @@ final class StrykerHtmlReportBuilderTest extends TestCase
             $detectionStatus,
             now(normalize_trailing_spaces($diff)),
             $mutantHash,
+            $mutatorClassName,
             MutatorName::getName($mutatorClassName),
             $originalFileRealPath,
             $originalStartingLine,

devTools/phpstan-src-baseline.neon
@@ -26,17 +26,12 @@ parameters:
 			path: ../src/Mutator/MutatorResolver.php
 
 		-
-			message: "#^Parameter &\\$mutators by\\-ref type of method Infection\\\\Mutator\\\\MutatorResolver\\:\\:registerFromClass\\(\\) expects array\\<string, array\\<string\\>\\>, array\\<string, array\\<array\\<string\\>\\|string\\>\\> given\\.$#"
+			message: "#^Parameter &\\$mutators by\\-ref type of method Infection\\\\Mutator\\\\MutatorResolver\\:\\:registerFromClass\\(\\) expects array\\<string, array\\<string, string\\>\\>, array\\<string, array\\<string, array\\<string\\>\\|string\\>\\> given\\.$#"
 			count: 1
 			path: ../src/Mutator/MutatorResolver.php
 
 		-
-			message: "#^Parameter &\\$mutators by\\-ref type of method Infection\\\\Mutator\\\\MutatorResolver\\:\\:registerFromClass\\(\\) expects array\\<string, array\\<string\\>\\>, array\\<string, array\\> given\\.$#"
-			count: 1
-			path: ../src/Mutator/MutatorResolver.php
-
-		-
-			message: "#^Parameter &\\$mutators by\\-ref type of method Infection\\\\Mutator\\\\MutatorResolver\\:\\:registerFromName\\(\\) expects array\\<string, array\\<string, string\\>\\>, array\\<string, array\\<string\\>\\> given\\.$#"
+			message: "#^Parameter &\\$mutators by\\-ref type of method Infection\\\\Mutator\\\\MutatorResolver\\:\\:registerFromClass\\(\\) expects array\\<string, array\\<string, string\\>\\>, array\\<string, array\\> given\\.$#"
 			count: 1
 			path: ../src/Mutator/MutatorResolver.php
 

devTools/phpstan-src.neon
@@ -54,11 +54,6 @@ parameters:
             path: ../src/Logger/Html/StrykerHtmlReportBuilder.php
 
         -
-            message: "#^PHPDoc tag @var with type Infection\\\\Mutator\\\\Mutator\\<PhpParser\\\\NodeAbstract\\> is not subtype of native type 'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\Assignment'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\AssignmentEqual'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\BitwiseAnd'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\BitwiseNot'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\BitwiseOr'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\BitwiseXor'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\Decrement'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\DivEqual'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\Division'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\Exponentiation'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\Increment'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\Minus'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\MinusEqual'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\ModEqual'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\Modulus'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\MulEqual'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\Multiplication'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\Plus'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\PlusEqual'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\PowEqual'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\RoundingFamily'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\ShiftLeft'\\|'Infection\\\\\\\\Mutator\\\\\\\\Arithmetic\\\\\\\\ShiftRight'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\ArrayItem'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\EqualIdentical'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\FalseValue'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\IdenticalEqual'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\InstanceOf_'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\LogicalAnd'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\LogicalAndAllSubExprNegation'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\LogicalAndNegation'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\LogicalAndSingleSubExprNegation'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\LogicalLowerAnd'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\LogicalLowerOr'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\LogicalNot'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\LogicalOr'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\LogicalOrAllSubExprNegation'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\LogicalOrNegation'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\LogicalOrSingleSubExprNegation'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\NotEqualNotIdentical'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\NotIdenticalNotEqual'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\TrueValue'\\|'Infection\\\\\\\\Mutator\\\\\\\\Boolean\\\\\\\\Yield_'\\|'Infection\\\\\\\\Mutator\\\\\\\\Cast\\\\\\\\CastArray'\\|'Infection\\\\\\\\Mutator\\\\\\\\Cast\\\\\\\\CastBool'\\|'Infection\\\\\\\\Mutator\\\\\\\\Cast\\\\\\\\CastFloat'\\|'Infection\\\\\\\\Mutator\\\\\\\\Cast\\\\\\\\CastInt'\\|'Infection\\\\\\\\Mutator\\\\\\\\Cast\\\\\\\\CastObject'\\|'Infection\\\\\\\\Mutator\\\\\\\\Cast\\\\\\\\CastString'\\|'Infection\\\\\\\\Mutator\\\\\\\\ConditionalBoundary\\\\\\\\GreaterThan'\\|'Infection\\\\\\\\Mutator\\\\\\\\ConditionalBoundary\\\\\\\\GreaterThanOrEqualTo'\\|'Infection\\\\\\\\Mutator\\\\\\\\ConditionalBoundary\\\\\\\\LessThan'\\|'Infection\\\\\\\\Mutator\\\\\\\\ConditionalBoundary\\\\\\\\LessThanOrEqualTo'\\|'Infection\\\\\\\\Mutator\\\\\\\\ConditionalNegotiation\\\\\\\\Equal'\\|'Infection\\\\\\\\Mutator\\\\\\\\ConditionalNegotiation\\\\\\\\GreaterThanNegotiation'\\|'Infection\\\\\\\\Mutator\\\\\\\\ConditionalNegotiation\\\\\\\\GreaterThanOrEqualToNegotiation'\\|'Infection\\\\\\\\Mutator\\\\\\\\ConditionalNegotiation\\\\\\\\Identical'\\|'Infection\\\\\\\\Mutator\\\\\\\\ConditionalNegotiation\\\\\\\\LessThanNegotiation'\\|'Infection\\\\\\\\Mutator\\\\\\\\ConditionalNegotiation\\\\\\\\LessThanOrEqualToNegotiation'\\|'Infection\\\\\\\\Mutator\\\\\\\\ConditionalNegotiation\\\\\\\\NotEqual'\\|'Infection\\\\\\\\Mutator\\\\\\\\ConditionalNegotiation\\\\\\\\NotIdentical'\\|'Infection\\\\\\\\Mutator\\\\\\\\Extensions\\\\\\\\BCMath'\\|'Infection\\\\\\\\Mutator\\\\\\\\Extensions\\\\\\\\MBString'\\|'Infection\\\\\\\\Mutator\\\\\\\\FunctionSignature\\\\\\\\ProtectedVisibility'\\|'Infection\\\\\\\\Mutator\\\\\\\\FunctionSignature\\\\\\\\PublicVisibility'\\|'Infection\\\\\\\\Mutator\\\\\\\\Loop\\\\\\\\DoWhile'\\|'Infection\\\\\\\\Mutator\\\\\\\\Loop\\\\\\\\For_'\\|'Infection\\\\\\\\Mutator\\\\\\\\Loop\\\\\\\\Foreach_'\\|'Infection\\\\\\\\Mutator\\\\\\\\Loop\\\\\\\\While_'\\|'Infection\\\\\\\\Mutator\\\\\\\\Number\\\\\\\\DecrementInteger'\\|'Infection\\\\\\\\Mutator\\\\\\\\Number\\\\\\\\IncrementInteger'\\|'Infection\\\\\\\\Mutator\\\\\\\\Number\\\\\\\\OneZeroFloat'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\AssignCoalesce'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\Break_'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\Catch_'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\Coalesce'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\Concat'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\Continue_'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\ElseIfNegation'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\Finally_'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\IfNegation'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\NullSafeMethodCall'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\NullSafePropertyCall'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\SpreadAssignment'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\SpreadOneItem'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\SpreadRemoval'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\Ternary'\\|'Infection\\\\\\\\Mutator\\\\\\\\Operator\\\\\\\\Throw_'\\|'Infection\\\\\\\\Mutator\\\\\\\\Regex\\\\\\\\PregMatchMatches'\\|'Infection\\\\\\\\Mutator\\\\\\\\Regex\\\\\\\\PregMatchRemoveCaret'\\|'Infection\\\\\\\\Mutator\\\\\\\\Regex\\\\\\\\PregMatchRemoveDollar'\\|'Infection\\\\\\\\Mutator\\\\\\\\Regex\\\\\\\\PregMatchRemoveFlags'\\|'Infection\\\\\\\\Mutator\\\\\\\\Regex\\\\\\\\PregQuote'\\|'Infection\\\\\\\\Mutator\\\\\\\\Removal\\\\\\\\ArrayItemRemoval'\\|'Infection\\\\\\\\Mutator\\\\\\\\Removal\\\\\\\\CatchBlockRemoval'\\|'Infection\\\\\\\\Mutator\\\\\\\\Removal\\\\\\\\CloneRemoval'\\|'Infection\\\\\\\\Mutator\\\\\\\\Removal\\\\\\\\ConcatOperandRemoval'\\|'Infection\\\\\\\\Mutator\\\\\\\\Removal\\\\\\\\FunctionCallRemoval'\\|'Infection\\\\\\\\Mutator\\\\\\\\Removal\\\\\\\\MatchArmRemoval'\\|'Infection\\\\\\\\Mutator\\\\\\\\Removal\\\\\\\\MethodCallRemoval'\\|'Infection\\\\\\\\Mutator\\\\\\\\Removal\\\\\\\\SharedCaseRemoval'\\|'Infection\\\\\\\\Mutator\\\\\\\\ReturnValue\\\\\\\\ArrayOneItem'\\|'Infection\\\\\\\\Mutator\\\\\\\\ReturnValue\\\\\\\\FloatNegation'\\|'Infection\\\\\\\\Mutator\\\\\\\\ReturnValue\\\\\\\\FunctionCall'\\|'Infection\\\\\\\\Mutator\\\\\\\\ReturnValue\\\\\\\\IntegerNegation'\\|'Infection\\\\\\\\Mutator\\\\\\\\ReturnValue\\\\\\\\NewObject'\\|'Infection\\\\\\\\Mutator\\\\\\\\ReturnValue\\\\\\\\This'\\|'Infection\\\\\\\\Mutator\\\\\\\\ReturnValue\\\\\\\\YieldValue'\\|'Infection\\\\\\\\Mutator\\\\\\\\Sort\\\\\\\\Spaceship'\\|'Infection\\\\\\\\Mutator\\\\\\\\SyntaxError'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayChangeKeyCase'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayChunk'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayColumn'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayCombine'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayDiff'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayDiffAssoc'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayDiffKey'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayDiffUassoc'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayDiffUkey'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayFilter'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayFlip'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayIntersect'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayIntersectAssoc'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayIntersectKey'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayIntersectUassoc'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayIntersectUkey'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayKeys'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayMap'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayMerge'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayMergeRecursive'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayPad'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayReduce'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayReplace'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayReplaceRecursive'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayReverse'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArraySlice'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArraySplice'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayUdiff'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayUdiffAssoc'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayUdiffUassoc'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayUintersect'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayUintersectAssoc'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayUintersectUassoc'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayUnique'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapArrayValues'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapFinally'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapLcFirst'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapLtrim'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapRtrim'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapStrIreplace'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapStrRepeat'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapStrReplace'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapStrRev'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapStrShuffle'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapStrToLower'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapStrToUpper'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapSubstr'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapTrim'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapUcFirst'\\|'Infection\\\\\\\\Mutator\\\\\\\\Unwrap\\\\\\\\UnwrapUcWords'\\.$#"
-            count: 1
-            path: ../src/Logger/Html/StrykerHtmlReportBuilder.php
-
-        -
             message: "#^Generator expects value type list\\<PhpParser\\\\Node\\\\Stmt\\>, array\\<PhpParser\\\\Node\\\\Stmt\\> given\\.$#"
             count: 1
             path: ../src/Mutator/Unwrap/UnwrapFinally.php

scoper.inc.php
@@ -35,6 +35,15 @@ declare(strict_types=1);
 
 return [
     'prefix' => 'Infected',
+    'expose-classes' => [
+        'Infection\Mutator\Definition',
+        'Infection\Mutator\Mutator',
+        'Infection\Mutator\MutatorCategory',
+    ],
+    'exclude-namespaces' => [
+        // we have to exclude it cause Mutator depends on PhpParser/Node interface, and it's not in a separate package
+        '/^PhpParser/',
+    ],
     'exclude-constants' => [
         // Symfony global constants
         '/^SYMFONY\_[\p{L}_]+$/',

src/Command/DescribeCommand.php
@@ -40,6 +40,7 @@ use function array_keys;
 use Infection\Console\IO;
 use Infection\Mutator\Definition;
 use Infection\Mutator\Mutator;
+use Infection\Mutator\MutatorResolver;
 use Infection\Mutator\ProfileList;
 use function sprintf;
 use Symfony\Component\Console\Input\InputArgument;
@@ -71,7 +72,9 @@ final class DescribeCommand extends BaseCommand
             );
         }
 
-        if (!array_key_exists($mutator, ProfileList::ALL_MUTATORS)) {
+        if (!array_key_exists($mutator, ProfileList::ALL_MUTATORS)
+            && !MutatorResolver::isValidMutator($mutator)
+        ) {
             $io->error(sprintf(
                 '"The %s mutator does not exist"',
                 $mutator,
@@ -80,7 +83,7 @@ final class DescribeCommand extends BaseCommand
             return false;
         }
 
-        $mutatorClass = ProfileList::ALL_MUTATORS[$mutator];
+        $mutatorClass = ProfileList::ALL_MUTATORS[$mutator] ?? $mutator;
 
         Assert::subclassOf($mutatorClass, Mutator::class);
 

src/Mutant/MutantExecutionResult.php
@@ -35,11 +35,11 @@ declare(strict_types=1);
 
 namespace Infection\Mutant;
 
-use function array_keys;
 use Infection\AbstractTestFramework\Coverage\TestLocation;
-use Infection\Mutator\ProfileList;
+use Infection\Mutator\MutatorResolver;
 use Later\Interfaces\Deferred;
 use RuntimeException;
+use function Safe\sprintf;
 use function strlen;
 use function strrpos;
 use Webmozart\Assert\Assert;
@@ -51,7 +51,7 @@ use Webmozart\Assert\Assert;
 class MutantExecutionResult
 {
     private readonly string $detectionStatus;
-    private readonly string $mutatorName;
+    private readonly string $mutatorClass;
 
     /**
      * @param Deferred<string> $mutantDiff
@@ -65,7 +65,8 @@ class MutantExecutionResult
         string $detectionStatus,
         private readonly Deferred $mutantDiff,
         private readonly string $mutantHash,
-        string $mutatorName,
+        string $mutatorClass,
+        private readonly string $mutatorName,
         private readonly string $originalFilePath,
         private readonly int $originalStartingLine,
         private readonly int $originalEndingLine,
@@ -76,9 +77,10 @@ class MutantExecutionResult
         private readonly array $tests,
     ) {
         Assert::oneOf($detectionStatus, DetectionStatus::ALL);
-        Assert::oneOf($mutatorName, array_keys(ProfileList::ALL_MUTATORS));
+        Assert::true(MutatorResolver::isValidMutator($mutatorClass), sprintf('Unknown mutator "%s"', $mutatorClass));
+
         $this->detectionStatus = $detectionStatus;
-        $this->mutatorName = $mutatorName;
+        $this->mutatorClass = $mutatorClass;
     }
 
     public static function createFromNonCoveredMutant(Mutant $mutant): self
@@ -119,6 +121,11 @@ class MutantExecutionResult
     public function getMutantHash(): string
     {
         return $this->mutantHash;
+    }
+
+    public function getMutatorClass(): string
+    {
+        return $this->mutatorClass;
     }
 
     public function getMutatorName(): string
@@ -197,6 +204,7 @@ class MutantExecutionResult
             $detectionStatus,
             $mutant->getDiff(),
             $mutant->getMutation()->getHash(),
+            $mutant->getMutation()->getMutatorClass(),
             $mutant->getMutation()->getMutatorName(),
             $mutation->getOriginalFilePath(),
             $mutation->getOriginalStartingLine(),

src/Mutant/MutantExecutionResultFactory.php
@@ -66,6 +66,7 @@ class MutantExecutionResultFactory
             $this->retrieveDetectionStatus($mutantProcess),
             $mutant->getDiff(),
             $mutation->getHash(),
+            $mutation->getMutatorClass(),
             $mutation->getMutatorName(),
             $mutation->getOriginalFilePath(),
             $mutation->getOriginalStartingLine(),

commit 036bf24d21c25aa1fb96d869fcb4d086f81d2fd5

When IgnoreMutator decorator class is used, work with underlying class of mutator to get its definition
Fixes:

```
DomainException: The class "Infection\Mutator\IgnoreMutator" does not have a definition
```

on StrykerHtmlReportBuilder.php

Bug has been introduced in https://github.com/infection/infection/pull/1686/files#diff-01143bfb1cd8c30fa7f368e58fa5b9bebf3f35de2a4bc509123ed81358b59ff8R249

src/Logger/Html/StrykerHtmlReportBuilder.php
@@ -54,13 +54,16 @@ use Infection\Mutant\DetectionStatus;
 use Infection\Mutant\MutantExecutionResult;
 use Infection\Mutator\FunctionSignature\ProtectedVisibility;
 use Infection\Mutator\FunctionSignature\PublicVisibility;
+use Infection\Mutator\Mutator;
 use Infection\Mutator\MutatorFactory;
 use Infection\Mutator\MutatorResolver;
+use Infection\Mutator\ProfileList;
 use Infection\Mutator\Removal\MethodCallRemoval;
 use Infection\Str;
 use function ltrim;
 use function md5;
 use const PHP_EOL;
+use PhpParser\NodeAbstract;
 use function Safe\file_get_contents;
 use function Safe\preg_match;
 use function Safe\preg_split;
@@ -246,7 +249,7 @@ final class StrykerHtmlReportBuilder
                     'id' => $result->getMutantHash(),
                     'mutatorName' => $result->getMutatorName(),
                     'replacement' => Str::convertToUtf8(Str::trimLineReturns(ltrim($replacement))),
-                    'description' => $this->getMutatorDescription($result->getMutatorClass()),
+                    'description' => $this->getMutatorDescription($result->getMutatorName(), $result->getMutatorClass()),
                     'location' => [
                         'start' => ['line' => $result->getOriginalStartingLine(), 'column' => $startingColumn],
                         'end' => ['line' => $endingLine, 'column' => $endingColumn],
@@ -312,9 +315,12 @@ final class StrykerHtmlReportBuilder
         return 0;
     }
 
-    private function getMutatorDescription(string $mutatorClass): string
+    private function getMutatorDescription(string $mutatorName, string $mutatorClass): string
     {
         Assert::true(MutatorResolver::isValidMutator($mutatorClass), sprintf('Unknown mutator "%s"', $mutatorClass));
+
+        /** @var Mutator<NodeAbstract> $mutatorClass */
+        $mutatorClass = ProfileList::ALL_MUTATORS[$mutatorName] ?? $mutatorClass;
 
         $definition = $mutatorClass::getDefinition();
 

tests/phpunit/Logger/Html/StrykerHtmlReportBuilderTest.php
@@ -45,6 +45,7 @@ use Infection\Metrics\ResultsCollector;
 use Infection\Mutant\DetectionStatus;
 use Infection\Mutant\MutantExecutionResult;
 use Infection\Mutator\FunctionSignature\PublicVisibility;
+use Infection\Mutator\IgnoreMutator;
 use Infection\Mutator\Removal\ArrayItemRemoval;
 use Infection\Mutator\Removal\MethodCallRemoval;
 use Infection\Tests\Mutator\MutatorName;
@@ -301,7 +302,7 @@ final class StrykerHtmlReportBuilderTest extends TestCase
                              $this->inner('3');
                     DIFF,
                 '32f68ca331c9262cc97322271d88d06d',
-                PublicVisibility::class,
+                IgnoreMutator::class,
                 realpath(__DIR__ . '/../../Fixtures/ForHtmlReport.php'),
                 13,
                 35,
@@ -312,6 +313,8 @@ final class StrykerHtmlReportBuilderTest extends TestCase
                     // check that duplicate values are moved in the report
                     new TestLocation('TestClass::test_method1', '/infection/path/to/TestClass.php', 0.123),
                 ],
+                'PHPUnit output. Tests: 1, Assertions: 3',
+                'PublicVisibility',
             ),
             // this tests diff on the one-line method call removal
             self::createMutantExecutionResult(
@@ -472,6 +475,7 @@ final class StrykerHtmlReportBuilderTest extends TestCase
         int $originalEndFilePosition,
         array $testLocations,
         ?string $processOutput = 'PHPUnit output. Tests: 1, Assertions: 3',
+        ?string $mutatorName = null,
     ): MutantExecutionResult {
         return new MutantExecutionResult(
             'bin/phpunit --configuration infection-tmp-phpunit.xml --filter "tests/Acme/FooTest.php"',
@@ -480,7 +484,7 @@ final class StrykerHtmlReportBuilderTest extends TestCase
             now(normalize_trailing_spaces($diff)),
             $mutantHash,
             $mutatorClassName,
-            MutatorName::getName($mutatorClassName),
+            $mutatorName ?? MutatorName::getName($mutatorClassName),
             $originalFileRealPath,
             $originalStartingLine,
             $originalEndingLine,