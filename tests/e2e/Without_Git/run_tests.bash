78d23049f8299cb1d47ab71324686b60b47235df
maks-rafalko <b0rn@list.ru>
2024-06-12 00:48:36 +0200

    Try to load infection/infection from git by branch, not from parent folder as it leads to fatal error
    error:
    
    ```
    Package infection/infection cannot install to "D:\a\infection\infection\tests\e2e\Without_Git\vendor\infection\infe
      ction" inside its source at "D:\a\infection\infection"
    ```

--- tests/e2e/Without_Git/run_tests.bash
+++ tests/e2e/Without_Git/run_tests.bash
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+set -e
+
+git_branch=$(echo "${GITHUB_HEAD_REF:-$(git rev-parse --abbrev-ref HEAD)}" | sed 's/\//\\\//g')
+
+echo "git_branch: ${git_branch}"
+
+if [ "$git_branch" == "master" ]; then
+  exit 0;
+fi;
+
+sed -i "s/\"infection\/infection\": \"dev-master\"/\"infection\/infection\": \"dev-${git_branch}\"/" composer.json
+
+set -e pipefail
+
+rm -f composer.lock
+composer install
+
 docker run -t -v "$PWD":/opt -w /opt php:8.1-alpine vendor/bin/infection --coverage=infection-coverage
 
 diff -w expected-output.txt infection.log
