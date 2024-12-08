018cd812b576ec8927c23389c8eadfd2e9233a07
Maks Rafalko <maks.rafalko@gmail.com>
2024-12-08 14:29:37 +0100

    Upgrade libraries to avoid PHP Warnings
    ```
    Implicitly marking parameter $var as nullable is deprecated
    ```

--- src/Config/Guesser/SourceDirGuesser.php
+++ src/Config/Guesser/SourceDirGuesser.php
@@ -40,7 +40,7 @@ use function in_array;
 use function is_array;
 use function is_string;
 use LogicException;
-use function Safe\array_walk_recursive;
+use function array_walk_recursive;
 use stdClass;
 use function trim;
 
