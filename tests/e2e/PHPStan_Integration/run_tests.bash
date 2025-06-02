95e673fdf259065be9fb2a16ee3cc246602f22e2
Rafalko, Maksim <maks.rafalko@gmail.com>
2025-06-02 23:31:09 +0200

    Fix e2e tests

--- tests/e2e/PHPStan_Integration/run_tests.bash
+++ tests/e2e/PHPStan_Integration/run_tests.bash
@@ -4,6 +4,6 @@ readonly INFECTION=../../../${1}
 
 set -e pipefail
 
-php $INFECTION --static-analysis-tool=phpstan
+php $INFECTION --static-analysis-tool=phpstan --no-progress --threads=2
 
 diff -w expected-output.txt infection.log
