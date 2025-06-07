e58d125b45fe8a913ea312d6256747df47a57eea
Markus Staab <maggus.staab@googlemail.com>
2025-06-07 14:12:59 +0200

    Don't mutate greater/smaller-than in ternary (#2141)

--- tests/phpunit/Mutator/ConditionalNegotiation/LessThanNegotiationTest.php
+++ tests/phpunit/Mutator/ConditionalNegotiation/LessThanNegotiationTest.php
@@ -68,5 +68,13 @@ final class LessThanNegotiationTest extends BaseMutatorTestCase
                 PHP
             ,
         ];
+
+        yield 'It does not mutate inside ternary to prevent overlap with TernaryMutator' => [
+            <<<'PHP'
+                <?php
+                $x < 6 ? 'yes' : 'no';
+                PHP
+            ,
+        ];
     }
 }
