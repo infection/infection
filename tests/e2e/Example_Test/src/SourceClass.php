a24e7e4d60bec29f3a1f2da2764fef66f8d1f773
Rafalko, Maksim <maks.rafalko@gmail.com>
2025-06-15 12:14:43 +0200

    Try to reproduce #2196

--- tests/e2e/Example_Test/src/SourceClass.php
+++ tests/e2e/Example_Test/src/SourceClass.php
@@ -6,6 +6,37 @@ class SourceClass
 {
     public function hello(): string
     {
+        $a = 1;
+
+        for ($i = 0; $i < 10; $i++) {
+
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+
+        }
+
+//        for ($i = 0; $i < 10; $i++) {
+//
+//        }
+
+        $a = 2 + 3 + 4 + 5;
+        $b = 2 + 3 + 4 + 5;
+        $c = 2 + 3 + 4 + 5;
+        $d = 2 + 3 + 4 + 5;
+
         return 'hello';
     }
 }
