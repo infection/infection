3bff84bf785a56861f6bed8d65e1fee8f42d0ae4
Markus Staab <maggus.staab@googlemail.com>
2025-06-17 10:09:44 +0200

    Killed timed-out mutant in ExcludeDirsProvider (#2219)
    * Killed timed-out mutant in ExcludeDirsProvider
    
    * fix build

--- src/Config/ValueProvider/ExcludeDirsProvider.php
+++ src/Config/ValueProvider/ExcludeDirsProvider.php
@@ -113,7 +113,8 @@ final readonly class ExcludeDirsProvider
         $question->setAutocompleterValues($autocompleteValues);
         $question->setValidator($this->getValidator(new RootsFileOrDirectoryLocator($sourceDirs, $this->filesystem)));
 
-        while (($dir = $this->questionHelper->ask($io->getInput(), $io->getOutput(), $question)) !== '') {
+        // @phpstan-ignore while.condNotBoolean (prevent timed-out mutant)
+        while ($dir = $this->questionHelper->ask($io->getInput(), $io->getOutput(), $question)) {
             $excludedDirs[] = $dir;
         }
 
