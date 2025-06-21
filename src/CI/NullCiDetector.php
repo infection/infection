3a89b2b44bb18f22a3f32dee850d312fc03db5ec
Markus Staab <maggus.staab@googlemail.com>
2025-06-21 09:11:15 +0200

    Cleanup MemoizedCiDetector, NullCiDetector (#2242)
    * Cleanup MemoizedCiDetector
    
    * cleanup the whole hack, as parent class meanwhile works for us
    
    * remove analog method on NullCiDetector

--- src/CI/NullCiDetector.php
+++ src/CI/NullCiDetector.php
@@ -37,7 +37,6 @@ namespace Infection\CI;
 
 use OndraM\CiDetector\Ci\CiInterface;
 use OndraM\CiDetector\CiDetector;
-use OndraM\CiDetector\Env;
 use OndraM\CiDetector\Exception\CiNotDetectedException;
 
 /**
@@ -45,11 +44,6 @@ use OndraM\CiDetector\Exception\CiNotDetectedException;
  */
 final class NullCiDetector extends CiDetector
 {
-    public static function fromEnvironment(Env $environment): CiDetector
-    {
-        return new self();
-    }
-
     public function isCiDetected(): bool
     {
         return false;
