55a1ed5f25339fd74221b114263dc81ad88ee3c2
Alexey Kopytko <alexey@kopytko.com>
2025-07-24 10:55:40 +0900

    Attempt to use sanmai/di-container

--- src/Container.php
+++ src/Container.php
@@ -165,11 +165,12 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Path;
 use Webmozart\Assert\Assert;
+use DIContainer\Container as DIContainer;
 
 /**
  * @internal
  */
-final class Container
+final class Container extends DIContainer
 {
     public const DEFAULT_CONFIG_FILE = null;
 
@@ -237,27 +238,7 @@ final class Container
 
     public const DEFAULT_MAP_SOURCE_CLASS_TO_TEST_STRATEGY = null;
 
-    /**
-     * @var array<class-string<object>, object>
-     */
-    private array $values = [];
-
-    /**
-     * @var array<class-string<object>, Closure(self): object>
-     */
-    private array $factories = [];
-
     private ?string $defaultJUnitPath = null;
-
-    /**
-     * @param array<class-string<object>, Closure(self): object> $values
-     */
-    public function __construct(array $values)
-    {
-        foreach ($values as $id => $value) {
-            $this->offsetSet($id, $value);
-        }
-    }
 
     public static function create(): self
     {
@@ -1176,142 +1157,6 @@ final class Container
      */
     private function offsetSet(string $id, Closure $value): void
     {
-        $this->factories[$id] = $value;
-        unset($this->values[$id]);
-    }
-
-    /**
-     * @template T of object
-     *
-     * @param class-string<T> $id
-     * @phpstan-return T
-     */
-    private function setValueOrThrow(string $id, object $value): object
-    {
-        Assert::isInstanceOf($value, $id);
-        $this->values[$id] = $value;
-
-        return $value;
-    }
-
-    /**
-     * @template T of object
-     *
-     * @param class-string<T> $id
-     * @phpstan-return T
-     */
-    private function get(string $id): object
-    {
-        if (array_key_exists($id, $this->values)) {
-            $value = $this->values[$id];
-            Assert::isInstanceOf($value, $id);
-
-            return $value;
-        }
-
-        if (array_key_exists($id, $this->factories)) {
-            $value = $this->factories[$id]($this);
-
-            return $this->setValueOrThrow($id, $value);
-        }
-
-        $value = $this->createService($id);
-
-        if ($value === null) {
-            throw new InvalidArgumentException(sprintf('Unknown service "%s"', $id));
-        }
-
-        return $this->setValueOrThrow($id, $value);
-    }
-
-    /**
-     * @template T of object
-     *
-     * @param class-string<T> $id
-     * @phpstan-return ?T
-     */
-    private function createService(string $id): ?object
-    {
-        $reflectionClass = new ReflectionClass($id);
-        $constructor = $reflectionClass->getConstructor();
-
-        if (!$reflectionClass->isInstantiable()) {
-            return null;
-        }
-
-        if ($constructor === null || $constructor->getNumberOfParameters() === 0) {
-            return $reflectionClass->newInstance();
-        }
-
-        $resolvedArguments = take($constructor->getParameters())
-            ->map($this->resolveParameter(...))
-            ->toList();
-
-        // Check if we identified all parameters for the service
-        if (count($resolvedArguments) !== $constructor->getNumberOfParameters()) {
-            return null;
-        }
-
-        return $reflectionClass->newInstanceArgs($resolvedArguments);
-    }
-
-    /**
-     * Builds a potentially incomplete list of arguments for a constructor; as list of arguments may
-     * contain null values, we use a generator that can yield none or one value as an option type.
-     *
-     * @return iterable<object>
-     */
-    private function resolveParameter(ReflectionParameter $parameter): iterable
-    {
-        // Variadic parameters need hand-weaving
-        if ($parameter->isVariadic()) {
-            return;
-        }
-
-        $paramType = $parameter->getType();
-
-        // Not considering composite types, such as unions or intersections, for now
-        Assert::isInstanceOf($paramType, ReflectionNamedType::class);
-
-        // Only attempt to resolve a non-built-in named type (a class/interface)
-        if ($paramType->isBuiltin()) {
-            return;
-        }
-
-        /** @var class-string $paramTypeName */
-        $paramTypeName = $paramType->getName();
-
-        // Found an instantiable class, done
-        if ((new ReflectionClass($paramTypeName))->isInstantiable()) {
-            yield $this->get($paramTypeName);
-
-            return;
-        }
-
-        // Look for a factory that can create an instance of an interface or abstract class
-        $matchingTypes = $this->factoriesForType($paramTypeName);
-
-        // We expect exactly one factory to match the type, otherwise we cannot resolve the parameter
-        if (count($matchingTypes) !== 1) {
-            return;
-        }
-
-        yield $this->get(reset($matchingTypes));
-    }
-
-    /**
-     * Retrieves the class or interface names of all registered factories that can produce instances of the given type.
-     * This includes direct implementations, subclasses, or the type itself.
-     *
-     * @template T of object
-     * @param class-string<T> $type the class or interface name to find factories for
-     * @return class-string<T>[] a list of factory IDs (class-strings) that are compatible with the given type
-     */
-    private function factoriesForType(string $type): array
-    {
-        return take($this->factories)
-            ->keys()
-            ->filter(static fn (string $id) => is_a($id, $type, true))
-            ->toList();
+        $this->set($id, $value);
     }
 }
