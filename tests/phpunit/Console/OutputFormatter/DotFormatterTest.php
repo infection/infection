e58708647aa40a6abf4f0310f4470fc60d0b5bfb
Théo FIDRY <theo.fidry@gmail.com>
2025-10-21 01:10:03 +0200

    fix tests on Windows

--- tests/phpunit/Console/OutputFormatter/DotFormatterTest.php
+++ tests/phpunit/Console/OutputFormatter/DotFormatterTest.php
@@ -57,32 +57,34 @@ final class DotFormatterTest extends TestCase
         $output = new BufferedOutput();
         $formatter = new DotFormatter($output);
 
-        $expected = implode(
-            "\n",
-            [
-                '',
-                implode(
-                    ', ',
-                    [
-                        '<killed>.</killed>: killed by tests',
-                        '<killed-by-static-analysis>A</killed-by-static-analysis>: killed by SA',
-                        '<escaped>M</escaped>: escaped',
-                        '<uncovered>U</uncovered>: uncovered',
-                    ],
-                ),
-                implode(
-                    ', ',
-                    [
-                        '<with-error>E</with-error>: fatal error',
-                        '<with-syntax-error>X</with-syntax-error>: syntax error',
-                        '<timeout>T</timeout>: timed out',
-                        '<skipped>S</skipped>: skipped',
-                        '<ignored>I</ignored>: ignored',
-                    ],
-                ),
-                '',
-                '',
-            ],
+        $expected = LineReturnNormalizer::normalize(
+            implode(
+                "\n",
+                [
+                    '',
+                    implode(
+                        ', ',
+                        [
+                            '<killed>.</killed>: killed by tests',
+                            '<killed-by-static-analysis>A</killed-by-static-analysis>: killed by SA',
+                            '<escaped>M</escaped>: escaped',
+                            '<uncovered>U</uncovered>: uncovered',
+                        ],
+                    ),
+                    implode(
+                        ', ',
+                        [
+                            '<with-error>E</with-error>: fatal error',
+                            '<with-syntax-error>X</with-syntax-error>: syntax error',
+                            '<timeout>T</timeout>: timed out',
+                            '<skipped>S</skipped>: skipped',
+                            '<ignored>I</ignored>: ignored',
+                        ],
+                    ),
+                    '',
+                    '',
+                ],
+            ),
         );
 
         $formatter->start(10);
