8c236277436666e4f6f145607cdcff139b80aa3c
Théo FIDRY <theo.fidry@gmail.com>
2025-11-01 10:25:47 +0100

    cleanup

--- tests/phpunit/AutoReview/ProjectCode/ProjectCodeProvider.php
+++ tests/phpunit/AutoReview/ProjectCode/ProjectCodeProvider.php
@@ -35,6 +35,7 @@ declare(strict_types=1);
 
 namespace Infection\Tests\AutoReview\ProjectCode;
 
+use Infection\Testing\SimpleMutation;
 use function array_filter;
 use const DIRECTORY_SEPARATOR;
 use function in_array;
