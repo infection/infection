infection/infection

commit 7ceadae0613366b591ca53000135b19aa093db95

refactor(SafeDOMXPath): Use the XML namespace (#2559)
While working on #2543, I noticed that `DOMDocument::load(string
$filename)` is a bit faster than getting the file content and using
`DOMDocument::loadXML(string $xml)`.

The main issue is... namespaces. When loading the string, we can,
beforehand, remove the namespaces found. This allows us to do queries
without caring about them at all.

However, if you load the document from the file, then this solution is
out of the window. You need to register a namespace and then uglify all
of your queries with it.

So this PR focuses on switching to namespaces. Switching to
`DOMDocument::load()` will be done separately.

<hr/>

You can find the results of the benchmark in #2561.

Personally, I hate either approach: I find ugly to load the file to
remove the namespaces, but the namspaces are a b**** to handle and makes
queries ugly.

So either we think the performance benefit is worth the tradeoff and we
merge this PR, or we don't in which case I'll adapt #2543 to stick to
loading the file first and stripping namespaces from the string.

devTools/phpstan-baseline.neon
@@ -1663,7 +1663,7 @@ parameters:
 			path: ../tests/phpunit/TestFramework/Coverage/XmlReport/TestLocatorTest.php
 
 		-
-			message: '#^Parameter \#2 \$haystack of method PHPUnit\\Framework\\Assert\:\:assertStringNotContainsString\(\) expects string, string\|false given\.$#'
+			message: '#^Parameter \#2 \$haystack of method PHPUnit\\Framework\\Assert\:\:assertStringContainsString\(\) expects string, string\|false given\.$#'
 			identifier: argument.type
 			count: 1
 			path: ../tests/phpunit/TestFramework/Coverage/XmlReport/XPathFactoryTest.php

src/TestFramework/Coverage/XmlReport/IndexXmlCoverageParser.php
@@ -63,7 +63,10 @@ class IndexXmlCoverageParser
         string $xmlIndexCoverageContent,
         string $coverageBasePath,
     ): iterable {
-        $xPath = XPathFactory::createXPath($xmlIndexCoverageContent);
+        $xPath = XPathFactory::createXPath(
+            $xmlIndexCoverageContent,
+            namespace: 'p',
+        );
 
         self::assertHasExecutedLines($xPath, $this->isForGitDiffLines);
 
@@ -80,7 +83,7 @@ class IndexXmlCoverageParser
     ): iterable {
         $projectSource = self::getProjectSource($xPath);
 
-        foreach ($xPath->query('//file') as $node) {
+        foreach ($xPath->query('//p:file') as $node) {
             $relativeCoverageFilePath = $node->getAttribute('href');
 
             yield new SourceFileInfoProvider(
@@ -97,7 +100,7 @@ class IndexXmlCoverageParser
      */
     private static function assertHasExecutedLines(SafeDOMXPath $xPath, bool $isForGitDiffLines): void
     {
-        $lineCoverage = $xPath->query('/phpunit/project/directory[1]/totals/lines')->item(0);
+        $lineCoverage = $xPath->query('/p:phpunit/p:project/p:directory[1]/p:totals/p:lines')->item(0);
 
         if (
             !$lineCoverage instanceof DOMElement
@@ -113,13 +116,13 @@ class IndexXmlCoverageParser
     private static function getProjectSource(SafeDOMXPath $xPath): string
     {
         // PHPUnit >= 6
-        $sourceNodes = $xPath->query('//project/@source');
+        $sourceNodes = $xPath->query('//p:project/@source');
 
         if ($sourceNodes->length > 0) {
             return $sourceNodes[0]->nodeValue;
         }
 
         // PHPUnit < 6
-        return $xPath->query('//project/@name')[0]->nodeValue;
+        return $xPath->query('//p:project/@name')[0]->nodeValue;
     }
 }

src/TestFramework/Coverage/XmlReport/SourceFileInfoProvider.php
@@ -91,12 +91,15 @@ class SourceFileInfoProvider
             ));
         }
 
-        return $this->xPath = XPathFactory::createXPath(file_get_contents($coverageFile));
+        return $this->xPath = XPathFactory::createXPath(
+            file_get_contents($coverageFile),
+            namespace: 'p',
+        );
     }
 
     private function retrieveSourceFileInfo(SafeDOMXPath $xPath): SplFileInfo
     {
-        $fileNode = $xPath->query('/phpunit/file')[0];
+        $fileNode = $xPath->query('/p:phpunit/p:file')[0];
 
         Assert::notNull($fileNode);
 

src/TestFramework/Coverage/XmlReport/XPathFactory.php
@@ -38,7 +38,6 @@ namespace Infection\TestFramework\Coverage\XmlReport;
 use DOMDocument;
 use Infection\CannotBeInstantiated;
 use Infection\TestFramework\SafeDOMXPath;
-use function Safe\preg_replace;
 use Webmozart\Assert\Assert;
 
 /**
@@ -48,26 +47,22 @@ final class XPathFactory
 {
     use CannotBeInstantiated;
 
-    public static function createXPath(string $coverageContent): SafeDOMXPath
-    {
+    public static function createXPath(
+        string $coverageContent,
+        string $namespace,
+    ): SafeDOMXPath {
         $document = new DOMDocument();
-        $success = @$document->loadXML(self::removeNamespace($coverageContent));
 
+        $success = @$document->loadXML($coverageContent);
         Assert::true($success);
 
-        return new SafeDOMXPath($document);
-    }
+        // @phpstan-ignore property.nonObject
+        $namespaceUri = $document->documentElement->namespaceURI;
+        Assert::notNull($namespaceUri, 'Expected the first document element to have a namespace URI. None found.');
 
-    /**
-     * Remove namespace to work with xPath without a headache
-     */
-    private static function removeNamespace(string $xml): string
-    {
-        /** @var string $cleanedXml */
-        $cleanedXml = preg_replace('/xmlns=\".*?\"/', '', $xml);
+        $xPath = new SafeDOMXPath($document);
+        $xPath->registerNamespace($namespace, $namespaceUri);
 
-        Assert::string($cleanedXml);
-
-        return $cleanedXml;
+        return $xPath;
     }
 }

src/TestFramework/Coverage/XmlReport/XmlCoverageParser.php
@@ -74,7 +74,7 @@ class XmlCoverageParser
 
     private static function retrieveTestLocations(SafeDOMXPath $xPath): TestLocations
     {
-        $linesNode = $xPath->query('/phpunit/file/totals/lines')[0];
+        $linesNode = $xPath->query('/p:phpunit/p:file/p:totals/p:lines')[0];
 
         $percentage = $linesNode->getAttribute('percent');
 
@@ -82,16 +82,16 @@ class XmlCoverageParser
             return new TestLocations();
         }
 
-        $coveredLineNodes = $xPath->query('/phpunit/file/coverage/line');
+        $coveredLineNodes = $xPath->query('/p:phpunit/p:file/p:coverage/p:line');
 
         if ($coveredLineNodes->length === 0) {
             return new TestLocations();
         }
 
-        $coveredMethodNodes = $xPath->query('/phpunit/file/class/method');
+        $coveredMethodNodes = $xPath->query('/p:phpunit/p:file/p:class/p:method');
 
         if ($coveredMethodNodes->length === 0) {
-            $coveredMethodNodes = $xPath->query('/phpunit/file/trait/method');
+            $coveredMethodNodes = $xPath->query('/p:phpunit/p:file/p:trait/p:method');
         }
 
         return new TestLocations(

src/TestFramework/SafeDOMXPath.php
@@ -89,4 +89,10 @@ final readonly class SafeDOMXPath
 
         return $nodes;
     }
+
+    public function registerNamespace(string $prefix, string $namespace): void
+    {
+        $result = $this->xPath->registerNamespace($prefix, $namespace);
+        Assert::true($result);
+    }
 }

tests/phpunit/TestFramework/Coverage/XmlReport/XPathFactoryTest.php
@@ -42,10 +42,13 @@ use PHPUnit\Framework\TestCase;
 #[CoversClass(XPathFactory::class)]
 final class XPathFactoryTest extends TestCase
 {
-    public function test_it_removes_namespace(): void
+    public function test_it_retains_namespace(): void
     {
-        $xPath = XPathFactory::createXPath('<?xml version="1.0"?><phpunit xmlns="http://schema.phpunit.de/coverage/1.0"></phpunit>');
+        $xPath = XPathFactory::createXPath(
+            '<?xml version="1.0"?><phpunit xmlns="http://schema.phpunit.de/coverage/1.0"></phpunit>',
+            namespace: 'p',
+        );
 
-        $this->assertStringNotContainsString('xmlns', $xPath->document->saveXML());
+        $this->assertStringContainsString('xmlns', $xPath->document->saveXML());
     }
 }

tests/phpunit/TestFramework/Coverage/XmlReport/XmlCoverageParserTest.php
@@ -177,7 +177,7 @@ final class XmlCoverageParserTest extends TestCase
      */
     private function createSourceFileInfoProvider(string $xml)
     {
-        $xPath = XPathFactory::createXPath($xml);
+        $xPath = XPathFactory::createXPath($xml, 'p');
 
         $providerMock = $this->createMock(SourceFileInfoProvider::class);
 

commit 611529b293db54c3fab09ca8368155cef43f1e73

test: Fix the CI (#2566)
Seems like I accidentally broke it in #2559. Somehow I missed the
autoreview failure, I thought the failure was only due to the escape
mutations which were expected (it was for classes out of scope of the
PR).

devTools/phpstan-baseline.neon
@@ -1663,12 +1663,6 @@ parameters:
 			path: ../tests/phpunit/TestFramework/Coverage/XmlReport/TestLocatorTest.php
 
 		-
-			message: '#^Parameter \#2 \$haystack of method PHPUnit\\Framework\\Assert\:\:assertStringContainsString\(\) expects string, string\|false given\.$#'
-			identifier: argument.type
-			count: 1
-			path: ../tests/phpunit/TestFramework/Coverage/XmlReport/XPathFactoryTest.php
-
-		-
 			message: '#^Cannot call method getSourceMethodRangeByMethod\(\) on Infection\\TestFramework\\Coverage\\TestLocations\|null\.$#'
 			identifier: method.nonObject
 			count: 1

src/TestFramework/PhpUnit/Config/Builder/MutationConfigBuilder.php
@@ -54,7 +54,7 @@ class MutationConfigBuilder extends ConfigBuilder
 {
     private ?string $originalBootstrapFile = null;
 
-    private readonly SafeDOMXPath $xPath;
+    private ?SafeDOMXPath $xPath = null;
 
     public function __construct(
         private readonly string $tmpDir,
@@ -122,7 +122,7 @@ class MutationConfigBuilder extends ConfigBuilder
 
     private function getXPath(): SafeDOMXPath
     {
-        if (!isset($this->xPath)) {
+        if ($this->xPath === null) {
             /** @psalm-suppress InaccessibleProperty */
             $this->xPath = SafeDOMXPath::fromString(
                 $this->originalXmlConfigContent,

tests/phpunit/TestFramework/SafeDOMXPath/SafeDOMXPathTest.php
@@ -148,15 +148,15 @@ final class SafeDOMXPathTest extends TestCase
 
         $firstElement = $xPath->document->firstElementChild;
 
-        // @phpstan-ignore property.nonObject
         // Beware: this is only the _document_ namespace, not a namespace registered to the XPath.
         $this->assertSame(
             'https://schema.phpunit.de/coverage/1.0',
+            // @phpstan-ignore property.nonObject
             $firstElement->namespaceURI,
             'Expected the document namespace to be left alone.',
         );
-        // @phpstan-ignore property.nonObject
         // Sanity check: ensuring we correctly parsed the XML.
+        // @phpstan-ignore property.nonObject
         $this->assertSame('phpunit', $firstElement->tagName);
 
         // Check that no namespace was registered.
@@ -201,8 +201,8 @@ final class SafeDOMXPathTest extends TestCase
 
         // @phpstan-ignore property.nonObject
         $this->assertNull($firstElement->namespaceURI);
-        // @phpstan-ignore property.nonObject
         // Sanity check: ensuring we correctly parsed the XML.
+        // @phpstan-ignore property.nonObject
         $this->assertSame('phpunit', $firstElement->tagName);
 
         // Since no namespace is registered and the document has no namespace,