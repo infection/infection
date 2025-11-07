37d5a45df13ad064f650a084bb68c8aba0061422
Alexey Kopytko <alexey@kopytko.com>
2025-11-07 18:58:11 +0900

    revert

--- tests/phpunit/TestFramework/SafeDOMXPath/SafeDOMXPathTest.php
+++ tests/phpunit/TestFramework/SafeDOMXPath/SafeDOMXPathTest.php
@@ -85,6 +85,7 @@ final class SafeDOMXPathTest extends TestCase
 
         $firstElement = $xPath->document->firstElementChild;
 
+        // @phpstan-ignore property.nonObject
         $this->assertSame($expectedFirstElementTagName, $firstElement->tagName);
     }
 
@@ -147,12 +148,14 @@ final class SafeDOMXPathTest extends TestCase
 
         $firstElement = $xPath->document->firstElementChild;
 
+        // @phpstan-ignore property.nonObject
         // Beware: this is only the _document_ namespace, not a namespace registered to the XPath.
         $this->assertSame(
             'https://schema.phpunit.de/coverage/1.0',
             $firstElement->namespaceURI,
             'Expected the document namespace to be left alone.',
         );
+        // @phpstan-ignore property.nonObject
         // Sanity check: ensuring we correctly parsed the XML.
         $this->assertSame('phpunit', $firstElement->tagName);
 
@@ -196,7 +199,9 @@ final class SafeDOMXPathTest extends TestCase
 
         $firstElement = $xPath->document->firstElementChild;
 
+        // @phpstan-ignore property.nonObject
         $this->assertNull($firstElement->namespaceURI);
+        // @phpstan-ignore property.nonObject
         // Sanity check: ensuring we correctly parsed the XML.
         $this->assertSame('phpunit', $firstElement->tagName);
 
