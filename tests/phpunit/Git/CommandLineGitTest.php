0bc71ee13fdff486274e2ac46a7dd3c17b8f397b
Théo FIDRY <theo.fidry@gmail.com>
2025-12-02 14:09:20 +0100

    add missing index

--- tests/phpunit/Git/CommandLineGitTest.php
+++ tests/phpunit/Git/CommandLineGitTest.php
@@ -151,6 +151,9 @@ final class CommandLineGitTest extends TestCase
         yield 'one file with added lines in different places' => [
             <<<'DIFF'
                 diff --git a/src/Container.php b/src/Container.php
+                index 2a9e281..01cbf04 100644
+                --- a/src/Container.php
+                +++ b/src/Container.php
                 @@ -37,0 +38 @@ namespace Infection;
                 @@ -533 +534,2 @@ final class Container
                 @@ -535,0 +538,3 @@ final class Container
@@ -169,11 +172,17 @@ final class CommandLineGitTest extends TestCase
         yield 'two files, second one is new created' => [
             <<<'DIFF'
                 diff --git a/src/Container.php b/src/Container.php
+                index 2a9e281..01cbf04 100644
+                --- a/src/Container.php
+                +++ b/src/Container.php
                 @@ -37,0 +38 @@ namespace Infection;
                 @@ -533 +534,2 @@ final class Container
                 @@ -535,0 +538,3 @@ final class Container
                 @@ -1207,0 +1213,5 @@ final class Container
                 diff --git a/src/Differ/FilesDiffChangedLines.php b/src/Differ/FilesDiffChangedLines.php
+                index 2a9e281..01cbf04 100644
+                --- a/src/Differ/FilesDiffChangedLines.php
+                +++ b/src/Differ/FilesDiffChangedLines.php
                 new file mode 100644
                 @@ -0,0 +1,18 @@
                 DIFF,
