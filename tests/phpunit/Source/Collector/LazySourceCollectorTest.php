4404dfa3c35935dc7eb070bb9f70128a14d5a06c
Théo FIDRY <theo.fidry@gmail.com>
2025-12-11 11:30:52 +0100

    fix

--- tests/phpunit/Source/Collector/LazySourceCollectorTest.php
+++ tests/phpunit/Source/Collector/LazySourceCollectorTest.php
@@ -33,7 +33,7 @@
 
 declare(strict_types=1);
 
-namespace Infection\Tests\Source\Collector\LazyCacheSourceCollector;
+namespace Infection\Tests\Source\Collector;
 
 use Infection\Source\Collector\FakeSourceCollector;
 use Infection\Source\Collector\FixedSourceCollector;
