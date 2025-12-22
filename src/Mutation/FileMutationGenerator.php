infection/infection

commit 4a978f2d51b3d0c5663cfcfc6a7c6d3166cc81cf

feat: Introduce the Tracer (#2707)
This PR completes a 6 years old challenge:
https://github.com/infection/infection/pull/927

We introduced back then `Trace`, which is an envelope for a source file
which contains all the necessary data about its tests. The `Tracer` is
the service that provides such a trace object for a given source file.

## Motivation

Currently we work as follow:

- go through the coverage data (the XML coverage index, in
`PhpUnitXmlCoverageTraceProvider`)
- for each tests, find the source the test is about
- filter out the tests for which the source is not part of the collected
sources (this is `CoveredTraceProvider` using
`BufferedSourceFileFilter`)
- if we allow non-covered code, we create a `ProxyTrace` for each source
file that was not found in the tests (this is `UncoveredTraceProvider`
using `BufferedSourceFileFilter`)
- for each trace, start the mutation generation process

One one hand this makes sense because we start from the data we have:
the coverage. On another, it seems a bit weird to start from the
coverage which may contain a lot more than necessary, especially if we
request specific source files (via filters).

In [Architecture
PoC](https://github.com/infection/infection/pull/2272/files#diff-9bc3105e506d7103f2b25138650f3e784a373d04b529f12ff34136a2fccc72c6),
the proposal is to:

- parse the source file
- enrich the AST (mark what code is eligible, dry areas, add types,
etc.)
- starting the mutation generation process

So for that purpose, the current architecture is unfit as we start from
the wrong end. The `Tracer` allows to inverse the control flow: we
request the data (the `Trace`) for a given source file, not the other
way around.


## The solution

The API of the Tracer is fairly simple, and in all implementation
attempts, it always ended up with the same API.

The implementation idea draws inspiration from some experiments I did
when working on the coverage data in

src/Mutation/FileMutationGenerator.php
@@ -45,8 +45,9 @@ use Infection\PhpParser\Visitor\MutationCollectorVisitor;
 use Infection\Source\Exception\NoSourceFound;
 use Infection\Source\Matcher\SourceLineMatcher;
 use Infection\TestFramework\Tracing\Trace\LineRangeCalculator;
-use Infection\TestFramework\Tracing\Trace\Trace;
+use Infection\TestFramework\Tracing\Tracer;
 use PhpParser\Node;
+use Symfony\Component\Finder\SplFileInfo;
 use Webmozart\Assert\Assert;
 
 /**
@@ -60,6 +61,7 @@ class FileMutationGenerator
         private readonly NodeTraverserFactory $traverserFactory,
         private readonly LineRangeCalculator $lineRangeCalculator,
         private readonly SourceLineMatcher $sourceLineMatcher,
+        private readonly Tracer $tracer,
     ) {
     }
 
@@ -73,7 +75,7 @@ class FileMutationGenerator
      * @return iterable<Mutation>
      */
     public function generate(
-        Trace $trace,
+        SplFileInfo $sourceFile,
         bool $onlyCovered,
         array $mutators,
         array $nodeIgnorers,
@@ -81,11 +83,13 @@ class FileMutationGenerator
         Assert::allIsInstanceOf($mutators, Mutator::class);
         Assert::allIsInstanceOf($nodeIgnorers, NodeIgnorer::class);
 
-        if ($onlyCovered && !$trace->hasTests()) {
+        // If $onlyCovered, then for a non-covered source file we will
+        // get an empty trace. This is configured at the tracer level.
+        if (!$this->tracer->hasTrace($sourceFile)) {
             return;
         }
 
-        $sourceFile = $trace->getSourceFileInfo();
+        $trace = $this->tracer->trace($sourceFile);
         [$initialStatements, $originalFileTokens] = $this->parser->parse($sourceFile);
 
         // Pre-traverse the nodes to connect them
@@ -95,7 +99,7 @@ class FileMutationGenerator
         $mutationCollectorVisitor = new MutationCollectorVisitor(
             new NodeMutationGenerator(
                 mutators: $mutators,
-                filePath: $trace->getRealPath(),
+                filePath: $sourceFile->getRealPath(),
                 fileNodes: $initialStatements,
                 trace: $trace,
                 onlyCovered: $onlyCovered,

tests/phpunit/Mutation/FileMutationGeneratorTest.php
@@ -46,8 +46,10 @@ use Infection\PhpParser\FileParser;
 use Infection\PhpParser\NodeTraverserFactory;
 use Infection\PhpParser\Visitor\MutationCollectorVisitor;
 use Infection\Source\Matcher\SourceLineMatcher;
+use Infection\TestFramework\Tracing\Trace\EmptyTrace;
 use Infection\TestFramework\Tracing\Trace\LineRangeCalculator;
 use Infection\TestFramework\Tracing\Trace\Trace;
+use Infection\TestFramework\Tracing\Tracer;
 use Infection\Testing\MutatorName;
 use Infection\Testing\SingletonContainer;
 use Infection\Tests\Fixtures\PhpParser\FakeIgnorer;
@@ -73,50 +75,50 @@ final class FileMutationGeneratorTest extends TestCase
 
     private MockObject&NodeTraverserFactory $traverserFactoryMock;
 
-    private FileMutationGenerator $mutationGenerator;
+    private MockObject&Tracer $tracerMock;
 
-    private MockObject&SourceLineMatcher $sourceLineMatcherMock;
+    private FileMutationGenerator $mutationGenerator;
 
     protected function setUp(): void
     {
         $this->fileParserMock = $this->createMock(FileParser::class);
         $this->traverserFactoryMock = $this->createMock(NodeTraverserFactory::class);
-        $this->sourceLineMatcherMock = $this->createMock(SourceLineMatcher::class);
+        $this->tracerMock = $this->createMock(Tracer::class);
 
         $this->mutationGenerator = new FileMutationGenerator(
             $this->fileParserMock,
             $this->traverserFactoryMock,
             new LineRangeCalculator(),
-            $this->sourceLineMatcherMock,
+            $this->createMock(SourceLineMatcher::class),
+            $this->tracerMock,
         );
     }
 
     public function test_it_generates_mutations_for_a_given_file(): void
     {
-        $traceMock = $this->createTraceMock(
-            self::FIXTURES_DIR . '/Mutation/OneFile/OneFile.php',
-            '',
-            '',
-        );
-        $traceMock
-            ->expects($this->never())
-            ->method('hasTests')
-        ;
-        $traceMock
-            ->expects($this->once())
-            ->method('getAllTestsForMutation')
-            ->willReturn([])
-        ;
-        $traceMock
-            ->expects($this->once())
-            ->method('getRealPath')
-            ->willReturn(self::FIXTURES_DIR . '/Mutation/OneFile/OneFile.php')
-        ;
+        $fileInfoMock = $this->createSplFileInfoMock(self::FIXTURES_DIR . '/Mutation/OneFile/OneFile.php');
 
-        $mutationGenerator = SingletonContainer::getContainer()->getFileMutationGenerator();
+        $this->tracerMock
+            ->expects($this->once())
+            ->method('hasTrace')
+            ->with($fileInfoMock)
+            ->willReturn(true);
+        $this->tracerMock
+            ->expects($this->once())
+            ->method('trace')
+            ->with($fileInfoMock)
+            ->willReturn(new EmptyTrace($fileInfoMock));
+
+        $mutationGenerator = new FileMutationGenerator(
+            SingletonContainer::getContainer()->getFileParser(),
+            SingletonContainer::getContainer()->getNodeTraverserFactory(),
+            SingletonContainer::getContainer()->getLineRangeCalculator(),
+            SingletonContainer::getContainer()->getSourceLineMatcher(),
+            $this->tracerMock,
+        );
 
         $mutations = $mutationGenerator->generate(
-            $traceMock,
+            $fileInfoMock,
             false,
             [new IgnoreMutator(new IgnoreConfig([]), new Plus())],
             [],
@@ -184,15 +186,23 @@ final class FileMutationGeneratorTest extends TestCase
             ->willReturnOnConsecutiveCalls($preTraverserMock, $traverserMock)
         ;
 
-        $traceMock = $this->createTraceMock(
+        $fileInfoMock = $this->createSplFileInfoMock(
             '/path/to/file',
             'relativePath',
             'relativePathName',
-            true,
         );
 
+        $this->tracerMock
+            ->method('hasTrace')
+            ->with($fileInfoMock)
+            ->willReturn(true);
+        $this->tracerMock
+            ->method('trace')
+            ->with($fileInfoMock)
+            ->willReturn(new EmptyTrace($fileInfoMock));
+
         $mutations = $this->mutationGenerator->generate(
-            $traceMock,
+            $fileInfoMock,
             false,
             [new IgnoreMutator(new IgnoreConfig([]), new Plus())],
             $nodeIgnorers,
@@ -258,15 +268,24 @@ final class FileMutationGeneratorTest extends TestCase
             ->willReturn($traverserMock)
         ;
 
-        $traceMock = $this->createTraceMock(
-            $file,
-            $relativePath,
-            $relativePathname,
-            $hasTests,
-        );
+        $fileInfoMock = $this->createSplFileInfoMock($file, $relativePath, $relativePathname);
+
+        $traceMock = $this->createMock(Trace::class);
+        $traceMock
+            ->method('hasTests')
+            ->willReturn($hasTests);
+
+        $this->tracerMock
+            ->method('hasTrace')
+            ->with($fileInfoMock)
+            ->willReturn($hasTests);
+        $this->tracerMock
+            ->method('trace')
+            ->with($fileInfoMock)
+            ->willReturn($traceMock);
 
         $mutations = $this->mutationGenerator->generate(
-            $traceMock,
+            $fileInfoMock,
             $onlyCovered,
             [new IgnoreMutator(new IgnoreConfig([]), new Plus())],
             $nodeIgnorers,
@@ -299,22 +318,24 @@ final class FileMutationGeneratorTest extends TestCase
             ->method('createPreTraverser')
         ;
 
-        $mutationGenerator = new FileMutationGenerator(
-            $this->fileParserMock,
-            $this->traverserFactoryMock,
-            new LineRangeCalculator(),
-            $this->sourceLineMatcherMock,
-        );
+        $fileInfoMock = $this->createSplFileInfoMock($file, $relativePath, $relativePathname);
 
-        $traceMock = $this->createTraceMock(
-            $file,
-            $relativePath,
-            $relativePathname,
-            $hasTests,
-        );
+        $traceMock = $this->createMock(Trace::class);
+        $traceMock
+            ->method('hasTests')
+            ->willReturn($hasTests);
 
-        $mutations = $mutationGenerator->generate(
-            $traceMock,
+        $this->tracerMock
+            ->method('hasTrace')
+            ->with($fileInfoMock)
+            ->willReturn($hasTests);
+        $this->tracerMock
+            ->method('trace')
+            ->with($fileInfoMock)
+            ->willReturn($traceMock);
+
+        $mutations = $this->mutationGenerator->generate(
+            $fileInfoMock,
             true,
             [new IgnoreMutator(new IgnoreConfig([]), new Plus())],
             [],
@@ -400,34 +421,10 @@ final class FileMutationGeneratorTest extends TestCase
         yield from [true, false];
     }
 
-    private function createTraceMock(
-        string $file,
-        string $relativePath,
-        string $relativePathname,
-        ?bool $hasTests = null,
-    ): Trace&MockObject {
-        $fileInfoMock = $this->createSplFileInfoMock($file, $relativePath, $relativePathname);
-
-        $traceMock = $this->createMock(Trace::class);
-        $traceMock
-            ->method('getSourceFileInfo')
-            ->willReturn($fileInfoMock)
-        ;
-
-        if ($hasTests !== null) {
-            $traceMock
-                ->method('hasTests')
-                ->willReturn($hasTests)
-            ;
-        }
-
-        return $traceMock;
-    }
-
     private function createSplFileInfoMock(
         string $file,
-        string $relativePath,
-        string $relativePathname,
+        string $relativePath = '',
+        string $relativePathname = '',
     ): SplFileInfo&MockObject {
         $splFileInfoMock = $this->createMock(SplFileInfo::class);
         $splFileInfoMock->method('__toString')->willReturn($file);

devTools/phpstan-baseline.neon
@@ -547,12 +547,6 @@ parameters:
 			path: ../tests/phpunit/Mutation/FileMutationGeneratorTest.php
 
 		-
-			message: '#^Parameter \#2 \$lazyTestLocations of class Infection\\TestFramework\\Tracing\\Trace\\ProxyTrace constructor expects Later\\Interfaces\\Deferred\<Infection\\TestFramework\\Tracing\\Trace\\TestLocations\>\|null, Later\\Interfaces\\Deferred\<int\> given\.$#'
-			identifier: argument.type
-			count: 2
-			path: ../tests/phpunit/Mutation/MutationGeneratorTest.php
-
-		-
 			message: '#^Parameter \#2 \$stmts of class PhpParser\\Node\\Stmt\\Namespace_ constructor expects array\<PhpParser\\Node\\Stmt\>\|null, array\<int, PhpParser\\Node\\Scalar\\Int_\> given\.$#'
 			identifier: argument.type
 			count: 5

devTools/phpstan.neon
@@ -146,6 +146,12 @@ parameters:
             message: '#::profile\(\) should return int but returns int.*\|null#'
             path: ../tests/benchmark/DummyInstrumentor.php
 
+        # This plugin does not seem to understand the PHPBench data providers
+        -
+            identifier: shipmonk.deadMethod
+            message: '#Unused .+Bench::provide*#'
+            path: ../tests/benchmark/*/*Bench.php
+
         # Test builders provide fluent API methods that may not be used immediately
         -
             identifier: shipmonk.deadMethod

src/Container.php
@@ -151,6 +151,8 @@ use Infection\TestFramework\Coverage\XmlReport\XmlCoverageParser;
 use Infection\TestFramework\Factory;
 use Infection\TestFramework\TestFrameworkExtraOptionsFilter;
 use Infection\TestFramework\Tracing\Trace\LineRangeCalculator;
+use Infection\TestFramework\Tracing\TraceProviderAdapterTracer;
+use Infection\TestFramework\Tracing\Tracer;
 use OndraM\CiDetector\CiDetector;
 use function php_ini_loaded_file;
 use PhpParser\Parser;
@@ -248,6 +250,9 @@ final class Container extends DIContainer
                 $container->getPhpUnitXmlCoverageTraceProvider(),
                 $container->getJUnitTestExecutionInfoAdder(),
                 $container->getBufferedSourceFileFilter(),
+            ),
+            Tracer::class => static fn (self $container) => new TraceProviderAdapterTracer(
+                $container->getUnionTraceProvider(),
             ),
             UnionTraceProvider::class => static fn (self $container): UnionTraceProvider => new UnionTraceProvider(
                 $container->getCoveredTraceProvider(),
@@ -447,6 +452,7 @@ final class Container extends DIContainer
                 $container->getNodeTraverserFactory(),
                 $container->getLineRangeCalculator(),
                 $container->getSourceLineMatcher(),
+                $container->getTracer(),
             ),
             FileLoggerFactory::class => static function (self $container): FileLoggerFactory {
                 $config = $container->getConfiguration();
@@ -530,11 +536,10 @@ final class Container extends DIContainer
                 $config = $container->getConfiguration();
 
                 return new MutationGenerator(
-                    $container->getUnionTraceProvider(),
+                    $container->getSourceCollector(),
                     $config->mutators,
                     $container->getEventDispatcher(),
                     $container->getFileMutationGenerator(),
-                    $config->noProgress,
                 );
             },
             MutationTestingRunner::class => static function (self $container): MutationTestingRunner {
@@ -721,6 +726,11 @@ final class Container extends DIContainer
     public function getFileSystem(): FileSystem
     {
         return $this->get(FileSystem::class);
+    }
+
+    public function getTracer(): Tracer
+    {
+        return $this->get(Tracer::class);
     }
 
     public function getUnionTraceProvider(): UnionTraceProvider

src/Mutation/MutationGenerator.php
@@ -35,22 +35,21 @@ declare(strict_types=1);
 
 namespace Infection\Mutation;
 
+use function count;
 use Infection\Event\EventDispatcher\EventDispatcher;
 use Infection\Event\MutableFileWasProcessed;
 use Infection\Event\MutationGenerationWasFinished;
 use Infection\Event\MutationGenerationWasStarted;
-use Infection\Framework\Iterable\IterableCounter;
 use Infection\Mutator\Mutator;
 use Infection\PhpParser\UnparsableFile;
 use Infection\PhpParser\Visitor\IgnoreNode\NodeIgnorer;
+use Infection\Source\Collector\SourceCollector;
 use Infection\Source\Exception\NoSourceFound;
 use Infection\TestFramework\Coverage\JUnit\TestFileNameNotFoundException;
 use Infection\TestFramework\Coverage\Locator\Throwable\NoReportFound;
 use Infection\TestFramework\Coverage\Locator\Throwable\ReportLocationThrowable;
 use Infection\TestFramework\Coverage\Locator\Throwable\TooManyReportsFound;
 use Infection\TestFramework\Coverage\XmlReport\InvalidCoverage;
-use Infection\TestFramework\Tracing\Trace\Trace;
-use Infection\TestFramework\Tracing\TraceProvider;
 use PhpParser\Node;
 use Webmozart\Assert\Assert;
 
@@ -67,11 +66,10 @@ class MutationGenerator
      * @param Mutator<Node>[] $mutators
      */
     public function __construct(
-        private readonly TraceProvider $traceProvider,
+        private readonly SourceCollector $sourceCollector,
         array $mutators,
         private readonly EventDispatcher $eventDispatcher,
         private readonly FileMutationGenerator $fileMutationGenerator,
-        private readonly bool $runConcurrently,
     ) {
         Assert::allIsInstanceOf($mutators, Mutator::class);
         $this->mutators = $mutators;
@@ -93,16 +91,14 @@ class MutationGenerator
      */
     public function generate(bool $onlyCovered, array $nodeIgnorers): iterable
     {
-        $traces = $this->traceProvider->provideTraces();
-
-        $numberOfFiles = IterableCounter::bufferAndCountIfNeeded($traces, $this->runConcurrently);
+        $sources = $this->sourceCollector->collect();
+        $numberOfFiles = count($sources);
 
         $this->eventDispatcher->dispatch(new MutationGenerationWasStarted($numberOfFiles));
 
-        /** @var Trace $trace */
-        foreach ($traces as $trace) {
+        foreach ($sources as $source) {
             yield from $this->fileMutationGenerator->generate(
-                $trace,
+                $source,
                 $onlyCovered,
                 $this->mutators,
                 $nodeIgnorers,

src/TestFramework/Tracing/TraceProviderAdapterTracer.php
@@ -1,0 +1,145 @@
+<?php
+/**
+ * This code is licensed under the BSD 3-Clause License.
+ *
+ * Copyright (c) 2017, Maks Rafalko
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+declare(strict_types=1);
+
+namespace Infection\TestFramework\Tracing;
+
+use function array_key_exists;
+use Generator;
+use Infection\Framework\Iterable\GeneratorFactory;
+use Infection\TestFramework\Tracing\Trace\Trace;
+use SplFileInfo;
+use Webmozart\Assert\Assert;
+
+/**
+ * Note that this implementation is not meant as a long-lived one. The goal
+ * is to phase out TraceProvider.
+ *
+ * @internal
+ */
+final class TraceProviderAdapterTracer implements Tracer
+{
+    private bool $traversed = false;
+
+    /**
+     * @var Generator<Trace>|null
+     */
+    private ?Generator $traceGenerator = null;
+
+    /**
+     * This is effectively used as a cache. Note that whilst it would be trivial
+     * to extract a CachedTracer implementation, this would make this implementation
+     * extremely brittle and unusable without the CachedTracer. Since the coupling
+     * is so tight, although we could technically decouple it thanks to interface,
+     * I decided against it to make the coupling explicit.
+     *
+     * @var array<string, Trace|null> Traces indexed by their source pathname.
+     */
+    private array $indexedTraces = [];
+
+    public function __construct(
+        private readonly TraceProvider $traceProvider,
+    ) {
+    }
+
+    public function hasTrace(SplFileInfo $fileInfo): bool
+    {
+        return $this->tryToTrace($fileInfo) !== null;
+    }
+
+    public function trace(SplFileInfo $fileInfo): Trace
+    {
+        $trace = $this->tryToTrace($fileInfo);
+
+        // We do not bother with a nice error here as `::hasTrace()` should
+        // have been used to ensure a trace exists in the first place.
+        Assert::notNull($trace);
+
+        return $trace;
+    }
+
+    private function tryToTrace(SplFileInfo $fileInfo): ?Trace
+    {
+        $sourcePathname = $fileInfo->getPathname();
+
+        return array_key_exists($sourcePathname, $this->indexedTraces)
+            ? $this->indexedTraces[$sourcePathname]
+            : $this->lookup($sourcePathname);
+    }
+
+    private function lookup(string $sourcePathname): ?Trace
+    {
+        if ($this->traversed) {
+            // We already got all the traces, yet it was not found.
+            // Cache the fact that no trace exists for that source file.
+            $this->indexedTraces[$sourcePathname] = null;
+
+            return null;
+        }
+
+        $traces = $this->getTraceGenerator();
+
+        // Do not use a foreach loop as it does a rewind which we do not want
+        // to do.
+        while ($traces->valid()) {
+            /** @var Trace $trace */
+            $trace = $traces->current();
+            $traces->next();
+
+            $traceSourcePathname = $trace->getSourceFileInfo()->getPathname();
+            $this->indexedTraces[$traceSourcePathname] = $trace;
+
+            if ($traceSourcePathname === $sourcePathname) {
+                return $trace;
+            }
+        }
+
+        $this->indexedTraces[$sourcePathname] = null;
+        $this->traversed = true;
+
+        return null;
+    }
+
+    /**
+     * @return Generator<Trace>
+     */
+    private function getTraceGenerator(): Generator
+    {
+        if ($this->traceGenerator === null) {
+            $this->traceGenerator = GeneratorFactory::fromIterable($this->traceProvider->provideTraces());
+        }
+
+        return $this->traceGenerator;
+    }
+}

src/TestFramework/Tracing/Tracer.php
@@ -33,66 +33,24 @@
 
 declare(strict_types=1);
 
-namespace Infection\Benchmark\Tracing;
+namespace Infection\TestFramework\Tracing;
 
-use Closure;
-use const PHP_INT_MAX;
-use PhpBench\Attributes\AfterMethods;
-use PhpBench\Attributes\BeforeMethods;
-use PhpBench\Attributes\Iterations;
-use PhpBench\Attributes\ParamProviders;
-use Webmozart\Assert\Assert;
+use Infection\TestFramework\Tracing\Trace\Trace;
+use Symfony\Component\Finder\SplFileInfo;
 
 /**
- * To execute this test run `make benchmark_tracing`
+ * The test framework Tracer is the service responsible for creating a Trace for a given file, i.e.
+ * to find all the necessary information about the tests for that file.
+ *
+ * @internal
  */
-final class TracingBench
+interface Tracer
 {
-    private Closure $main;
-
-    private int $count;
-
-    public function setUp(): void
-    {
-        $this->main = (require __DIR__ . '/create-main.php')(PHP_INT_MAX);
-    }
-
-    public function tearDown(): void
-    {
-        Assert::greaterThan(
-            $this->count,
-            0,
-            'No trace was generated.',
-        );
-    }
+    public function hasTrace(SplFileInfo $fileInfo): bool;
 
     /**
-     * @param array{float} $params
+     * Beware! Whilst the absence of Trace guarantees the absence of tests, its reciprocal is false.
+     * A trace may end up being empty.
      */
-    #[BeforeMethods('setUp')]
-    #[AfterMethods('tearDown')]
-    #[Iterations(5)]
-    #[ParamProviders('providePercentile')]
-    public function bench(array $params): void
-    {
-        $percentage = (float) $params[0];
-
-        $this->count = ($this->main)($percentage);
-    }
-
-    /**
-     * @return iterable<array{float}>
-     */
-    public static function providePercentile(): iterable
-    {
-        yield '10%' => [.1];
-
-        yield '25%' => [.25];
-
-        yield '50%' => [.5];
-
-        yield '75%' => [.75];
-
-        yield '100%' => [1.];
-    }
+    public function trace(SplFileInfo $fileInfo): Trace;
 }

commit c881c41846e5ea5fe892c4ecb035e1332cf6efee

fix: Checks for the trace steps before proceeding with the parsing of a file (#2734)
Fixes an issue introduced in #2707.

In there I did the mistake of conflicting the existence of a trace with
the existence of tests. We previously (before #2707) were checking the
existence of tests for the trace, this should remain.

This PR introduces this back. Thanks to #2737, it is easy to understand
the difference in tests.

src/Mutation/FileMutationGenerator.php
@@ -44,6 +44,7 @@ use Infection\PhpParser\Visitor\IgnoreNode\NodeIgnorer;
 use Infection\PhpParser\Visitor\MutationCollectorVisitor;
 use Infection\Source\Exception\NoSourceFound;
 use Infection\Source\Matcher\SourceLineMatcher;
+use Infection\TestFramework\Tracing\Trace\EmptyTrace;
 use Infection\TestFramework\Tracing\Trace\LineRangeCalculator;
 use Infection\TestFramework\Tracing\Tracer;
 use PhpParser\Node;
@@ -83,13 +84,14 @@ class FileMutationGenerator
         Assert::allIsInstanceOf($mutators, Mutator::class);
         Assert::allIsInstanceOf($nodeIgnorers, NodeIgnorer::class);
 
-        // If $onlyCovered, then for a non-covered source file we will
-        // get an empty trace. This is configured at the tracer level.
-        if (!$this->tracer->hasTrace($sourceFile)) {
+        $trace = $this->tracer->hasTrace($sourceFile)
+            ? $this->tracer->trace($sourceFile)
+            : new EmptyTrace($sourceFile);
+
+        if ($onlyCovered && !$trace->hasTests()) {
             return;
         }
 
-        $trace = $this->tracer->trace($sourceFile);
         [$initialStatements, $originalFileTokens] = $this->parser->parse($sourceFile);
 
         // Pre-traverse the nodes to connect them

tests/phpunit/Mutation/FileMutationGenerator/FileMutationGeneratorTest.php
@@ -278,9 +278,10 @@ final class FileMutationGeneratorTest extends TestCase
 
         yield 'nominal' => [$nominalScenario];
 
-        yield 'onlyCovered=true: do not skip generation if no tests' => [
+        yield 'onlyCovered=true: skip generation if no tests' => [
             $nominalScenario
-                ->withTraceHasTests(false),
+                ->withTraceHasTests(false)
+                ->withExpected(false),
         ];
 
         yield 'onlyCovered=true: skip generation if no trace' => [
@@ -295,11 +296,17 @@ final class FileMutationGeneratorTest extends TestCase
                 ->withTraceHasTests(false),
         ];
 
-        yield 'onlyCovered=false: skip generation if no trace' => [
+        yield 'onlyCovered=false: do not skip generation if no trace' => [
+            $nominalScenario
+                ->withOnlyCovered(false)
+                ->withHasTrace(false),
+        ];
+
+        yield 'onlyCovered=false: do not skip generation if no trace and no tests' => [
             $nominalScenario
                 ->withOnlyCovered(false)
                 ->withHasTrace(false)
-                ->withExpected(false),
+                ->withTraceHasTests(false),
         ];
     }
 