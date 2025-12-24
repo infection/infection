71d4a12cbe62d79c06dce628b8a5fe234636424c
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2025-12-24 14:55:03 +0100

    test: Rework the PHPUnit end-to-end tests (#2624)
    ## Backgorund
    
    For #2543 I need to touch a lot of the coverage part and one thing I am
    missing is getting hands on a good enough sample of coverage for various
    test frameworks and different versions of those.
    
    We've had a mix of fixtures:
    
    - I introduced some for the tracing test in
    `/Users/tfidry/Project/Humbug/infection/tests/phpunit/TestFramework/Tracing/Fixtures`
    in ##2444.
    - We have some comprehensive ones with a bit more information about what
    wee get from the coverage data with
    `/Users/tfidry/Project/Humbug/infection/tests/phpunit/Fixtures/TestFramework/PhpUnit/Coverage/*`
    (see #957)
    
    I'm however not too happy with the current state of things: it's hard to
    find the right balance to maintain them.
    
    
    ## Solution
    
    The idea of this PR is to leverage the end-to-end tests. We have e2e
    tests for the various test frameworks with their different
    setup/versions. What we can do is to actually enrich them a bit to be
    able to generate coverage from them, when needed.
    
    We don't really need to go out of our way to automate it: getting the
    coverage for PHPUnit 9.3 for instance doesn't need to be done more. The
    day we need more out of it, e.g. we want to see how a trait is described
    in the XML coverage or something so we need to add a new class/tests.
    Then we can pick up the generated coverage and place it where needed.
    
    ## Implementation
    
    This PR updates the code to offer a better range of covered and
    non-covered code as well as a `Makefile` command to make the test
    generation easy.
    
    I plan to touch the PhpSpec & Codeception in a similar fashion too.

---
+++ tests/e2e/PHPUnit_10-1/src/Uncovered/functions.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Infection\E2ETests\PHPUnit_10_1\Uncovered;
+
+function formatName(string $firstName, string $lastName): string
+{
+    if (empty($firstName) && empty($lastName)) {
+        return 'Anonymous';
+    }
+
+    if (empty($firstName)) {
+        return $lastName;
+    }
+
+    if (empty($lastName)) {
+        return $firstName;
+    }
+
+    return "{$firstName} {$lastName}";
+}
+
+function calculateDiscount(float $price, int $discountPercent): float
+{
+    if ($discountPercent < 0 || $discountPercent > 100) {
+        throw new \InvalidArgumentException('Discount must be between 0 and 100');
+    }
+
+    if ($price <= 0) {
+        return 0.0;
+    }
+
+    $discount = ($price * $discountPercent) / 100;
+    return $price - $discount;
+}
+
+function isValidEmail(string $email): bool
+{
+    if (empty($email)) {
+        return false;
+    }
+
+    return str_contains($email, '@') && str_contains($email, '.');
+}
--- tests/e2e/PHPUnit_11/README.md
+++ tests/e2e/PHPUnit_11/README.md
@@ -1,9 +1,18 @@
-# Title
-
-* https://github.com/infection/infection/pull/2001
-* https://github.com/infection/infection/pull/2003
-
 ## Summary
 
-With PHPUnit 11 the support for the attribute `cacheResultFile` was removed.  
-This test is to ensure that the `cacheResultFile` is not used with phpunit >=11.
+The goal of this test is to ensure Infection works with PHPUnit 10.1+.
+
+See:
+
+- https://github.com/infection/infection/pull/2001
+- https://github.com/infection/infection/pull/2003
+
+This project test contains:
+
+- A class `Calculator`.
+- A trait `LoggerTrait`.
+- A class using a trait `UserService`.
+- All of are present in two directories, a version covered in
+  `src/Covered` and uncovered in `src/Uncovered`.
+
+The coverage data can be generated with `make phpunit-coverage`.
