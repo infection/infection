a4f3e6ba25350801c9211080f9d5eb079b75f4cf
Théo FIDRY <theo.fidry@gmail.com>
2026-01-03 21:44:45 +0100

    move things

---
+++ tests/phpunit/PhpParser/Visitor/IgnoreNode/InterfaceIgnorerTest.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * This code is licensed under the BSD 3-Clause License.
+ *
+ * Copyright (c) 2017, Maks Rafalko
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+declare(strict_types=1);
+
+namespace Infection\Tests\PhpParser\Visitor\IgnoreNode;
+
+use Infection\PhpParser\Visitor\IgnoreNode\InterfaceIgnorer;
+use Infection\PhpParser\Visitor\NonMutableNodesIgnorerVisitor;
+use Infection\Tests\PhpParser\Visitor\MarkTraversedNodesAsVisitedVisitor\MarkTraversedNodesAsVisitedVisitor;
+use Infection\Tests\PhpParser\Visitor\VisitorTestCase;
+use PhpParser\NodeTraverser;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+#[CoversClass(InterfaceIgnorer::class)]
+final class InterfaceIgnorerTest extends VisitorTestCase
+{
+    #[DataProvider('nodeProvider')]
+    public function test_it_annotates_excluded_nodes_and_stops_the_traversal(
+        string $code,
+        string $expected,
+    ): void {
+        $nodes = $this->createParser()->parse($code);
+
+        $traverser = new NodeTraverser(
+            new NonMutableNodesIgnorerVisitor([new InterfaceIgnorer()]),
+            new MarkTraversedNodesAsVisitedVisitor(),
+        );
+        $traverser->traverse($nodes);
+
+        $actual = $this->dumper->dump($nodes);
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public static function nodeProvider(): iterable
+    {
+        yield 'interfaces are ignored' => [
+            <<<'PHP'
+                <?php
+
+                interface Bar
+                {
+                    public function nope(Bar $ignored): void;
+                }
+
+                PHP,
+            <<<'AST'
+                array(
+                    0: <skipped>
+                )
+                AST,
+        ];
+
+        yield 'classes are not ignored' => [
+            <<<'PHP'
+                <?php
+
+                class Bar
+                {
+                    public function nope(Bar $counted)
+                    {
+                        $counted = true;
+                    }
+                }
+
+                PHP,
+            <<<'AST'
+                array(
+                    0: Stmt_Class(
+                        name: Identifier
+                        stmts: array(
+                            0: Stmt_ClassMethod(
+                                name: Identifier
+                                params: array(
+                                    0: Param(
+                                        type: Name
+                                        var: Expr_Variable
+                                    )
+                                )
+                                stmts: array(
+                                    0: Stmt_Expression(
+                                        expr: Expr_Assign(
+                                            var: Expr_Variable
+                                            expr: Expr_ConstFetch(
+                                                name: Name
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+                AST,
+        ];
+
+        yield 'it ignores only interfaces' => [
+            <<<'PHP'
+                <?php
+
+                interface Bar
+                {
+                    public function nope(Bar $ignored): void;
+                }
+
+                class Bar
+                {
+                    public function nope(Bar $counted)
+                    {
+                        $counted = true;
+                    }
+                }
+
+                PHP,
+            <<<'AST'
+                array(
+                    0: <skipped>
+                    1: Stmt_Class(
+                        name: Identifier
+                        stmts: array(
+                            0: Stmt_ClassMethod(
+                                name: Identifier
+                                params: array(
+                                    0: Param(
+                                        type: Name
+                                        var: Expr_Variable
+                                    )
+                                )
+                                stmts: array(
+                                    0: Stmt_Expression(
+                                        expr: Expr_Assign(
+                                            var: Expr_Variable
+                                            expr: Expr_ConstFetch(
+                                                name: Name
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+                AST,
+        ];
+    }
+}
