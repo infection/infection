37a3e35444843fc0aecd41911f9194e9133299bd
Théo FIDRY <theo.fidry@gmail.com>
2026-02-12 12:53:06 +0100

    test(ast): Add a test for the VisitorTestCase

--- tests/phpunit/TestingUtility/PhpParser/Visitor/AddIdToTraversedNodesVisitor/AddIdToTraversedNodesVisitorTest.php
+++ tests/phpunit/TestingUtility/PhpParser/Visitor/AddIdToTraversedNodesVisitor/AddIdToTraversedNodesVisitorTest.php
@@ -35,7 +35,7 @@ declare(strict_types=1);
 
 namespace Infection\Tests\TestingUtility\PhpParser\Visitor\AddIdToTraversedNodesVisitor;
 
-use Infection\Tests\PhpParser\Visitor\VisitorTestCase;
+use Infection\Tests\PhpParser\Visitor\VisitorTestCase\VisitorTestCase;
 use PhpParser\Node\Expr\Assign;
 use PhpParser\Node\Expr\Variable;
 use PhpParser\Node\Scalar\String_;
