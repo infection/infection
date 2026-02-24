066efe6467052394e92670c0a0555f19be57fd88
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2026-02-24 11:23:24 +0100

    refactor(event): Introduce InitialTestsExecutionLogger (#2978)
    ## Description
    
    This PR applies the same pattern that we employed for the mutation
    analysis phase to the initial tests execution.
    
    ## Motivations
    
    The motivations is the same as detailed
    [here](https://github.com/infection/infection/pull/2937#issuecomment-3859485503)
    for the mutation analysis: we try to have a better control and
    understanding about what is logged and when.
    
    The current scope is to explicitly declare what is part of logging via a
    logger interface, and move it out to the subscribers (to use
    implementations of the logger).
    
    Note that I am not 100% sure yet if in the future we will keep this
    interface or unify multiple loggers as one logger interface, but with
    the current change either situation is fine (the interface is fine as it
    is if we do not want to, and if we want to have only one logger
    interface it will be easy to do so).
    
    ## Changes
    
    - Introduce `InitialTestsExecutionLogger` (analogue to
    `MutationAnalysisLogger`)
    - Make `InitialTestsConsoleLoggerSubscriber` into a
    `InitialTestsExecutionLoggerSubscriber` which subscribers to the desired
    events and call the logger (analogue to
    `MutationAnalysisLoggerSubscriber`).
    - Transform `CiInitialTestsConsoleLoggerSubscriber` into a
    `InitialTestsExecutionLogger` implementation. Renamed it to
    `ConsoleNoProgressLogger`: it is in practice not CI specific hence I
    adjusted the name to reflect that.
    - Transform `InitialTestsConsoleLoggerSubscriber` into a
    `InitialTestsExecutionLogger`. Renamed it to `ConsoleProgressBarLogger`
    to highlight that it is 1) console specific 2) about having a progress
    bar: we could (and will) have non-progress bar implementations.
    
    ## Reviewer Notes
    
    - The same will be done for the other parts: static analysis, mutation
    generation (which I think make sense to move as part of the
    `MutationAnalysisLogger)`.
    - This is not highlighted here, but we have quite a bit of duplicated
    here especially in regards to the initial test execution and initial
    static analysis. This was highlighted by this and the sub-sequent
    refactorings. If this PR is not desired however this can still be done.
    
    
    ## Related issues
    
    - https://github.com/infection/infection/pull/2927

--- tests/phpunit/Event/Subscriber/MutationAnalysisLoggerSubscriberTest.php
+++ tests/phpunit/Event/Subscriber/MutationAnalysisLoggerSubscriberTest.php
@@ -48,11 +48,9 @@ use Infection\Process\Runner\ProcessRunner;
 use Infection\Tests\Mutant\MutantExecutionResultBuilder;
 use Infection\Tests\Mutation\MutationBuilder;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
-#[Group('integration')]
 #[CoversClass(MutationAnalysisLoggerSubscriber::class)]
 final class MutationAnalysisLoggerSubscriberTest extends TestCase
 {
