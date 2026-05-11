ee4687cab33092b8ddd2d155806a95881dd000fd
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2026-05-11 18:11:46 +0200

    test(telemetry): Make the e2e telemetry test more complete (#3125)
    ## Description
    
    Add PHPStan and reports to the `Telemetry_Console` end-to-end test. This
    allows to have a more complete (but still simple) trace that can be used
    for quick demos.
    
    This allows one to use a tracing system (e.g.
    [Jeager](https://www.jaegertracing.io/))
    https://github.com/infection/infection/pull/3118 via OTEL environment
    variables for local testing or for a screenshot for a PR. This is how,
    for example, I did for the screenshot in
    https://github.com/infection/infection/pull/3118.
    
    For reference, it now looks like this:
    
    <img width="1440" height="434" alt="Screenshot 2026-05-11 at 18 11 07"
    src="https://github.com/user-attachments/assets/de1d3cef-04e8-4997-9027-75f79751d4e0"
    />
    
    
    ## Related issues
    
    - #3090

--- tests/e2e/Telemetry_Console/run_tests.bash
+++ tests/e2e/Telemetry_Console/run_tests.bash
@@ -46,16 +46,20 @@ assert_line_count() {
     fi
 }
 
+rm -rf var/cache-phpstan var/infection var/phpunit var/report
+
 INFECTION_TELEMETRY=false php $INFECTION --no-interaction --no-progress \
     1> var/execution-no-env-variable.stdout \
     2> var/execution-no-env-variable.stderr
 
 diff -u --ignore-all-space expected.stderr var/execution-no-env-variable.stderr
-assert_contains "2 mutations were generated:" var/execution-no-env-variable.stdout
-assert_contains "2 mutants were killed by Test Framework" var/execution-no-env-variable.stdout
+assert_contains "6 mutations were generated:" var/execution-no-env-variable.stdout
+assert_contains "6 mutants were killed by Test Framework" var/execution-no-env-variable.stdout
 assert_contains "Mutation Code Coverage: 100%" var/execution-no-env-variable.stdout
 assert_contains "Covered Code MSI: 100%" var/execution-no-env-variable.stdout
 assert_not_contains '"name": "infection.' var/execution-no-env-variable.stdout
+
+rm -rf var/cache-phpstan var/infection var/phpunit var/report
 
 INFECTION_TELEMETRY=true OTEL_TRACES_EXPORTER=console php $INFECTION --no-interaction --quiet --no-progress \
     1> var/execution-with-trace-exporter.stdout \
@@ -66,8 +70,8 @@ assert_line_count 1 '"name": "infection.run"' var/execution-with-trace-exporter.
 assert_line_count 1 '"name": "infection.initial_tests"' var/execution-with-trace-exporter.stdout
 assert_line_count 1 '"name": "infection.mutation_generation"' var/execution-with-trace-exporter.stdout
 assert_line_count 1 '"name": "infection.mutation_testing"' var/execution-with-trace-exporter.stdout
-assert_line_count 2 '"name": "infection.mutation_evaluation"' var/execution-with-trace-exporter.stdout
+assert_line_count 6 '"name": "infection.mutation_evaluation"' var/execution-with-trace-exporter.stdout
 assert_contains '"service.name": "infection"' var/execution-with-trace-exporter.stdout
-assert_contains '"infection.source_file.count": 1' var/execution-with-trace-exporter.stdout
+assert_contains '"infection.source_file.count": 2' var/execution-with-trace-exporter.stdout
 assert_contains '"infection.mutation.count":' var/execution-with-trace-exporter.stdout
 assert_contains '"infection.mutation.status": "killed by tests"' var/execution-with-trace-exporter.stdout
