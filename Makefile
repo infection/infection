infection/infection

commit 2a14b20091b64c1300a489a2aa7f1c3226aeeef5

feat(telemetry): Add OTLP HTTP trace export support (#3109)
## Description

This enables `OTEL_TRACES_EXPORTER=otlp` and OTLP endpoint-based
tracing.

## Changes

- Allow the `otlp` traces exporter.
- Enable telemetry when `OTEL_EXPORTER_OTLP_ENDPOINT` or
`OTEL_EXPORTER_OTLP_TRACES_ENDPOINT` is configured.
- Add a failing tracer provider factory so exporter/configuration errors
are surfaced.
- Add Guzzle as the PSR HTTP client used by the OTLP HTTP exporter in
the PHAR and in development.
- Update telemetry docs with OTLP HTTP/protobuf examples, protobuf
extension notes, trace-specific OTLP variable precedence, compression
behavior, and `debug:telemetry` limitations.

## Reviewer Notes

The protocol `http/protobuf` works through the Composer
`google/protobuf` package which is required and act as polyfill for the
extension `protobuf`. However, the latter drastically improve
performances so a warning is logged when protobuf without the extension
is being used.

## Related issues

- https://github.com/infection/infection/issues/3090

Makefile
@@ -306,13 +306,23 @@ $(MAGO): vendor
 	touch -c $@
 
 $(INFECTION): vendor $(shell find bin/ src/ -type f) $(BOX) box.json.dist .git/HEAD
-	composer require infection/codeception-adapter infection/phpspec-adapter
+	# Backup the composer files
+	cp composer.json var/phar/composer.json.bak
+	cp composer.lock var/phar/composer.lock.bak
+
+	composer require \
+		guzzlehttp/guzzle \
+		infection/codeception-adapter \
+		infection/phpspec-adapter
+
 	# Workaround for https://github.com/box-project/box/issues/580
 	composer install --no-dev
 	$(BOX) --version
 	$(BOX) validate
 	$(BOX) compile
-	composer remove infection/codeception-adapter infection/phpspec-adapter
+
+	mv -f var/phar/composer.json.bak composer.json
+	mv -f var/phar/composer.lock.bak composer.lock
 	composer install
 	touch -c $@
 

devTools/mago-baseline.toml
@@ -109,6 +109,12 @@ message = 'Potentially unhandled exception `Symfony\Component\Filesystem\Excepti
 count = 1
 
 [[issues]]
+file = "src/Command/Debug/TelemetryConfigCommand.php"
+code = "unhandled-thrown-type"
+message = 'Potentially unhandled exception `RuntimeException` in `Infection\Command\Debug\TelemetryConfigCommand::executeCommand`.'
+count = 1
+
+[[issues]]
 file = "src/Command/DescribeCommand.php"
 code = "impossible-condition"
 message = "This condition (type `false`) will always evaluate to false."
@@ -2800,6 +2806,18 @@ count = 1
 file = "src/StaticAnalysis/StaticAnalysisToolTypes.php"
 code = "unused-parameter"
 message = "Parameter `$installedExtensions` is never used."
+count = 1
+
+[[issues]]
+file = "src/Telemetry/SDK/FailingTracerProviderFactory.php"
+code = "unhandled-thrown-type"
+message = 'Potentially unhandled exception `RuntimeException` in `Infection\Telemetry\SDK\FailingTracerProviderFactory::createExporter`.'
+count = 1
+
+[[issues]]
+file = "src/Telemetry/Subscriber/OpenTelemetryTracerSubscriberFactory.php"
+code = "unhandled-thrown-type"
+message = 'Potentially unhandled exception `RuntimeException` in `Infection\Telemetry\Subscriber\OpenTelemetryTracerSubscriberFactory::create`.'
 count = 1
 
 [[issues]]
@@ -11407,6 +11425,24 @@ message = "Type `iterable` in return type of `tracerProvider` is imprecise, equi
 count = 1
 
 [[issues]]
+file = "tests/phpunit/Telemetry/OpenTelemetryTracerFactoryTest.php"
+code = "unhandled-thrown-type"
+message = 'Potentially unhandled exception `RuntimeException` in `Infection\Tests\Telemetry\OpenTelemetryTracerFactoryTest::test_it_creates_a_tracer`.'
+count = 1
+
+[[issues]]
+file = "tests/phpunit/Telemetry/OpenTelemetryTracerFactoryTest.php"
+code = "unhandled-thrown-type"
+message = 'Potentially unhandled exception `RuntimeException` in `Infection\Tests\Telemetry\OpenTelemetryTracerFactoryTest::test_it_keeps_the_existing_service_name_when_creating_a_tracer`.'
+count = 1
+
+[[issues]]
+file = "tests/phpunit/Telemetry/OpenTelemetryTracerFactoryTest.php"
+code = "unhandled-thrown-type"
+message = 'Potentially unhandled exception `RuntimeException` in `Infection\Tests\Telemetry\OpenTelemetryTracerFactoryTest::test_it_sets_the_default_service_name_when_creating_a_tracer`.'
+count = 1
+
+[[issues]]
 file = "tests/phpunit/TestFramework/Coverage/CoveredTraceProviderTest.php"
 code = "impossible-type-comparison"
 message = 'Impossible type assertion: `$traces` of type `array<array-key, Infection\TestFramework\Tracing\Trace\Trace>` can never be `list{int(1), int(2), int(3)}`.'
@@ -13624,4 +13660,10 @@ count = 1
 file = "tests/phpunit/WithConsecutive.php"
 code = "unhandled-thrown-type"
 message = 'Potentially unhandled exception `RuntimeException` in `Infection\Tests\WithConsecutive::create`.'
+count = 1
+
+[[issues]]
+file = "tests/phpunit/bootstrap.php"
+code = "mixed-assignment"
+message = "Assigning `mixed` type to a variable may lead to unexpected behavior."
 count = 1

phpunit.xml.dist
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <phpunit xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
          xsi:noNamespaceSchemaLocation="./vendor/phpunit/phpunit/phpunit.xsd"
-         bootstrap="./vendor/autoload.php"
+         bootstrap="./tests/phpunit/bootstrap.php"
          colors="true"
          executionOrder="defects,random"
          failOnWarning="true"

src/Telemetry/OpenTelemetryTracerFactory.php
@@ -41,9 +41,10 @@ use function filter_var;
 use function getenv;
 use function implode;
 use function in_array;
+use Infection\Telemetry\SDK\FailingTracerProviderFactory;
 use InvalidArgumentException;
 use OpenTelemetry\SDK\Common\Configuration\Variables;
-use OpenTelemetry\SDK\Trace\TracerProviderFactory;
+use RuntimeException;
 use function Safe\putenv;
 use function sprintf;
 use function strtolower;
@@ -58,6 +59,9 @@ final readonly class OpenTelemetryTracerFactory
      */
     private const string TRACER_NAME = 'infection';
 
+    /**
+     * @throws RuntimeException
+     */
     public function create(): ?OpenTelemetryTracer
     {
         self::guardSupportedExporters();
@@ -71,7 +75,7 @@ final readonly class OpenTelemetryTracerFactory
         // However, it's a lot of boilerplate, so not worth it, at least at
         // the time of writing.
         self::setDefaultServiceName();
-        $tracerProvider = (new TracerProviderFactory())->create();
+        $tracerProvider = (new FailingTracerProviderFactory())->create();
 
         return new OpenTelemetryTracer(
             $tracerProvider->getTracer(self::TRACER_NAME),
@@ -88,8 +92,12 @@ final readonly class OpenTelemetryTracerFactory
     {
         $tracesExporter = getenv(Variables::OTEL_TRACES_EXPORTER);
 
-        return $tracesExporter !== false
-            && strtolower($tracesExporter) !== 'none';
+        if ($tracesExporter !== false) {
+            return strtolower($tracesExporter) !== 'none';
+        }
+
+        return getenv(Variables::OTEL_EXPORTER_OTLP_ENDPOINT) !== false
+            || getenv(Variables::OTEL_EXPORTER_OTLP_TRACES_ENDPOINT) !== false;
     }
 
     private static function setDefaultServiceName(): void
@@ -105,7 +113,7 @@ final readonly class OpenTelemetryTracerFactory
 
     private static function guardSupportedExporters(): void
     {
-        self::guardExporter(Variables::OTEL_TRACES_EXPORTER, ['console', 'none']);
+        self::guardExporter(Variables::OTEL_TRACES_EXPORTER, ['otlp', 'console', 'none']);
         self::guardExporter(Variables::OTEL_METRICS_EXPORTER, ['none']);
         self::guardExporter(Variables::OTEL_LOGS_EXPORTER, ['none']);
         self::guardAutoload();

src/Telemetry/SDK/FailingTracerProviderFactory.php
@@ -1,0 +1,104 @@
+<?php
+/**
+ * This code is licensed under the BSD 3-Clause License.
+ *
+ * Copyright (c) 2017, Maks Rafalko
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+declare(strict_types=1);
+
+namespace Infection\Telemetry\SDK;
+
+use function extension_loaded;
+use OpenTelemetry\API\Behavior\LogsMessagesTrait;
+use OpenTelemetry\Contrib\Otlp\SpanExporter;
+use OpenTelemetry\SDK\Sdk;
+use OpenTelemetry\SDK\Trace\ExporterFactory;
+use OpenTelemetry\SDK\Trace\NoopTracerProvider;
+use OpenTelemetry\SDK\Trace\SamplerFactory;
+use OpenTelemetry\SDK\Trace\SpanExporterInterface;
+use OpenTelemetry\SDK\Trace\SpanProcessorFactory;
+use OpenTelemetry\SDK\Trace\TracerProvider;
+use OpenTelemetry\SDK\Trace\TracerProviderFactory;
+use OpenTelemetry\SDK\Trace\TracerProviderInterface;
+use RuntimeException;
+
+/**
+ * Unlike the upstream factory, it lets the configuration errors cause a failure
+ * instead of degrading to no-op telemetry.
+ *
+ * @see TracerProviderFactory
+ * @internal
+ */
+final readonly class FailingTracerProviderFactory
+{
+    use LogsMessagesTrait;
+
+    public function __construct(
+        private ExporterFactory $exporterFactory = new ExporterFactory(),
+        private SamplerFactory $samplerFactory = new SamplerFactory(),
+        private SpanProcessorFactory $spanProcessorFactory = new SpanProcessorFactory(),
+    ) {
+    }
+
+    /**
+     * @throws RuntimeException
+     */
+    public function create(): TracerProviderInterface
+    {
+        if (Sdk::isDisabled()) {
+            return new NoopTracerProvider();
+        }
+
+        // No exception is caught (unlike TracerProviderFactory) when creating
+        // those services.
+        $exporter = $this->createExporter();
+        $sampler = $this->samplerFactory->create();
+        $spanProcessor = $this->spanProcessorFactory->create($exporter);
+
+        return new TracerProvider(
+            $spanProcessor,
+            $sampler,
+        );
+    }
+
+    private function createExporter(): ?SpanExporterInterface
+    {
+        $exporter = $this->exporterFactory->create();
+
+        if (
+            $exporter instanceof SpanExporter
+            && !extension_loaded('protobuf')
+        ) {
+            self::logWarning('protobuf is being used as a transport without the extension. See https://opentelemetry.io/docs/languages/php/#optional-php-extensions');
+        }
+
+        return $exporter;
+    }
+}

tests/phpunit/AutoReview/ProjectCode/ProjectCodeProvider.php
@@ -91,6 +91,7 @@ use Infection\Source\Collector\FixedSourceCollector;
 use Infection\Source\Collector\GitDiffSourceCollector;
 use Infection\Source\Matcher\FakeSourceLineMatcher;
 use Infection\Source\Matcher\NullSourceLineMatcher;
+use Infection\Telemetry\SDK\FailingTracerProviderFactory;
 use Infection\Telemetry\SpanHandle;
 use Infection\Telemetry\Subscriber\OpenTelemetryTracerSubscriberFactory;
 use Infection\TestFramework\AdapterInstaller;
@@ -149,6 +150,7 @@ final class ProjectCodeProvider
         FakeSourceCollector::class,
         FakeSourceFilter::class,
         FakeSourceLineMatcher::class,
+        FailingTracerProviderFactory::class,
         FileSystem::class,
         FilterOption::class,
         FixedSourceCollector::class,
@@ -343,6 +345,7 @@ final class ProjectCodeProvider
             ->files()
             ->name('*.php')
             ->in(__DIR__ . '/../../../../tests')
+            ->notName('bootstrap.php')
             ->notName('DummySymfony5FileSystem.php')
             ->notName('DummySymfony6FileSystem.php')
             ->exclude([

tests/phpunit/Telemetry/OpenTelemetryTracerFactoryTest.php
@@ -39,6 +39,7 @@ use Exception;
 use function getenv;
 use Infection\Telemetry\OpenTelemetryTracer;
 use Infection\Telemetry\OpenTelemetryTracerFactory;
+use Infection\Telemetry\SDK\FailingTracerProviderFactory;
 use Infection\Tests\EnvVariableManipulation\BacksUpEnvironmentVariables;
 use InvalidArgumentException;
 use OpenTelemetry\SDK\Common\Configuration\Variables;
@@ -48,10 +49,12 @@ use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase;
 use function Safe\putenv;
+use UnexpectedValueException;
 
 #[BackupGlobals(true)]
 #[Group('integration')]
 #[CoversClass(OpenTelemetryTracerFactory::class)]
+#[CoversClass(FailingTracerProviderFactory::class)]
 final class OpenTelemetryTracerFactoryTest extends TestCase
 {
     use BacksUpEnvironmentVariables;
@@ -126,6 +129,43 @@ final class OpenTelemetryTracerFactoryTest extends TestCase
             $expectTracer,
         ];
 
+        yield 'OTLP traces exporter' => [
+            [
+                Variables::OTEL_TRACES_EXPORTER => 'otlp',
+            ],
+            $expectTracer,
+        ];
+
+        yield 'OTLP exporter endpoint' => [
+            [
+                Variables::OTEL_EXPORTER_OTLP_ENDPOINT => 'http://localhost:4318',
+            ],
+            $expectTracer,
+        ];
+
+        yield 'OTLP traces exporter endpoint' => [
+            [
+                Variables::OTEL_EXPORTER_OTLP_TRACES_ENDPOINT => 'http://localhost:4318/v1/traces',
+            ],
+            $expectTracer,
+        ];
+
+        yield 'OTLP exporter endpoint with unsupported OTLP protocol' => [
+            [
+                Variables::OTEL_EXPORTER_OTLP_ENDPOINT => 'http://localhost:4318',
+                Variables::OTEL_EXPORTER_OTLP_PROTOCOL => 'foo',
+            ],
+            new UnexpectedValueException('Unknown protocol: foo'),
+        ];
+
+        yield 'OTLP traces exporter endpoint with unsupported OTLP traces protocol' => [
+            [
+                Variables::OTEL_EXPORTER_OTLP_TRACES_ENDPOINT => 'http://localhost:4318/v1/traces',
+                Variables::OTEL_EXPORTER_OTLP_TRACES_PROTOCOL => 'foo',
+            ],
+            new UnexpectedValueException('Unknown protocol: foo'),
+        ];
+
         yield 'console traces exporter with OpenTelemetry PHP autoload disabled' => [
             [
                 Variables::OTEL_TRACES_EXPORTER => 'console',
@@ -136,10 +176,10 @@ final class OpenTelemetryTracerFactoryTest extends TestCase
 
         yield 'unsupported traces exporter' => [
             [
-                Variables::OTEL_TRACES_EXPORTER => 'otlp',
+                Variables::OTEL_TRACES_EXPORTER => 'http',
             ],
             new InvalidArgumentException(
-                'Unsupported OpenTelemetry exporter configured via OTEL_TRACES_EXPORTER="otlp". Supported values: console, none.',
+                'Unsupported OpenTelemetry exporter configured via OTEL_TRACES_EXPORTER="http". Supported values: otlp, console, none.',
             ),
         ];
 

tests/phpunit/bootstrap.php
@@ -1,0 +1,43 @@
+<?php
+/**
+ * This code is licensed under the BSD 3-Clause License.
+ *
+ * Copyright (c) 2017, Maks Rafalko
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+declare(strict_types=1);
+
+use OpenTelemetry\API\Behavior\Internal\Logging;
+use OpenTelemetry\API\Behavior\Internal\LogWriter\NoopLogWriter;
+
+$loader = require __DIR__ . '/../../vendor/autoload.php';
+
+Logging::setLogWriter(new NoopLogWriter());
+
+return $loader;

commit 89006b8a77edc3c02fe46ec3befa5820f6b75017

build: Fix the PHAR building
Building the PHAR failed because we tried to copy into the non-existent
directory `var/phar`.

This issue was introduced in #3109.

Makefile
@@ -321,6 +321,7 @@ $(MAGO): vendor
 
 $(INFECTION): vendor $(shell find bin/ src/ -type f) $(BOX) box.json.dist .git/HEAD
 	# Backup the composer files
+	mkdir -p var/phar
 	cp composer.json var/phar/composer.json.bak
 	cp composer.lock var/phar/composer.lock.bak
 