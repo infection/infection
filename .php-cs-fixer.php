26d63ddb3acc363f42b4e70c2ed57809ad77f119
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2026-05-29 13:11:20 +0200

    chore: PHP CS Fixer - no need to explicitly name PHPUnit 6x0 migration, as it's part of 7x5 one

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -80,7 +80,6 @@ return (new Config())
     ->setRules([
         '@PHP7x1Migration' => true,
         '@PHP7x1Migration:risky' => true,
-        '@PHPUnit6x0Migration:risky' => true,
         '@PHPUnit7x5Migration:risky' => true,
         '@Symfony' => true,
         '@Symfony:risky' => true,
