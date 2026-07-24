cde8b38a15f2ead5f83a812f922ac2e26fdd5c4b
Théo FIDRY <theo.fidry@gmail.com>
2026-07-24 12:07:34 +0200

    add test

---
+++ tests/e2e/PCOV_Source_Outside_Working_Directory/server/src/SourceClass.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCOVSourceOutsideWorkingDirectory;
+
+final class SourceClass
+{
+    public function returnsTrue(): bool
+    {
+        return true;
+    }
+}
