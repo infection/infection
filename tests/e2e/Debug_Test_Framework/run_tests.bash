c44a5610e95e500a14239cd8a9dc4adda8c0284e
Théo FIDRY <theo.fidry@gmail.com>
2026-08-03 01:07:13 +0200

    remove phpdbg and fix coverage paths

--- tests/e2e/Debug_Test_Framework/run_tests.bash
+++ tests/e2e/Debug_Test_Framework/run_tests.bash
@@ -9,23 +9,19 @@ readonly INFECTION=../../../${1}
 rm -rf var
 mkdir var
 
-if [ "${DRIVER:-}" = "phpdbg" ]; then
-    environment=phpdbg
-    phpdbg -qrr "$INFECTION" --coverage=coverage --no-progress
+if php -r 'exit(extension_loaded("pcov") ? 0 : 1);'; then
+    environment=pcov
 else
-    if php -r 'exit(extension_loaded("pcov") ? 0 : 1);'; then
-        environment=pcov
-    else
-        environment=xdebug
-    fi
-
-    if XDEBUG_MODE=coverage php -r 'exit(extension_loaded("xdebug") ? 0 : 1);'; then
-        if [ "$environment" = "pcov" ]; then
-            environment=pcov-with-xdebug
-        fi
-    fi
-
-    XDEBUG_MODE=coverage php -d memory_limit=-1 "$INFECTION" --coverage=coverage --no-progress
+    environment=xdebug
 fi
+
+if XDEBUG_MODE=coverage php -r 'exit(extension_loaded("xdebug") ? 0 : 1);'; then
+    if [ "$environment" = "pcov" ]; then
+        environment=pcov-with-xdebug
+    fi
+fi
+
+XDEBUG_MODE=coverage php vendor/bin/phpunit --coverage-xml=var/coverage
+XDEBUG_MODE=coverage php -d memory_limit=-1 "$INFECTION" --coverage=var/coverage --no-progress
 
 php verify.php var/infection.debug.jsonl "expected/$environment.jsonl"
