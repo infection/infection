74b97117a422bb92bb87a960c797fd4016fa3b49
Théo FIDRY <theo.fidry@gmail.com>
2026-08-13 10:04:59 +0200

    revert

--- src/TestFramework/Common/CommandLineBuilder.php
+++ src/TestFramework/Common/CommandLineBuilder.php
@@ -85,7 +85,7 @@ class CommandLineBuilder
          * This lets folks use, say, a bash wrapper over phpunit.
          */
         if ('cli' === PHP_SAPI && $phpExtraArgs === [] && is_executable($testFrameworkExecutable) && shell_exec('command -v php') !== null) {
-            return array_values(array_merge([$testFrameworkExecutable], $frameworkArgs));
+            return array_merge([$testFrameworkExecutable], $frameworkArgs);
         }
 
         /*
