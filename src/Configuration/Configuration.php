865f21aaa5451bfebc18ba4c318c3c09358b45fd
Théo FIDRY <theo.fidry@gmail.com>
2026-08-14 23:39:16 +0200

    revert

--- src/Configuration/Configuration.php
+++ src/Configuration/Configuration.php
@@ -119,8 +119,6 @@ readonly class Configuration
         Assert::nullOrGreaterThanEq($minMsi, 0.);
         Assert::greaterThanEq($threadCount, 0);
 
-        var_dump(LogVerbosity::resolveDisplayedStatuses($this->logVerbosity));die;
-
         if (is_string($dotsPerRow)) {
             Assert::same($dotsPerRow, 'max');
         } else {
