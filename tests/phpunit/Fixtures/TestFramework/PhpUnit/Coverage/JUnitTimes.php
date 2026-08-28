5d2dd3d5c5b9bb69f381485e6372249be8c6e5cf
Théo FIDRY <theo.fidry@gmail.com>
2026-08-28 10:42:57 +0200

    cleanup

--- tests/phpunit/Fixtures/TestFramework/PhpUnit/Coverage/JUnitTimes.php
+++ tests/phpunit/Fixtures/TestFramework/PhpUnit/Coverage/JUnitTimes.php
@@ -33,7 +33,7 @@
 
 declare(strict_types=1);
 
-namespace Infection\Tests\TestFramework\Factory\PhpUnit\Coverage;
+namespace Infection\Tests\Fixtures\TestFramework\PhpUnit\Coverage;
 
 final class JUnitTimes
 {
